b08be634eebf0975a1a1cbe9c34018236970085c
Rutherford Appleton Labs <obselete@comlab.ox.ac.uk>
2005-04-20 16:38:29 +0000

    End of Wednesday
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@32 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pdes/AbstractMeshReader.hpp
+++ pdes/AbstractMeshReader.hpp
@@ -12,9 +12,55 @@ class AbstractMeshReader
 	//private:
 	protected:
 		int mNumElements;
+		int mNumNodes;
+		int mNumFaces;
+		int mDimension;
 		std::string mPathBaseName;
+		
+		double **mppNodes;
+		int **mppElements;
 	
-	//public:
+		int** MakeIntDynamicArray(int numRows, int numColumns)
+		{
+			int **ppDynamicArray = new int *[numRows];
+			for (int i = 0; i < numRows; i++)
+			{
+				ppDynamicArray[i] = new int [numColumns];
+				for (int j = 0; j < numColumns; j++)
+				{
+					ppDynamicArray[i][j] = 0;	
+				}
+			}
+			return ppDynamicArray;
+		}
+		
+		double** MakeDoubleDynamicArray(int numRows, int numColumns)
+		{
+			double **ppDynamicArray = new double *[numRows];
+			for (int i = 0; i < numRows; i++)
+			{
+				ppDynamicArray[i] = new double [numColumns];
+				for (int j = 0; j < numColumns; j++)
+				{
+					ppDynamicArray[i][j] = 0.0;	
+				}
+			}
+			return ppDynamicArray;
+		}
+	
+	public:
+		AbstractMeshReader()
+		{
+			mNumElements = 0;
+			mNumNodes = 0;
+			mNumFaces = 0;
+			mDimension = 0;
+			mPathBaseName = "";
+		}
+		int GetNumElements(){return mNumElements;}
+		int GetNumNodes(){return mNumNodes;}
+		int GetNumFaces(){return mNumFaces;}
+		int GetDimension(){return mDimension;}		
 
 };
 #endif //_ABSTRACTMESHREADER_HPP_
--- pdes/TrianglesMeshReader.cpp
+++ pdes/TrianglesMeshReader.cpp
@@ -13,6 +13,29 @@ TrianglesMeshReader::TrianglesMeshReader(std::string pathBaseName)
 	{
 		throw Exception("Could not open node file "+nodeFileName+" .");
 	}
+	
+	int numAttributes, maxBoundaryMarkers;
+	nodeFile >> mNumNodes >> mDimension >> numAttributes >> maxBoundaryMarkers;
+	
+	mppNodes = MakeDoubleDynamicArray(mNumNodes,3);
+	
+	nodeFile.close();
+	
+	std::string elementFileName=pathBaseName+".ele";
+	std::ifstream elementFile(elementFileName.c_str());
+	
+	
+	if (!elementFile.is_open())
+	{
+		throw Exception("Could not open element file "+elementFileName+" .");
+	}
+	
+	int numAttributes, maxBoundaryMarkers;
+	elementFile >> mNumElements >> mDimension >> numAttributes >> maxBoundaryMarkers;
+	
+	mppNodes = MakeDoubleDynamicArray(mNumNodes,3);
+	
+	nodeFile.close();
 }
 
 TrianglesMeshReader::~TrianglesMeshReader()
