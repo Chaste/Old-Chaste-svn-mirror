c9ef4f561b792926a6e5ec381b574325a4c8f990
Old User <obselete@comlab.ox.ac.uk>
2005-04-21 17:09:24 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@118 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ odes/AdamsBashforthIvpOdeSolver.cpp
@@ -0,0 +1,91 @@
+#include "AdamsBashforthIvpOdeSolver.hpp"
+#include "AbstractIvpOdeSolver.hpp"
+#include "AbstractOdeSystem.hpp"
+#include "OdeSolution.hpp"
+
+#include <iostream>
+#include <vector>
+
+/*
+ * OdeSolution is an object containing an integer of the number of equations, 
+ * a std::vector of times and a std::vector of std::vectors of the solution 
+ * of the ODE system at those times
+*/
+
+OdeSolution AdamsBashforthIvpOdeSolver::Solve(AbstractOdeSystem* pAbstractOdeSystem, 
+				double startTime,
+				double endTime,
+				double timeStep,
+				std::vector<double> initialConditions)
+{
+
+    int num_equations = pAbstractOdeSystem->mNumberOfEquations;
+    int num_timesteps = ((int) ((endTime - startTime)/timeStep));
+    
+    double last_timestep = endTime - ((double) num_timesteps)*timeStep;
+    
+	OdeSolution solutions;
+	solutions.mNumberOfTimeSteps = num_timesteps;
+		
+	solutions.mSolutions.push_back(initialConditions);
+	solutions.mTime.push_back(startTime);
+	
+	std::vector<double> row(num_equations);	
+	std::vector<double> dy(num_equations);
+	
+	row=initialConditions;
+	
+	std::vector<std::vector<double> > temp;
+	
+	
+	// Apply Euler's method for first three timesteps: 
+	// we might need to think about maintaining 4th order accuracy of A-B method
+	for(int timeindex=0; timeindex<3; timeindex++)
+	{
+        dy = pAbstractOdeSystem->EvaluateYDerivatives(solutions.mTime[timeindex],row);
+        
+		for(int i=0;i<num_equations; i++) 
+		{
+			row[i] = row[i] + timeStep*dy[i];		
+		}
+		
+		solutions.mSolutions.push_back(row);	
+		solutions.mTime.push_back(solutions.mTime[timeindex]+timeStep);
+		temp.push_back(dy);
+			}
+        
+	// Apply Adams-Bashforth method
+    for (int timeindex=3; timeindex<num_timesteps; timeindex++)
+    {
+    	
+        
+    	dy = pAbstractOdeSystem->EvaluateYDerivatives(solutions.mTime[timeindex],row);
+        
+  		for(int i=0;i<num_equations; i++) 
+		{		
+			row[i] = row[i] + (timeStep/24.0)*(55.0*dy[i] - 59.0*temp[timeindex-1][i] + 37.0*temp[timeindex-2][i] - 9.0*temp[timeindex-3][i]);		
+		}
+		
+		solutions.mSolutions.push_back(row);
+		solutions.mTime.push_back(solutions.mTime[timeindex]+timeStep);
+		temp.push_back(dy);
+	}
+	
+	// Extra step to get to exactly endTime
+	int timeindex = num_timesteps;
+	if(last_timestep > 0.00001)
+	{	
+		solutions.mNumberOfTimeSteps=num_timesteps+1;
+		dy = pAbstractOdeSystem->EvaluateYDerivatives(solutions.mTime[num_timesteps],row);
+		for(int i=0;i<num_equations; i++) 
+		{				
+			row[i] = row[i] + (timeStep/24.0)*(55.0*dy[i] - 59.0*temp[timeindex-1][i] + 37.0*temp[timeindex-2][i] - 9.0*temp[timeindex-3][i]);		
+		}
+		
+		solutions.mSolutions.push_back(row);
+		solutions.mTime.push_back(solutions.mTime[num_timesteps]+last_timestep);
+	}
+	
+			
+	return solutions;
+}
