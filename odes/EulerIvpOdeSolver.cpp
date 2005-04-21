ed329f29f4e8a84a48db57641cfdc0672ef8b2dc
Old User <obselete@comlab.ox.ac.uk>
2005-04-21 14:45:41 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@80 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- odes/EulerIvpOdeSolver.cpp
+++ odes/EulerIvpOdeSolver.cpp
@@ -3,17 +3,20 @@
 #include "AbstractOdeSystem.hpp"
 #include "OdeSolution.hpp"
 
-//#include "petscvec.h"
 #include <iostream>
 #include <vector>
 
-// OdeSolution is an object containing PETSc vectors of time and solution of the ODE system at those times
+/*
+ * OdeSolution is an object containing an integer of the number of equations, 
+ * a std::vector of times and a std::vector of std::vectors of the solution 
+ * of the ODE system at those times
+*/
 
 OdeSolution EulerIvpOdeSolver::Solve(AbstractOdeSystem* pAbstractOdeSystem, 
-				double startTime,
-				double endTime,
-				double timeStep,
-				std::vector<double> initialConditions)
+				                     double startTime,
+				                     double endTime,
+				                     double timeStep,
+				                     std::vector<double> initialConditions)
 {
 
     int num_equations = pAbstractOdeSystem->mNumberOfEquations;
@@ -21,38 +24,30 @@ OdeSolution EulerIvpOdeSolver::Solve(AbstractOdeSystem* pAbstractOdeSystem,
     
     double last_timestep = endTime - ((double) num_timesteps)*timeStep;
     
-	// Convert Initial conditions from PETSc to normal vector
 	OdeSolution solutions;
-	// (num_timesteps)(num_equations)
+		
+	solutions.mSolutions.push_back(initialConditions);
+	solutions.mTime.push_back(startTime);
 	
-	std::vector<double> row(num_equations);	
-	std::vector<double> dy(num_equations);
-	
+    std::vector<double> row(num_equations);	
 	row=initialConditions;
 	
-	solutions.mSolutions.push_back(initialConditions);
-
-	
-	solutions.mTime.push_back(startTime);
+	std::vector<double> dy(num_equations);	
 	
 	for(int timeindex=0;timeindex<num_timesteps;timeindex++)
 	{
-		// Work out next time step
-		// EULER METHOD HERE ************************************		
+		// Apply Euler's method
         dy = pAbstractOdeSystem->EvaluateYDerivatives(solutions.mTime[timeindex],row);
+        
 		for(int i=0;i<num_equations; i++) 
 		{
 			row[i] = row[i] + timeStep*dy[i];		
 		}
-		solutions.mSolutions.push_back(row);
 		
+		solutions.mSolutions.push_back(row);
 		solutions.mTime.push_back(solutions.mTime[timeindex]+timeStep);
 	}
-	
-	
-	
-
-	// Create the PETSc vector of times		
-		solutions.mNumberOfTimeSteps = num_timesteps;
-		return solutions;
+		
+	solutions.mNumberOfTimeSteps = num_timesteps;
+	return solutions;
 }
