cb922f78e0eeb79d5ec5dc09ece32b71b4bb2be3
Old User <obselete@comlab.ox.ac.uk>
2005-04-21 14:40:43 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@79 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ heart/tests/TestOdeSolverForLR91.hpp
@@ -0,0 +1,20 @@
+#ifndef _TESTODESOLVERFORLR91_HPP_
+#define _TESTODESOLVERFORLR91_HPP_
+
+
+#include <cxxtest/TestSuite.h>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "LR91Model.hpp""
+
+
+class TestOdeSolverForLR91 : public CxxTest::TestSuite
+{
+    
+}
+
+
+
+#endif //_TESTODESOLVERFORLR91_HPP_
