83833697a3ffb77e04bc3edb774dccc5e1b14d60
Old User <obselete@comlab.ox.ac.uk>
2005-04-22 12:43:31 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@159 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/tests/TestOdeSolverForLR91.hpp
+++ heart/tests/TestOdeSolverForLR91.hpp
@@ -10,6 +10,9 @@
 #include "LR91Model.hpp"
 #include "AbstractStimulusFunction.hpp"
 #include "InitialStimulus.hpp"
+#include "SodiumCurrentLR91.hpp"
+#include "PotassiumTimeDependentCurrentLR91.hpp"
+#include "PotassiumTimeIndependentCurrentLR91.hpp"
 #include "EulerIvpOdeSolver.hpp"
 #include "OdeSolution.hpp"
 
@@ -67,11 +70,19 @@ class TestOdeSolverForLR91 : public CxxTest::TestSuite
 //        TS_ASSERT_DELTA(Result[6],  , 0.0001);
 //        TS_ASSERT_DELTA(Result[7],  , 0.0001);
 //          
+//        TS_TRACE("IT WORKS!!!! :)) ");
         TS_TRACE("LupRudy WORKS!!!! :)) ");
         
 //        
 //       //test 2 
-//
+//        voltage = -40.0;
+//        m = 0.1;
+//        h = 0.03;
+//        j = 0.7;
+//        d = 0.05;
+//        f = 0.5;
+//        x = 0.1;
+//        caI = 0.001;
 //        double magnitudeOfStimulus = 80.0;        
 //        AbstractStimulusFunction *pStimulus = new InitialStimulus(magnitudeOfStimulus);
 //        
@@ -98,42 +109,67 @@ class TestOdeSolverForLR91 : public CxxTest::TestSuite
     
     
    
-//	// Tests that Na gating variables within range 0 to 1 inclusive
-//	void testSodiumGatingVariables( void )
-//	{
-//		double m = 0.0017;
-//		double h = 0.9833;
-//		double j = 0.9895;
-//		double voltage = -75.0;
-//		
-//		SodiumCurrentLR91 *pINa = new SodiumCurrentLR91();
-//		pINa->UpdateMagnitudeOfCurrent(voltage,m,h,j);
-//		
-//		std::cout << "\n";
-//		std::cout << "INa m gate: " << pINa->GetM() << "\n";
-//		TS_ASSERT(pINa->GetM() >= 0);
-//		TS_ASSERT(pINa->GetM() <= 1);
-//		
-//		std::cout << "INa h gate: " << pINa->GetH() << "\n";
-//		TS_ASSERT(pINa->GetH() >= 0);
-//		TS_ASSERT(pINa->GetH() <= 1);		
-//	}
-//	
-//	// Tests that K gating variables within range 0 to 1 inclusive
-//	void testPotassiumGatingVariables( void )
-//	{
-//		double m = 0.0017;
-//		double h = 0.9833;
-//		double j = 0.9895;
-//		double voltage = -75.0;
-//		
-//		PotassiumCurrent *pIK = new PotassiumCurrent();
-//		pIK->UpdateMagnitudeOfCurrent(voltage,m,h,j);
-//			
-//		std::cout << "IK n gate: " << pIK->GetN() << "\n";
-//		TS_ASSERT(pIK->GetN() >= 0);
-//		TS_ASSERT(pIK->GetN() <= 1);		
-//	}
+	// Tests that Na gating variables within range 0 to 1 inclusive
+	void testSodiumGatingVariables( void )
+	{
+		double m = 0.0017;
+		double h = 0.9833;
+		double j = 0.9895;
+		double voltage = -75.0;
+		
+		SodiumCurrentLR91 *pINa = new SodiumCurrentLR91();
+		pINa->UpdateMagnitudeOfCurrent(voltage,m,h,j);
+		
+		std::cout << "\n";
+		std::cout << "INa m gate: " << pINa->GetM() << "\n";
+		TS_ASSERT(pINa->GetM() >= 0);
+		TS_ASSERT(pINa->GetM() <= 1);
+		
+		std::cout << "INa h gate: " << pINa->GetH() << "\n";
+		TS_ASSERT(pINa->GetH() >= 0);
+		TS_ASSERT(pINa->GetH() <= 1);		
+	}
+	
+	// Tests that K gating variables within range 0 to 1 inclusive
+	void testPotassiumTimeDependentGatingVariables( void )
+	{
+		double x = 0.0056;
+		double voltage = -75.0;
+		
+		PotassiumTimeDependentCurrentLR91 *pIK = new PotassiumTimeDependentCurrentLR91();
+		pIK->UpdateMagnitudeOfCurrent(voltage,x);
+			
+		std::cout << "IK n gate: " << pIK->GetX() << "\n";
+		TS_ASSERT(pIK->GetX() >= 0);
+		TS_ASSERT(pIK->GetX() <= 1);		
+	}
+	
+	// Tests that K gating variables within range 0 to 1 inclusive
+	void testPotassiumTimeIndependentGatingVariables( void )
+	{
+		double voltage = -75.0;
+		
+		PotassiumTimeIndependentCurrentLR91 *pIK = new PotassiumTimeIndependentCurrentLR91();
+		pIK->UpdateMagnitudeOfCurrent(voltage);
+			
+		std::cout << "IK n gate: " << pIK->GetK1(voltage) << "\n";
+		TS_ASSERT(pIK->GetK1(voltage) >= 0);
+		TS_ASSERT(pIK->GetK1(voltage) <= 1);		
+	}
+	
+	// Tests that K gating variables within range 0 to 1 inclusive
+	void testPlateauPotassiumCurrentLR91( void )
+	{
+		double x = 0.0056;
+		double voltage = -75.0;
+		
+		PotassiumTimeDependentCurrentLR91 *pIK = new PotassiumTimeDependentCurrentLR91();
+		pIK->UpdateMagnitudeOfCurrent(voltage,x);
+			
+		std::cout << "IK n gate: " << pIK->GetX() << "\n";
+		TS_ASSERT(pIK->GetX() >= 0);
+		TS_ASSERT(pIK->GetX() <= 1);		
+	}
 	
 	
 	
