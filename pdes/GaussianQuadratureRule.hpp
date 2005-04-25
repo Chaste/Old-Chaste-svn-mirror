29e2803b64fefc017019ae5f1193ea3411a9775b
Old User <obselete@comlab.ox.ac.uk>
2005-04-25 10:12:51 +0000

    Add 2d quadrature points, and tests.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@186 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pdes/GaussianQuadratureRule.hpp
+++ pdes/GaussianQuadratureRule.hpp
@@ -7,6 +7,16 @@
 #include <iostream>
 #include <cmath>
 
+/**
+ * This class encapsulates tables of gaussian quadrature points and the
+ * associated weights.
+ * 
+ * Data is available for 1d and 2d quadrature over (canonical) triangles,
+ * with between 1 and 3 (inclusive) gauss points in each dimension.
+ * 
+ * TODO: Add 3d.
+ */
+
 template<int ELEM_DIM>
 class GaussianQuadratureRule
 {
@@ -16,6 +26,11 @@ class GaussianQuadratureRule
 	
 public:
 
+	/**
+	 * The constructor builds the appropriate table for the dimension (given
+	 * by the template argument) and number of points in each dimension (given
+	 * as a constructor argument).
+	 */
 	GaussianQuadratureRule(int numPointsInEachDimension)
 	{
 		assert(numPointsInEachDimension >  0);
@@ -32,12 +47,12 @@ public:
 			{
 				switch(numPointsInEachDimension)
 				{
-					case 1: 
+					case 1: // 1d, 1 point
 					mWeights.push_back(1);
 					mPoints.push_back(Point<ELEM_DIM>(0.5)); //check
 					break;
 					
-					case 2: 
+					case 2: // 1d, 2 points
 					mWeights.push_back(0.5);
 					mWeights.push_back(0.5);
 					
@@ -45,7 +60,7 @@ public:
 					mPoints.push_back(Point<ELEM_DIM>(0.78867513459481));
 					break;
 					
-					case 3: 
+					case 3: // 1d, 3 points
 					mWeights.push_back(5.0/18.0);
 					mWeights.push_back(4.0/9.0);
 					mWeights.push_back(5.0/18.0);
@@ -58,49 +73,46 @@ public:
 			}
 			break;
 			case 2 :
-			{
-				//TODO: fill in
-				assert(0);
-				
+			{				
 				switch(numPointsInEachDimension)
 				{
-					case 1: 
-					mWeights.push_back(1);
-					mPoints.push_back(Point<ELEM_DIM>(1));
+					case 1: // 2d, 1 point per dimension
+					mWeights.push_back(0.5);
+					mPoints.push_back(Point<ELEM_DIM>(0.25,0.5));
 					break;
 					
-					case 2: 
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
+					case 2: // 2d, 2 points per dimension
+					mWeights.push_back(0.19716878364870);
+					mWeights.push_back(0.19716878364870);
+					mWeights.push_back(0.05283121635130);
+					mWeights.push_back(0.05283121635130);
 					
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
+					mPoints.push_back(Point<ELEM_DIM>(0.16666666666667,0.21132486540519));
+					mPoints.push_back(Point<ELEM_DIM>(0.62200846792815,0.21132486540519));
+					mPoints.push_back(Point<ELEM_DIM>(0.04465819873852,0.78867513459481));
+					mPoints.push_back(Point<ELEM_DIM>(0.16666666666667,0.78867513459481));
 					break;
 					
-					case 3: 
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
-					mWeights.push_back(1);
+					case 3: // 2d, 3 points per dimension
+					mWeights.push_back(0.06846437766975);
+					mWeights.push_back(0.10954300427160);
+					mWeights.push_back(0.06846437766975);
+					mWeights.push_back(0.06172839506173);
+					mWeights.push_back(0.09876543209877);
+					mWeights.push_back(0.06172839506173);
+					mWeights.push_back(0.00869611615741);
+					mWeights.push_back(0.01391378585185);
+					mWeights.push_back(0.00869611615741);
 					
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
-					mPoints.push_back(Point<ELEM_DIM>(1));
+					mPoints.push_back(Point<ELEM_DIM>(0.10000000001607,0.11270166540000));
+					mPoints.push_back(Point<ELEM_DIM>(0.44364916730000,0.11270166540000));
+					mPoints.push_back(Point<ELEM_DIM>(0.78729833458393,0.11270166540000));
+					mPoints.push_back(Point<ELEM_DIM>(0.05635083270000,0.50000000000000));
+					mPoints.push_back(Point<ELEM_DIM>(0.25000000000000,0.50000000000000));
+					mPoints.push_back(Point<ELEM_DIM>(0.44364916730000,0.50000000000000));
+					mPoints.push_back(Point<ELEM_DIM>(0.01270166538393,0.88729833460000));
+					mPoints.push_back(Point<ELEM_DIM>(0.05635083270000,0.88729833460000));
+					mPoints.push_back(Point<ELEM_DIM>(0.10000000001607,0.88729833460000));
 					break;
 				}
 				
@@ -120,24 +132,36 @@ public:
 
 	}
 	
+	/**
+	 * Get a quadrature point.
+	 * 
+	 * @param index The index of the point to return.
+	 * @return A gaussian quadrature point.
+	 */
 	Point<ELEM_DIM> GetQuadPoint(int index) const
 	{
 		assert(index < mNumQuadPoints);
 		return mPoints[index];
 	}
 	
+	/**
+	 * Get the weight associated with a quadrature point.
+	 */
 	double GetWeight(int index) const
 	{
 		assert(index < mNumQuadPoints);
 		return mWeights[index];
 	}
 	
+	/**
+	 * Get the number of quadrature points. This is the number of points in 
+	 * each dimension, raised to the power of the number of dimensions.
+	 */
 	int GetNumQuadPoints() const
 	{
 		return mNumQuadPoints;
 	}
 	
-	// 
 	
 	/**
 	 * This method is only used by TestGaussianQuadratureRule at the moment.
--- pdes/tests/TestGaussianQuadratureRule.hpp
+++ pdes/tests/TestGaussianQuadratureRule.hpp
@@ -12,33 +12,61 @@ class TestGaussianQuadratureRule : public CxxTest::TestSuite
 {
 
 public :
+
+	/**
+	 * Check points and weights are in the right ranges
+	 */
 	void testGaussianQuadratureRule()
 	{
-		GaussianQuadratureRule<1> quadRule(3);
-		
-		for(int i=0; i<quadRule.GetNumQuadPoints(); i++)
+		// 1d
+		for (int nPoints=1; nPoints<4; nPoints++)
 		{
+			GaussianQuadratureRule<1> quadRule(nPoints);
 			
-			TS_ASSERT_LESS_THAN_EQUALS( quadRule.GetWeight(i),1);
-			TS_ASSERT_LESS_THAN_EQUALS(-quadRule.GetWeight(i),0);
-
-			TS_ASSERT_LESS_THAN( quadRule.GetQuadPoint(i)[0],1);
-			TS_ASSERT_LESS_THAN(-quadRule.GetQuadPoint(i)[0],0);
-		}
+			for(int i=0; i<quadRule.GetNumQuadPoints(); i++)
+			{
+				TS_ASSERT_LESS_THAN_EQUALS( quadRule.GetWeight(i),1);
+				TS_ASSERT_LESS_THAN_EQUALS(-quadRule.GetWeight(i),0);
+	
+				TS_ASSERT_LESS_THAN( quadRule.GetQuadPoint(i)[0],1); // x<1
+				TS_ASSERT_LESS_THAN(-quadRule.GetQuadPoint(i)[0],0); // x>0
+			}
+		}		
 		
-				
-		//TODO: 2d and 3d
+		// 2d
+		for (int nPoints=1; nPoints<4; nPoints++)
+		{
+			GaussianQuadratureRule<2> quadRule(nPoints);
+			
+			for(int i=0; i<quadRule.GetNumQuadPoints(); i++)
+			{
+				TS_ASSERT_LESS_THAN_EQUALS( quadRule.GetWeight(i),1);
+				TS_ASSERT_LESS_THAN_EQUALS(-quadRule.GetWeight(i),0);
+
+				TS_ASSERT_LESS_THAN(-(1-quadRule.GetQuadPoint(i)[0]
+									   -quadRule.GetQuadPoint(i)[1]),0); // 1-x-y>0
+				TS_ASSERT_LESS_THAN(-quadRule.GetQuadPoint(i)[0],0);  // x>0
+				TS_ASSERT_LESS_THAN(-quadRule.GetQuadPoint(i)[1],0);  // y>0
+			}
+		}
+
+		//TODO: 3d
 		
 		//TODO: test through integration
 	}
 	
+	/**
+	 * Test by integrating polynomials of the form x^n.
+	 * 
+	 * 1d case.
+	 */
 	void testGaussianQuadratureRuleIntegralOneD( void )
 	{
 		for (int num_quad_points=1; num_quad_points<4; num_quad_points++)
 		{
 			GaussianQuadratureRule<1> quad_rule(num_quad_points);
 
-			for (int poly_degree=0; poly_degree<1; poly_degree++)
+			for (int poly_degree=0; poly_degree<2*num_quad_points; poly_degree++)
 			{
 
 				PolyFunction function(poly_degree);
@@ -62,6 +90,55 @@ public :
 								1.0/(poly_degree+1.0)*(pow(3,poly_degree+1)-1),
 								1e-7);
 				
+			}
+		}
+			
+	}
+
+	/**
+	 * Test by integrating polynomials up to degree 2p-2, where p is the no. of
+	 * points in each dimension. This is the 2d case.
+	 * 
+	 * We integrate things like x, y, x^2, xy, y^2, ...
+	 */
+	void testGaussianQuadratureRuleIntegralTwoD( void )
+	{
+		// Expected answers [degree_x][degree_y]
+		double expected[5][5] = { {1.0/2,  1.0/6,  1.0/12, 1.0/20, 1.0/30},
+								  {1.0/6,  1.0/24, 1.0/60, 1.0/120, 0},
+								  {1.0/12, 1.0/60, 1.0/180, 0, 0},
+								  {1.0/20, 1.0/120, 0,      0, 0},
+								  {1.0/30, 0,       0,      0, 0} };
+		
+		for (int num_quad_points=1; num_quad_points<4; num_quad_points++)
+		{
+			GaussianQuadratureRule<2> quad_rule(num_quad_points);
+
+			for (int poly_degree_x=0; poly_degree_x<2*num_quad_points-1;
+				 poly_degree_x++)
+			{
+
+				for (int poly_degree_y=0;
+					 poly_degree_y<2*num_quad_points-1-poly_degree_x;
+					 poly_degree_y++)
+				{			
+					double integral = 0.0;
+					
+					for (int quad_index=0;
+						 quad_index<quad_rule.GetNumQuadPoints();
+						 quad_index++)
+					{
+						Point<2> quad_point=quad_rule.GetQuadPoint(quad_index);
+						
+						integral += pow(quad_point[0], poly_degree_x)
+									*pow(quad_point[1], poly_degree_y)
+									*quad_rule.GetWeight(quad_index);
+					}
+					
+					TS_ASSERT_DELTA(integral,
+									expected[poly_degree_x][poly_degree_y],
+									1e-7);
+				}
 			}
 		}
 			
