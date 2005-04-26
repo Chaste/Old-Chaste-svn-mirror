9fe6276a20eff0bca7fa9751b0d751b85468298f
Rutherford Appleton Labs <obselete@comlab.ox.ac.uk>
2005-04-26 12:57:37 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@267 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pdes/tests/TestMeshWriters.hpp
+++ pdes/tests/TestMeshWriters.hpp
@@ -11,7 +11,12 @@
 #include <cxxtest/TestSuite.h>
 #include "../MemfemMeshReader.hpp"
 #include "../FemlabMeshReader.hpp"
+#include "../TrianglesMeshReader.hpp"
 #include "../TrianglesMeshWriter.hpp"
+
+static AbstractMeshReader *spImportMeshReader;
+static AbstractMeshReader *spNewMeshReader;
+static TrianglesMeshWriter *spMeshWriter;
 
 class TestMeshWriters : public CxxTest::TestSuite
 {
@@ -20,12 +25,60 @@ class TestMeshWriters : public CxxTest::TestSuite
 	
 	void testMemfemtoTetgen(void)
 	{	
-		TS_ASSERT(1==1);
+		spImportMeshReader=new MemfemMeshReader(
+							"pdes/tests/meshdata/Memfem_slab");
+		spMeshWriter=new TrianglesMeshWriter(
+							"/tmp/MeshFromMemfem",3);
+	
+		int i;
+		for (i=0; i<spImportMeshReader->GetNumNodes();i++)
+		{
+			spMeshWriter->SetNextNode(spImportMeshReader->GetNextNode());
+		}
+		for (i=0; i<spImportMeshReader->GetNumElements();i++)
+		{
+			spMeshWriter->SetNextElement(spImportMeshReader->GetNextElement());
+		}
+		for (i=0; i<spImportMeshReader->GetNumBoundaryFaces();i++)
+		{
+			spMeshWriter->SetNextBoundaryFace(spImportMeshReader->GetNextBoundaryFace());
+		}
+		
+		spMeshWriter->WriteFiles();
+		
+		TS_ASSERT_THROWS_ANYTHING(spNewMeshReader = 
+								new TrianglesMeshReader("/tmp/MeshFromMemfem"));
+	
 	}
 
 	void testFemlabtoTriangles(void)
 	{	
-		TS_ASSERT(1==1);
+		spImportMeshReader=new FemlabMeshReader(
+							"pdes/tests/meshdata/",
+		                  	"femlab_lshape_nodes.dat",
+		                  	"femlab_lshape_elements.dat",
+		                  	"femlab_lshape_edges.dat");
+		spMeshWriter=new TrianglesMeshWriter(
+							"/tmp/MeshFromFemlab",2);
+		int i;
+		for (i=0; i<spImportMeshReader->GetNumNodes();i++)
+		{
+			spMeshWriter->SetNextNode(spImportMeshReader->GetNextNode());
+		}
+		for (i=0; i<spImportMeshReader->GetNumElements();i++)
+		{
+			spMeshWriter->SetNextElement(spImportMeshReader->GetNextElement());
+		}
+		for (i=0; i<spImportMeshReader->GetNumBoundaryFaces();i++)
+		{
+			spMeshWriter->SetNextBoundaryFace(spImportMeshReader->GetNextBoundaryFace());
+		}
+		
+		spMeshWriter->WriteFiles();
+
+
+		TS_ASSERT_THROWS_ANYTHING(spNewMeshReader = 
+							new TrianglesMeshReader("/tmp/MeshFromFemlab"));
 	}
 	
 };
