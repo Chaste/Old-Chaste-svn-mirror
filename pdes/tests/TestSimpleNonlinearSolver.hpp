b2643c4c111421e6fbcc6cfa6d99c4c1e7eed040
Old User <obselete@comlab.ox.ac.uk>
2005-04-27 13:46:19 +0000

    New Nonlinear solver test added
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@312 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pdes/tests/TestSimpleNonlinearSolver.hpp
+++ pdes/tests/TestSimpleNonlinearSolver.hpp
@@ -6,9 +6,12 @@
 #include "petscvec.h"
 #include "petscmat.h"
 #include <iostream>
+#include <math.h>
   
 PetscErrorCode ComputeTestResidual(SNES snes,Vec solutionGuess,Vec residual,void *pContext);  
 PetscErrorCode ComputeTestJacobian(SNES snes,Vec input,Mat *pJacobian ,Mat *pPreconditioner,MatStructure *pMatStructure ,void *pContext);
+PetscErrorCode ComputeTestResidual3d(SNES snes,Vec solutionGuess,Vec residual,void *pContext);  
+PetscErrorCode ComputeTestJacobian3d(SNES snes,Vec input,Mat *pJacobian ,Mat *pPreconditioner,MatStructure *pMatStructure ,void *pContext);
   
 class TestSimpleNonlinearSolver : public CxxTest::TestSuite 
 {
@@ -53,9 +56,47 @@ public:
 		double y = answerElements[1];
 		VecRestoreArray(answer,&answerElements);
     	
-    	double tol = 1e-5;
-    	TS_ASSERT_DELTA(x,0.707107,tol);
-    	TS_ASSERT_DELTA(y,0.707107,tol);
+    	double tol = 1e-6;
+    	TS_ASSERT_DELTA(x,1/sqrt(2),tol);
+    	TS_ASSERT_DELTA(y,1/sqrt(2),tol);
+    }
+    
+    void testOn3dNonlinearProblem(void)
+    {
+    	SimpleNonlinearSolver *solver=new SimpleNonlinearSolver();
+    	
+    	// Set up solution guess for residuals
+    	int length=3;
+		    	
+    	// Set up initial Guess
+    	Vec initialGuess;
+    	VecCreate(PETSC_COMM_WORLD, &initialGuess);
+    	VecSetSizes(initialGuess, PETSC_DECIDE,length);
+    	VecSetType(initialGuess, VECSEQ);
+    	VecSetValue(initialGuess, 0, 1.0 ,INSERT_VALUES);
+		VecSetValue(initialGuess, 1, 1.0 ,INSERT_VALUES);
+		VecSetValue(initialGuess, 2, 1.0 ,INSERT_VALUES);
+    	VecAssemblyBegin(initialGuess);
+		VecAssemblyEnd(initialGuess);
+		
+    	Vec answer;
+    	Vec residual;
+    	VecDuplicate(initialGuess,&residual);
+    	VecDuplicate(initialGuess,&answer);
+    	    	
+ 		answer=solver->Solve(&ComputeTestResidual3d, &ComputeTestJacobian3d, residual, initialGuess, NULL);
+    	
+    	PetscScalar *answerElements;
+		VecGetArray(answer, &answerElements);
+		double x = answerElements[0];
+		double y = answerElements[1];
+		double z = answerElements[2];
+		VecRestoreArray(answer,&answerElements);
+    	
+    	double tol = 1e-6;
+    	TS_ASSERT_DELTA(x,1/sqrt(3),tol);
+    	TS_ASSERT_DELTA(y,1/sqrt(3),tol);
+    	TS_ASSERT_DELTA(z,1/sqrt(3),tol);
     }
     
 };
@@ -91,6 +132,51 @@ PetscErrorCode ComputeTestJacobian(SNES snes,Vec input,Mat *pJacobian ,Mat *pPre
 	MatSetValue(*pJacobian, 0 , 1 , 2.0*y, INSERT_VALUES);
 	MatSetValue(*pJacobian, 1 , 0 , 1.0, INSERT_VALUES);
 	MatSetValue(*pJacobian, 1 , 1 , -1.0, INSERT_VALUES);
+    MatAssemblyBegin(*pJacobian,MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(*pJacobian,MAT_FINAL_ASSEMBLY);
+	
+	return 0;
+}
+
+PetscErrorCode ComputeTestResidual3d(SNES snes,Vec solutionGuess,Vec residual,void *pContext)
+{
+	double x,y,z;
+	
+	PetscScalar *solutionGuessElements;
+	VecGetArray(solutionGuess, &solutionGuessElements);
+	x = solutionGuessElements[0];
+	y = solutionGuessElements[1];
+	z = solutionGuessElements[2];
+	
+	VecRestoreArray(solutionGuess,&solutionGuessElements);
+	VecSetValue(residual,0,x*x+y*y+z*z-1,INSERT_VALUES);
+	VecSetValue(residual,1,x-y,INSERT_VALUES);
+	VecSetValue(residual,2,y-z,INSERT_VALUES);
+	VecAssemblyBegin(residual);
+	VecAssemblyEnd(residual);
+	return 0;
+}
+
+PetscErrorCode ComputeTestJacobian3d(SNES snes,Vec input,Mat *pJacobian ,Mat *pPreconditioner,MatStructure *pMatStructure ,void *pContext)
+{
+	double x, y, z;
+		
+	PetscScalar *inputElements;
+	VecGetArray(input, &inputElements);
+	x = inputElements[0];
+	y = inputElements[1];
+	z = inputElements[2];
+	VecRestoreArray(input,&inputElements);
+		
+	MatSetValue(*pJacobian, 0 , 0 , 2.0*x , INSERT_VALUES);
+	MatSetValue(*pJacobian, 0 , 1 , 2.0*y, INSERT_VALUES);
+	MatSetValue(*pJacobian, 0 , 2 , 2.0*z, INSERT_VALUES);
+	MatSetValue(*pJacobian, 1 , 0 , 1.0, INSERT_VALUES);
+	MatSetValue(*pJacobian, 1 , 1 , -1.0, INSERT_VALUES);
+	MatSetValue(*pJacobian, 1 , 2 , 0.0, INSERT_VALUES);
+	MatSetValue(*pJacobian, 2 , 0 , 0.0, INSERT_VALUES);
+	MatSetValue(*pJacobian, 2 , 1 , 1.0, INSERT_VALUES);
+	MatSetValue(*pJacobian, 2 , 2 , -1.0, INSERT_VALUES);
     MatAssemblyBegin(*pJacobian,MAT_FINAL_ASSEMBLY);
     MatAssemblyEnd(*pJacobian,MAT_FINAL_ASSEMBLY);
 	
