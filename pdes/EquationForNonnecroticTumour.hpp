83b624f757bfd4b5d4702a25442e52ef6fabd88c
Old User <obselete@comlab.ox.ac.uk>
2005-04-27 16:53:00 +0000

    Build equation for non-necrotic tumour concentration.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@331 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ pdes/EquationForNonnecroticTumour.hpp
@@ -0,0 +1,35 @@
+#ifndef _EQUATIONFORNONNECROTICTUMOUR_HPP_
+#define _EQUATIONFORNONNECROTICTUMOUR_HPP_
+
+#include "AbstractNonlinearEllipticPde.hpp"
+/*
+ * this function is written for testing Practical 1 
+ * 
+ * 
+ */
+
+template <int SPACE_DIM>
+class EquationForNonnecroticTumour : public AbstractNonlinearEllipticPde<SPACE_DIM>
+{
+public:
+
+    double ComputeLinearSourceTerm(Point<SPACE_DIM> x)
+    {
+        return 0.0;
+    }
+    
+    double ComputeNonlinearSourceTerm(Point<SPACE_DIM> x, double u)
+    {
+        const double sigma_0 = 1.0;
+        const double sigma_1 = 1.0;
+        
+        return sigma_0*u/(1+sigma_1*u);
+    }
+
+    MatrixDouble ComputeDiffusionTerm(Point<SPACE_DIM> x, double u)
+    {        
+        return MatrixDouble::Identity(SPACE_DIM) * 1.0;
+    }
+
+};
+#endif //_EQUATIONFORNONNECROTICTUMOUR_HPP_
