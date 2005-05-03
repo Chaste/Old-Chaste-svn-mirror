4c2f9b9c7110190a91bb7242d03df36a0629e747
Old User <obselete@comlab.ox.ac.uk>
2005-05-03 16:20:59 +0000

    Refactored TestQuadraticBasisFunction
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@434 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pdes/tests/TestQuadraticBasisFunction.hpp
+++ pdes/tests/TestQuadraticBasisFunction.hpp
@@ -5,7 +5,9 @@
 #include "QuadraticBasisFunction.cpp"
 #include "GaussianQuadratureRule.hpp"
 #include "VectorDouble.hpp"
+#include "BasisFunctionsCheckers.hpp"
 #include <vector>
+
 
 class TestQuadraticBasisFunction : public CxxTest::TestSuite 
 {
@@ -18,35 +20,7 @@ class TestQuadraticBasisFunction : public CxxTest::TestSuite
 		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 0), 1.0, 1e-12);
 	}
 	
-	void checkBasisFunctions1d(AbstractBasisFunction<1>* pBasisFunc,
-							   std::vector<Point<1>*> evaluationPoints)
-	{
-		int size = evaluationPoints.size();		// number of evalutation points and basis functions too
-		std::vector<double> basis_function_vector; // store results of evalutation
-		double expected_evaluation;
-		
-		for (int point_index=0; point_index<size; point_index++)
-		{
-			std::vector<double> basis_function_vector = pBasisFunc->ComputeBasisFunctions(*(evaluationPoints[point_index]));
-			for (int func_index=0; func_index<size; func_index ++)
-			{
-				if (func_index==point_index)
-				{
-					expected_evaluation=1.0;
-				}
-				else
-				{
-					expected_evaluation = 0.0;
-				}
-				TS_ASSERT_DELTA(basis_function_vector[func_index],
-								expected_evaluation,
-								1e-12);
-				TS_ASSERT_DELTA(pBasisFunc->ComputeBasisFunction(*(evaluationPoints[point_index]),func_index),
-								expected_evaluation,
-								1e12);
-			}
-		}
-	}
+	
 		
 	
 	void testQuadraticBasisFunction1d()
@@ -55,24 +29,15 @@ class TestQuadraticBasisFunction : public CxxTest::TestSuite
 		Point<1> zero(0);
 		Point<1> one(1);
 		Point<1> half(0.5);
-		evaluation_points.push_back(new Point<1>(0));
-		evaluation_points.push_back(new Point<1>(0.5));
-		evaluation_points.push_back(new Point<1>(1));
+		evaluation_points.push_back(&zero);
+		evaluation_points.push_back(&one);
+		evaluation_points.push_back(&half);
 		
 		QuadraticBasisFunction<1> basis_func;
 		
-		// checkBasisFunctions1d(&basis_func, evaluation_points);
+		BasisFunctionsCheckers<1> checker;
+		checker.checkBasisFunctions(&basis_func, evaluation_points);
 		
-		// Single compute
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 0), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(half,  0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(half,  1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(half,  2), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(one,  0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(one,  1), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(one,  2), 0.0, 1e-12);
 		
 		// Mass compute
 		std::vector<double> basis_function_vector;
@@ -123,62 +88,19 @@ class TestQuadraticBasisFunction : public CxxTest::TestSuite
 		Point<2> zerohalf(0,0.5);
 		Point<2> halfhalf(0.5,0.5);
 		
+		std::vector<Point<2>*> evaluation_points;
+		evaluation_points.push_back(&zero);
+		evaluation_points.push_back(&onezero);
+		evaluation_points.push_back(&zeroone);
+		evaluation_points.push_back(&halfzero);
+		evaluation_points.push_back(&zerohalf);
+		evaluation_points.push_back(&halfhalf);
+		
 		QuadraticBasisFunction<2> basis_func;
 		
-		// Single compute
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 0), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 5), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezero, 1), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezero, 5), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroone, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroone, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroone, 2), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroone, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroone, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroone, 5), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzero, 3), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzero, 5), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalf, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalf, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalf, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalf, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalf, 4), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalf, 5), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalf, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalf, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalf, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalf, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalf, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalf, 5), 1.0, 1e-12);
-		
-		// Mass compute
-		std::vector<double> basis_function_vector;
-		basis_function_vector = basis_func.ComputeBasisFunctions(zero);
-		TS_ASSERT_EQUALS(basis_function_vector.size(), 6);
-		TS_ASSERT_DELTA(basis_function_vector[0], 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[1], 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[2], 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[3], 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[4], 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[5], 0.0, 1e-12);
-		
+		BasisFunctionsCheckers<2> checker;
+		checker.checkBasisFunctions(&basis_func, evaluation_points);
+				
 		// Derivatives
 		std::vector<VectorDouble> derivatives;
 		derivatives = basis_func.ComputeBasisFunctionDerivatives(onezero);
@@ -227,7 +149,6 @@ class TestQuadraticBasisFunction : public CxxTest::TestSuite
 		TS_ASSERT_DELTA(derivatives[5](1), 0, 1e-12);
 	}
 
-
 	void testQuadraticBasisFunction3d()
 	{
 		Point<3> zero(0,0,0);
@@ -241,128 +162,23 @@ class TestQuadraticBasisFunction : public CxxTest::TestSuite
 		Point<3> zerohalfzero(0.0,0.5,0.0);
 		Point<3> zerozerohalf(0.0,0.0,0.5);
 		
+		std::vector<Point<3>*> evaluation_points;
+		evaluation_points.push_back(&zero);
+		evaluation_points.push_back(&onezerozero);
+		evaluation_points.push_back(&zeroonezero);
+		evaluation_points.push_back(&zerozeroone);
+		evaluation_points.push_back(&halfzerozero);
+		evaluation_points.push_back(&zerohalfzero);
+		evaluation_points.push_back(&zerozerohalf);
+		evaluation_points.push_back(&halfhalfzero);
+		evaluation_points.push_back(&halfzerohalf);
+		evaluation_points.push_back(&zerohalfhalf);
+
 		QuadraticBasisFunction<3> basis_func;
 		
-		// Single compute
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 0), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zero, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 1), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(onezerozero, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 2), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zeroonezero, 9), 0.0, 1e-12);
+		BasisFunctionsCheckers<3> checker;
+		checker.checkBasisFunctions(&basis_func, evaluation_points);
 
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 3), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozeroone, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 7), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfhalfzero, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 4), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerozero, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 8), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(halfzerohalf, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfhalf, 9), 1.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 5), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 6), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerohalfzero, 9), 0.0, 1e-12);
-		
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 0), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 1), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 2), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 3), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 4), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 5), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 6), 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 7), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 8), 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_func.ComputeBasisFunction(zerozerohalf, 9), 0.0, 1e-12);		
-		
-		// Mass compute
-		std::vector<double> basis_function_vector;
-		basis_function_vector = basis_func.ComputeBasisFunctions(zero);
-		TS_ASSERT_EQUALS(basis_function_vector.size(), 10);
-		TS_ASSERT_DELTA(basis_function_vector[0], 1.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[1], 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[2], 0.0, 1e-12);
-		TS_ASSERT_DELTA(basis_function_vector[3], 0.0, 1e-12);
-		
 		// Derivatives
 		std::vector<VectorDouble> derivatives;
 		derivatives = basis_func.ComputeBasisFunctionDerivatives(onezerozero);
