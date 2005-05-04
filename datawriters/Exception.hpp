31f1a5b3cd936e7df191d6448b7764027b66069b
Old User <obselete@comlab.ox.ac.uk>
2005-05-04 08:32:14 +0000

    Tidying.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@452 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- datawriters/Exception.hpp
+++ datawriters/Exception.hpp
@@ -1,23 +1,26 @@
-#ifndef EXCEPTION_HPP
-#define EXCEPTION_HPP
+#ifndef _EXCEPTION_HPP_
+#define _EXCEPTION_HPP_
 
 #include <string>
 
 /**
- * Exception class
- *
+ * Exception class.
+ * All exceptions thrown by this code are currently instances of this class.
+ * 
+ * \todo Might we want this class to inherit from STL exceptions?
  */
 class Exception
 {
 private:
     std::string mMessage; /**< Exception message */
-
+ 
 public:
     /** Construct an exception with a message string */
     Exception(std::string message):mMessage(message)
-    {
+    {   
       //nothing to do here
     }
+    
     /** Get the message associated with the exception 
      *
      * @return The message set when the exception was thrown.
@@ -26,7 +29,6 @@ public:
     {
         return mMessage;
     }
-
 };
 
-#endif
+#endif // _EXCEPTION_HPP_
