3e6913253d046406c0b5cdfe5d0f4df4a68c8627
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2005-06-16 10:35:13 +0000

    Add skeleton BuildTypes python module.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@553 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ BuildTypes.py
@@ -0,0 +1,8 @@
+# Chaste Build System Scripts
+
+# This module is designed to be imported by both the build scripts and the
+# web interface to test results. Given a name representing a build type
+# (a valid value of the build argument to scons) it determines what compile
+# tools & flags to use, and also how to interpret the status string of a test
+# suite.
+
