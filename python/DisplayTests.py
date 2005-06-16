a45081c37eb895dc77a7695a5143cb43136b9b98
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2005-06-16 16:56:50 +0000

    Bug fixing.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@591 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/DisplayTests.py
+++ python/DisplayTests.py
@@ -261,7 +261,7 @@ def _importBuildTypesModule(revision=pysvn.Revision(pysvn.opt_revision_kind.head
   module from the repository.
   By default import the latest version.
   """
-  if not isinstance(revision, pysvn.Revision):
+  if type(revision) == type(""):
     revision = pysvn.Revision(pysvn.opt_revision_kind.number, int(revision))
   return _importModuleFromSvn('BuildTypes', '/python/BuildTypes.py', revision)
 
