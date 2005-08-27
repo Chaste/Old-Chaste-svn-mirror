9bd2e3810239c1f506760d878c926cef5399cc6b
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2005-08-27 06:18:17 +0000

    Update infrastructure scripts.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@685 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- infrastucture-scripts/nightly-build.sh
+++ infrastucture-scripts/nightly-build.sh
@@ -8,19 +8,24 @@
 # The integration machine
 machine='userpc60.comlab.ox.ac.uk'
 # Base path for where test results will appear
-remote_base='eclipse/workspace/trunk/testoutput/'
+remote_base='testresults/'
 # Base path where test results should be put
 local_base='/var/www/chaste_test_data/nightly/'
 
 # Latest revision
 rev=`svnlook youngest /var/svn/chaste`
 
+# Create output dir
+if [ ! -d $local_base$rev ]; then
+	mkdir $local_base$rev
+fi
+
 do_build ()
 {
 	# Run the build, discarding output
-	ssh bob@$machine ./builder $rev $1 >/dev/null
+	ssh bob@$machine ./builder $rev $1 2>&1 >/dev/null
 	# Copy results
-	scp -r bob@$machine:$path_base$machine.$1 $local_base$rev/$machine.$1
+	scp -r bob@$machine:$remote_base$machine.$1 $local_base$rev/$machine.$1
 }
 
 if [ -z "$1" ]; then
