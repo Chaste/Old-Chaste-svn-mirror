c447cbbd7156fd8bfb0d1f3f7d3a87525995513d
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2005-09-14 14:45:05 +0000

    Start refactoring tests of single cell ionic models into a single test suite.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@708 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- ode/test/TestIonicModels.hpp
+++ ode/test/TestIonicModels.hpp
@@ -1,24 +1,30 @@
-#ifndef _TESTODESOLVERFORHH52_HPP_
-#define _TESTODESOLVERFORHH52_HPP_
+#ifndef _TESTIONICMODELS_HPP_
+#define _TESTIONICMODELS_HPP_
 
 #include <cxxtest/TestSuite.h>
 #include <cmath>
 #include <iostream>
 #include <fstream>
 #include <vector>
+
 #include "AbstractStimulusFunction.hpp"
 #include "InitialStimulus.hpp"
 #include "RegularStimulus.hpp"
+
 #include "EulerIvpOdeSolver.hpp"
 #include "RungeKutta2IvpOdeSolver.hpp"
 #include "RungeKutta4IvpOdeSolver.hpp"
 #include "AdamsBashforthIvpOdeSolver.hpp"
+
 #include "OdeSolution.hpp"
+
 #include "ColumnDataWriter.hpp"
 #include "ColumnDataReader.hpp"
-#include "HodgkinHuxleySquidAxon1952OriginalOdeSystem.hpp"
 
-const double TOLERANCE = 1e-2;
+#include "HodgkinHuxleySquidAxon1952OriginalOdeSystem.hpp"
+#include "FitzHughNagumo1961OdeSystem.hpp"
+
+const double TOLERANCE = 1e-2; // Not used at present
 
 
 class TestOdeSolverForHH52 : public CxxTest::TestSuite
@@ -60,7 +66,7 @@ class TestOdeSolverForHH52 : public CxxTest::TestSuite
         initial_conditions.push_back(m);        
 
         /*
-         * Instantiate the Luo-Rudy model: need to pass initial data and stimulus function
+         * Instantiate the ionic model: need to pass stimulus function
          */        
         HodgkinHuxleySquidAxon1952OriginalOdeSystem hh52_ode_system(pStimulus);
         
@@ -79,7 +85,7 @@ class TestOdeSolverForHH52 : public CxxTest::TestSuite
         
               
         /*
-         * Write data to a file NewLR91.dat using ColumnDataWriter
+         * Write data to a file using ColumnDataWriter
          */                                                           
         ColumnDataWriter *p_writer;
         p_writer = new ColumnDataWriter("testoutput",pFilename);
@@ -161,10 +167,114 @@ class TestOdeSolverForHH52 : public CxxTest::TestSuite
          */            
         runOdeSolverForHH52(&stimulus, "HH52RegResult.dat", 150.0, 0.01);
              
-    }   
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    void runOdeSolverForFHN61(AbstractStimulusFunction *pStimulus,
+                              const char *pFilename,
+                              double endTime,
+                              double timeStep)
+    {
+        /*
+         * Set initial conditions
+         */
+        double voltage = 0.0; // initial resting potential
+        double w = 0.0;       // initial value for recovery variable
+        
+        /*
+         * Collect initial data in a vector
+         * 
+         */  
+        std::vector<double> initial_conditions;
+        initial_conditions.push_back(voltage);
+        initial_conditions.push_back(w);
+        
+        /*
+         * Instantiate the FHN model: need to pass stimulus function
+         */        
+        FitzHughNagumo1961OdeSystem fhn61_ode_system(pStimulus);
+        
+        /*
+         * Choose an ode solver
+         */      
+        EulerIvpOdeSolver solver;
+        
+        /*
+         * Solve
+         */
+        double start_time = 0.0;
+        OdeSolution solution = solver.Solve(&fhn61_ode_system,
+                                            start_time,
+                                            endTime,
+                                            timeStep,
+                                            initial_conditions);  
+         
+        /*
+         * Write data to a file FHN61.dat using ColumnDataWriter
+         */                                                           
+        ColumnDataWriter writer("testoutput", pFilename);
+        int new_time_var_id = writer.DefineFixedDimension("Time", "ms",
+                                                          solution.mSolutions.size());
+        int new_v_var_id = writer.DefineVariable("V","mV");
+        int new_w_var_id = writer.DefineVariable("w","");
+        writer.EndDefineMode();
+                
+        for (int i = 0; i < solution.mSolutions.size(); i++) 
+        {
+            writer.PutVariable(new_time_var_id, solution.mTime[i], i);
+            writer.PutVariable(new_v_var_id, solution.mSolutions[i][0], i);
+            writer.PutVariable(new_w_var_id, solution.mSolutions[i][1], i);            
+        }
+        writer.Close();                            
+    }
+    
+    // Test Ode Solver for FHN61
+    void testOdeSolverForFHN61WithInitialStimulus(void)
+    {
+        /*
+         * Choose function for stimulus
+         */             
+        double magnitude = 1.0;  
+        double duration  = 0.5;  // ms                   
+        InitialStimulus stimulus(magnitude, duration); 
+        
+        /*
+         * Solve 
+         */
+        runOdeSolverForFHN61(&stimulus, "FHN61Result.dat", 500.0, 0.01);
+    }
+    
+    void testOdeSolverForFHN61WithRegularStimulus(void)
+    {
+        /*
+         * Choose function for stimulus
+         */             
+        double magnitude = 1.0;  
+        double duration  = 0.5 ;  // ms                     
+        double frequency = 1.0/500.0;
+        double start_stimulus = 40.0;                 
+        RegularStimulus stimulus(magnitude, duration, frequency, start_stimulus); 
 
+        /*
+         * Solve 
+         */
+        runOdeSolverForFHN61(&stimulus, "FHN61RegResult.dat", 500.0, 0.01);
+    }
 };
 
 
 
-#endif //_TESTODESOLVERFORHH52_HPP_
+#endif //_TESTIONICMODELS_HPP_
