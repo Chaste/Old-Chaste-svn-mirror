87dfdac3ebbcfd5ad8538144c68a7f1e5fa2f8af
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2005-10-06 11:17:25 +0000

    Wrote some tests for PropagationPropertiesCalculator and moved it to coupled.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@739 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ coupled/test/TestPropagationPropertiesCalculator.hpp
@@ -0,0 +1,32 @@
+#ifndef _TESTPROPAGATIONPROPERTIESCALCULATOR_HPP_
+#define _TESTPROPAGATIONPROPERTIESCALCULATOR_HPP_
+
+
+#include <cxxtest/TestSuite.h>
+
+#include <iostream>
+
+#include "PropagationPropertiesCalculator.hpp"
+#include "ColumnDataReader.hpp"
+
+class TestPropagationPropertiesCalculator : public CxxTest::TestSuite
+{
+public:
+
+    void TestConductionVelocity1D(void)
+    {
+        ColumnDataReader simulation_data("coupled/test/data/MonoDg01d",
+                                         "NewMonodomainLR91_1d");
+        PropagationPropertiesCalculator ppc(&simulation_data);
+        double velocity=ppc.CalculateConductionVelocity(5,15,0.1);
+        TS_ASSERT_DELTA(velocity, 0.0961538, 0.001);
+        
+        // Should throw because AP does not propagate far enough in simulation time
+        TS_ASSERT_THROWS_ANYTHING(ppc.CalculateConductionVelocity(5,95,0.9));
+        
+        // Should throw because AP does not propagate far enough in simulation time
+        TS_ASSERT_THROWS_ANYTHING(ppc.CalculateConductionVelocity(90,100,0.1));
+    }
+};
+
+#endif //_TESTPROPAGATIONPROPERTIESCALCULATOR_HPP_
