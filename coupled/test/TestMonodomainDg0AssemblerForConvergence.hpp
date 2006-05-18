a1afeef97353c7a7b09cbd9c39dd5b0b1058becd
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2006-05-18 10:54:39 +0000

    This is a quick commit - I hope to get us out of trouble...
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@993 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- coupled/test/TestMonodomainDg0AssemblerForConvergence.hpp
+++ coupled/test/TestMonodomainDg0AssemblerForConvergence.hpp
@@ -58,9 +58,49 @@ class TestMonodomainDg0AssemblerForConvergence : public CxxTest::TestSuite
 {
 public:
 
+    void WriteTemporaryMeshFiles(std::string meshFilename, int middleNode)
+    {
+            int nb_of_eles = 2*middleNode;
+            int nb_of_nodes = nb_of_eles+1;
+
+            // Nodes file
+
+            std::ofstream node_file((meshFilename+".node").c_str());
+            node_file << std::scientific;
+
+            node_file << nb_of_nodes << "\t1\t0\t1" << std::endl;
+            
+            for (int i = 0; i < nb_of_nodes; i++)
+            {
+                int b = 0;
+                if ((i == 0) || (i == nb_of_nodes-1))
+                {
+                    b = 1;
+                }
+
+                node_file << i << "\t" << 0.1*i/nb_of_eles << "\t" << b << std::endl;
+            }
+
+            node_file.close();
+
+            // Elements file
+
+            std::ofstream ele_file((meshFilename+".ele").c_str());
+
+            ele_file << std::scientific;
+
+            ele_file << nb_of_eles << "\t2\t0" << std::endl;
+            
+            for (int i = 0; i < nb_of_eles; i++)
+            {
+                ele_file << i << "\t" << i << "\t" << i+1 << std::endl;
+            }
+
+            ele_file.close();
+        }
     // Solve on a 1D string of cells, 1mm long with a space step of 0.1mm and a
     // time step that starts at 0.01ms and gets halved until we get convergence.
-    void TestMonodomainDg01D()
+    void xTestMonodomainDg01D()
     {
         bool converging = false;
         double prev_voltage = -999;   // To ensure that the first test fails
@@ -159,44 +199,7 @@ public:
 
         do
         {
-            int nb_of_eles = 2*middle_node;
-            int nb_of_nodes = nb_of_eles+1;
-
-            // Nodes file
-
-            std::ofstream node_file((mesh_filename+".node").c_str());
-    
-            node_file << std::scientific;
-
-            node_file << nb_of_nodes << "\t1\t0\t1" << std::endl;
-            
-            for (int i = 0; i < nb_of_nodes; i++)
-            {
-                int b = 0;
-                if ((i == 0) || (i == nb_of_nodes-1))
-                {
-                    b = 1;
-                }
-
-                node_file << i << "\t" << 0.1*i/nb_of_eles << "\t" << b << std::endl;
-            }
-
-            node_file.close();
-
-            // Elements file
-
-            std::ofstream ele_file((mesh_filename+".ele").c_str());
-
-            ele_file << std::scientific;
-
-            ele_file << nb_of_eles << "\t2\t0" << std::endl;
-            
-            for (int i = 0; i < nb_of_eles; i++)
-            {
-                ele_file << i << "\t" << i << "\t" << i+1 << std::endl;
-            }
-
-            ele_file.close();
+            WriteTemporaryMeshFiles(mesh_filename, middle_node);
 
             // Solve the problem itself
            
@@ -281,6 +284,82 @@ public:
         
         TS_ASSERT_DELTA(space_step, 0.0025, 0.0);
     }
+    
+    void TestMonodomainDg01DSpaceAndTime()
+    {
+       const std::string mesh_filename = "testoutput/1D_0_to_1mm";
+        
+       //Invariant: middle_node*space_step = 0.05
+       double space_step=0.01;
+       //Loop over the space step
+       for (int middle_node=5; middle_node<100; middle_node*=2){
+            space_step=0.05/middle_node;
+            
+            bool converging = false;
+            double prev_voltage = -999;   // To ensure that the first test fails
+            double time_step = 0.01;   // ms
+    
+            WriteTemporaryMeshFiles(mesh_filename, middle_node);
+
+            do
+            {
+                PointStimulusCellFactory cell_factory(time_step);
+                MonodomainProblem<1> monodomain_problem(&cell_factory);
+        
+                monodomain_problem.SetMeshFilename(mesh_filename);
+                monodomain_problem.SetEndTime(200);   // 200 ms
+                monodomain_problem.SetEndTime(10);   // 10 ms  < --- Delete this!
+                
+                monodomain_problem.SetOutputDirectory("testoutput/MonoDg01D");
+                monodomain_problem.SetOutputFilenamePrefix("NewMonodomainLR91_1d");
+                monodomain_problem.SetPdeTimeStep(time_step);
+                monodomain_problem.Initialise();
+                std::cout<<"Solving with space_step="<<space_step<<" time_step="<<time_step<<"\n";
+                monodomain_problem.Solve();
+        
+                double* p_voltage_array;
+                int lo, hi;
+                monodomain_problem.GetVoltageArray(&p_voltage_array, lo, hi); 
+        
+        
+                double my_voltage = 0.0;
+                if ((lo <= middle_node) && (middle_node < hi))
+                {
+                    int local_index = middle_node-lo;
+                    my_voltage = p_voltage_array[local_index];
+                }
+                double probe_voltage;
+                MPI_Allreduce(&my_voltage, &probe_voltage, 1, MPI_DOUBLE, MPI_SUM, PETSC_COMM_WORLD);
+    
+                double relerr = fabs ((probe_voltage - prev_voltage) / prev_voltage);
+                std::cout<<"probe_voltage="<<probe_voltage<<" prev_voltage="<<prev_voltage
+                  <<" relerr="<<relerr<<"\n";
+                      
+                if (relerr < 1e-2)
+                {
+                    converging = true;
+                    //std::cout << "Converged at dt = " << monodomain_problem.time_step << "ms" << std::endl;
+                }
+                else
+                {
+                    // Get ready for the next test by halving the time step
+        
+                    time_step *= 0.5;
+                }
+    
+                prev_voltage = probe_voltage;
+        
+                monodomain_problem.RestoreVoltageArray(&p_voltage_array);
+              } while(!converging);    //do while: time_step   
+              
+              std::cout<<"Converged\n";   
+        }//for: space_step
+        
+
+    }    
+    
+    
+    
     
 };
 
