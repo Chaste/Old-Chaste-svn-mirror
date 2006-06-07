f7344589f97e7d57e7604c15e193cf621ccf03c0
James Osborne <jameso@comlab.ox.ac.uk>
2006-06-07 16:18:08 +0000

    Added TestBidomainHeart
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1045 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ coupled/test/TestBidomainHeart.hpp
@@ -0,0 +1,88 @@
+#ifndef _TESTBIDOMAINHEART_HPP_
+#define _TESTBIDOMAINHEART_HPP_
+
+#include "Element.hpp"
+#include <cxxtest/TestSuite.h>
+#include "petscvec.h"
+#include <vector>
+//#include <iostream>
+
+#include "ConformingTetrahedralMesh.cpp"
+#include "PetscSetupAndFinalize.hpp"
+#include "BidomainPde.hpp"
+#include "BidomainProblem.hpp"
+#include "AbstractCardiacCellFactory.hpp"
+
+#include "LuoRudyIModel1991OdeSystem.hpp"
+
+class PointStimulusHeartCellFactory : public AbstractCardiacCellFactory<3>
+{
+private:
+    InitialStimulus *mpStimulus;
+public:
+    PointStimulusHeartCellFactory(double timeStep) : AbstractCardiacCellFactory<3>(timeStep)
+    {
+        mpStimulus = new InitialStimulus(-3000.0*1000, 0.5);
+    }
+    
+    AbstractCardiacCell* CreateCardiacCellForNode(int node)
+    {
+        return new LuoRudyIModel1991OdeSystem(mpSolver, mTimeStep, mpZeroStimulus, mpZeroStimulus);
+    }
+    
+    void FinaliseCellCreation(std::vector<AbstractCardiacCell* >* pCellsDistributed, int lo, int hi)
+    {
+        int stimulated_cells[] = {  37484-1,        
+                                    37499-1, 
+                                    37777-1, 
+                                    37779-1, 
+                                    38008-1, 
+                                    38332-1, 
+                                    38587-1, 
+                                    38588-1, 
+                                    39312-1, 
+                                    39314-1, 
+                                    39643-1, 
+                                    40588-1, 
+                                    40590-1, 
+                                    63885-1 
+                                 };
+
+        for(int i=0; i<14; i++)
+        {
+            int global_index = stimulated_cells[i];
+            if((global_index>=lo) && (global_index<hi))
+            {
+                int local_index = global_index - lo;
+                (*pCellsDistributed)[ local_index ]->SetStimulusFunction(mpStimulus);
+            }
+        }
+    }
+    
+    ~PointStimulusHeartCellFactory(void)
+    {
+        delete mpStimulus;
+    }
+};
+
+class TestBidomainHeart : public CxxTest::TestSuite 
+{   
+ 
+public:
+    void TestBidomainDg0Heart()
+    {
+        PointStimulusHeartCellFactory cell_factory(0.01);
+        BidomainProblem<3> bidomain_problem(&cell_factory);
+
+        bidomain_problem.SetMeshFilename("mesh/test/data/heart_fifth");
+        bidomain_problem.SetEndTime(100);   // 100 ms
+        bidomain_problem.SetOutputDirectory("/tmp/testoutput/BiDg0_FifthHeart");
+        bidomain_problem.SetOutputFilenamePrefix("BidomainLR91_FifthHeart");
+        bidomain_problem.SetPdeTimeStep(0.01);
+        bidomain_problem.Initialise();        
+
+        bidomain_problem.Solve();
+    }
+};
+
+#endif //_TESTBIDOMAINHEART_HPP_
