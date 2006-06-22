e2d7f254122db8f1ef3eda85b7da6a11976ae5c2
Pras Pathmanathan <pras@cs.ox.ac.uk>
2006-06-22 11:26:34 +0000

    Added TestBidomainCompareWithMemfem, associated mesh and put it in FailingTestPack
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1090 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ coupled/test/TestBidomainCompareWithMemfem.hpp
@@ -0,0 +1,72 @@
+#ifndef TESTBIDOMAINCOMPAREWITHMEMFEM_HPP_
+#define TESTBIDOMAINCOMPAREWITHMEMFEM_HPP_
+
+
+// Element.hpp includes the Boost ublas objects - these need to
+// be included early...  We think.  We're not that sure.
+#include "Element.hpp"
+#include <cxxtest/TestSuite.h>
+#include <petscvec.h>
+#include <vector>
+//#include <iostream>
+#include "PetscSetupAndFinalize.hpp"
+#include "BidomainProblem.hpp"
+#include "MonodomainProblem.hpp"
+#include "AbstractCardiacCellFactory.hpp"
+#include "LuoRudyIModel1991OdeSystem.hpp"
+
+
+class BidomainFaceStimulusCellFactory : public AbstractCardiacCellFactory<3>
+{
+private:
+    InitialStimulus *mpStimulus;
+public:
+    BidomainFaceStimulusCellFactory() : AbstractCardiacCellFactory<3>(0.01)
+    {
+        mpStimulus = new InitialStimulus(-600.0*1000, 3, 50);
+    }
+    
+    AbstractCardiacCell* CreateCardiacCellForNode(int node)
+    {
+        if (node==19)
+        {
+            return new LuoRudyIModel1991OdeSystem(mpSolver, mTimeStep, mpStimulus, mpZeroStimulus);
+        }
+        else
+        {
+            return new LuoRudyIModel1991OdeSystem(mpSolver, mTimeStep, mpZeroStimulus, mpZeroStimulus);
+        }
+    }
+        
+    ~BidomainFaceStimulusCellFactory(void)
+    {
+        delete mpStimulus;
+    }
+};
+
+class TestBidomainCompareWithMemfem :  public CxxTest::TestSuite 
+{
+public:
+    
+    void testBidomainCompareWithMemfem()
+    {
+        BidomainFaceStimulusCellFactory bidomain_cell_factory;
+        
+        BidomainProblem<3> bidomain_problem( &bidomain_cell_factory );
+
+        bidomain_problem.SetMeshFilename("coupled/test/data/memfem_mesh/simple");
+        
+        bidomain_problem.SetEndTime(300);   // ms
+        bidomain_problem.SetOutputDirectory("Bidomain3d_CompareWithMemfem");
+        bidomain_problem.SetOutputFilenamePrefix("bidomain3d");
+
+        bidomain_problem.Initialise();
+ 
+        // not set condutivities to agree with memfem, or grounded any nodes
+ 
+        bidomain_problem.Solve();
+    }
+};
+
+
+#endif /*TESTBIDOMAINCOMPAREWITHMEMFEM_HPP_*/
