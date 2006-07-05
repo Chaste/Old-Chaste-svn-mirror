9e5231eea22f284742d7aa205d4381264c5a57c2
Lee Momtahan <leemo@comlab.ox.ac.uk>
2006-07-05 15:12:45 +0000

    Sort the list of test types.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1114 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/DisplayTests.py
+++ python/DisplayTests.py
@@ -25,7 +25,9 @@ def index(req):
   This is the funky new interface to Chaste's testing suite.
   </p>
   <ul>""")
-  for tests_type in os.listdir(_tests_dir):
+  tests_types = os.listdir(_tests_dir)
+  tests_types.sort()
+  for tests_type in tests_types:
     if tests_type[0] != '.':
       output.append('\n    <li><a href="%s/recent?type=%s">Recent %s builds.</a></li>' %
                     (_our_url, tests_type, tests_type))
