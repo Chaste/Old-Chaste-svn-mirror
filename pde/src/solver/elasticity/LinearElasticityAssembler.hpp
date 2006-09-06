85d917a1eaeb36c91d3663961791512110c8cab8
Pras Pathmanathan <pras@cs.ox.ac.uk>
2006-09-06 17:22:43 +0000

    linear elasticity
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1352 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pde/src/solver/elasticity/LinearElasticityAssembler.hpp
+++ pde/src/solver/elasticity/LinearElasticityAssembler.hpp
@@ -9,9 +9,12 @@ template <int DIM>
 class LinearElasticityAssembler : public AbstractLinearStaticProblemAssembler<DIM,DIM,DIM>
 {
 private :
-    double mYoungsModulus;            // units??!!!!!!!!!
-    double mPoissonsRatio;            // dimensionless
-    c_vector<double, DIM>  mGravity;  // units??
+    double mLambda;                  // units??!!!!!!!!!
+    double mMu;                      // units??
+    double mDensity;                 // units??
+    c_vector<double, DIM> mGravity;  // units??
+    
+    
     
     virtual c_matrix<double,DIM*(DIM+1),DIM*(DIM+1)> ComputeLhsTerm(
         const c_vector<double, DIM+1> &rPhi,
@@ -19,7 +22,27 @@ private :
         const Point<DIM> &rX,
         const c_vector<double,DIM> &u)
     {
-        return zero_matrix<double>(DIM*(DIM+1)); // todo: fill in
+        c_matrix<double,DIM*(DIM+1),DIM*(DIM+1)> ret;
+        
+        for(unsigned I=0; I<DIM+1; I++) // I=node index
+        {
+            for(unsigned s=0; s<DIM; s++) // s=spatial dimension index
+            {
+                for(unsigned J=0; J<DIM+1; J++) //J=node index
+                {
+                    for(unsigned t=0; t<DIM; t++) //t=spatial dimension index
+                    {
+                        ret(DIM*I+s, DIM*J+t) = mLambda*rGradPhi(s,I)*rGradPhi(t,J) + mMu*rGradPhi(t,I)*rGradPhi(s,J);
+                        
+                        for(unsigned j=0; j<DIM; j++)
+                        {
+                            ret(DIM*I+s, DIM*J+t) += mMu*(s==t)*rGradPhi(j,I)*rGradPhi(j,J);
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
     }
     
     
@@ -28,7 +51,15 @@ private :
         const Point<DIM> &rX,
         const c_vector<double,DIM> &u)
     {
-        return zero_vector<double>(DIM*(DIM+1)); // todo: fill in
+        c_vector<double,DIM*(DIM+1)> ret;
+        for(unsigned I=0; I<DIM+1; I++) // I = node_index
+        {
+            for(unsigned s=0; s<DIM; s++) // s = spatial dimension index
+            {
+                ret(DIM * I + s) = mDensity * mGravity(s) * rPhi(I);
+            }
+        }
+        return ret;
     }  
 
     virtual c_vector<double, DIM*DIM> ComputeSurfaceRhsTerm(
@@ -49,9 +80,14 @@ public :
         
         this->mpMesh = pMesh;
         this->mpBoundaryConditions = pBoundaryConditions;
+        
+        mGravity = zero_vector<double>(DIM);
     }
     
-    void SetParameters(double youngsModulus, double poissonsRatio, c_vector<double, DIM> gravity)
+    
+    ///\todo: finish this 
+    /*
+    void SetYoungsModulusAndPoissonsRatio(double youngsModulus, double poissonsRatio)
     {
         if(youngsModulus < 0.0)
         {
@@ -62,11 +98,27 @@ public :
             EXCEPTION("Poisson's ratio, nu, must be less than 0.5");
         }
         
-        mYoungsModulus = youngsModulus;
-        mPoissonsRatio = poissonsRatio;
-        
+        mLambda = .....;
+        mMu = .....;
+    }
+    */
+    
+    void SetLameCoefficients(double lambda, double mu)
+    {
+        mLambda = lambda;
+        mMu = mu;
+    }
+    
+    
+    void SetDensityAndGravity(double density, c_vector<double, DIM> gravity)
+    {
+        if(density < 0.0)
+        {
+            EXCEPTION("Density must be positive");
+        }
+        mDensity = density;
         mGravity = gravity;
-   }
+    }
 };
 
 #endif /*LINEARELASTICITYASSEMBLER_HPP_*/
--- pde/test/TestLinearElasticityAssembler.hpp
+++ pde/test/TestLinearElasticityAssembler.hpp
@@ -12,30 +12,94 @@
 class TestLinearElasticityAssembler : public CxxTest::TestSuite
 {
 public:
-    void Test3dExample(void)
+
+    //////////////////////////////////////////////////////////////////
+    // Solve a 1d linear elasticity problem with the 
+    // LinearElasticityAssembler
+    //
+    // Fix node 0, leave other end free and under zero-traction
+    // In this case: 
+    //     sigma = lam u' delta_{11} + mu (u' + u')
+    //           = (lam + 2mu) u'               
+    //
+    // so eqn of state is 
+    //     (lam+2mu)u'' + rho g = 0
+    // 
+    // from which exact solution can be computed to be (using
+    // boundary conditions u=0 at x=0, sigma=0 at x=1):
+    //     u = ( rho g / (lam + 2mu ) ) x  (1 - x/2)
+    //////////////////////////////////////////////////////////////////
+    void Test1dExample(void) throw (Exception)
     {
-        TrianglesMeshReader<3,3> mesh_reader("mesh/test/data/cube_136_elements");
-        ConformingTetrahedralMesh<3,3> mesh;
+        TrianglesMeshReader<1,1> mesh_reader("mesh/test/data/1D_0_to_1_10_elements");
+        ConformingTetrahedralMesh<1,1> mesh;
         mesh.ConstructFromMeshReader(mesh_reader);
-
-        double E = 10;
-        double nu = 0.3;
-        c_vector<double,3> g;
-        g(0) = -9.81;
-        g(1) = 0; 
-        g(2) = 0;
         
-        BoundaryConditionsContainer<3,3,3> bcc(mesh.GetNumNodes());
-        
-        LinearElasticityAssembler<3> assembler(&mesh,&bcc);
-        assembler.SetParameters(E,nu,g);
+        BoundaryConditionsContainer<1,1,1> bcc(mesh.GetNumNodes());
+        ConstBoundaryCondition<1>* p_bc = new ConstBoundaryCondition<1>(0);
+        bcc.AddDirichletBoundaryCondition(mesh.GetNodeAt(0),p_bc);
 
-        Vec result = assembler.Solve(); // doesnt do anything as ComputeRhsTerm etc not yet filled it
+        double lambda = 2.0;
+        double mu = 1.5;
+        double rho = 1;
+
+        c_vector<double,1> g;
+        g(0) = 1;
+        
+        LinearElasticityAssembler<1> assembler(&mesh,&bcc);
+        assembler.SetLameCoefficients(lambda, mu);
+        assembler.SetDensityAndGravity(rho, g);
+
+        Vec result = assembler.Solve(); 
         ReplicatableVector result_repl(result);
         
         for(int i=0; i<mesh.GetNumNodes(); i++)
         {
-            TS_ASSERT_DELTA(result_repl[i], 0.0, 1e-6);
+            double x = mesh.GetNodeAt(i)->GetPoint()[0];
+            
+            double factor = rho*g(0)/(lambda+2*mu);
+            double exact_solution = factor * x * (1-x/2);
+
+            TS_ASSERT_DELTA(result_repl[i], exact_solution, 1e-6);
+            //std::cout << x << " " << result_repl[i] << " " << exact_solution << "\n";
+        }
+    }
+
+    void DONT_________________Test3dExample(void) throw (Exception)
+    {
+        TrianglesMeshReader<3,3> mesh_reader("mesh/test/data/cube_136_elements");
+        ConformingTetrahedralMesh<3,3> mesh;
+        mesh.ConstructFromMeshReader(mesh_reader);
+        
+        BoundaryConditionsContainer<3,3,3> bcc(mesh.GetNumNodes());
+
+        ConstBoundaryCondition<3>* p_bc0 = new ConstBoundaryCondition<3>(0);
+        ConstBoundaryCondition<3>* p_bc1 = new ConstBoundaryCondition<3>(0);
+        ConstBoundaryCondition<3>* p_bc2 = new ConstBoundaryCondition<3>(0);
+        
+        bcc.AddDirichletBoundaryCondition(mesh.GetNodeAt(0),p_bc0,0);
+        bcc.AddDirichletBoundaryCondition(mesh.GetNodeAt(0),p_bc1,1);
+        bcc.AddDirichletBoundaryCondition(mesh.GetNodeAt(0),p_bc2,2);
+
+        //double E = 10;
+        //double nu = 0.3;
+        double rho = 1;
+        c_vector<double,1> g;
+        g(0) = 1;
+        g(1) = 0; 
+        g(2) = 0;
+        
+        LinearElasticityAssembler<3> assembler(&mesh,&bcc);
+        //assembler.SetYoungsModulusAndPoissonsRatio(E,nu);
+        assembler.SetDensityAndGravity(rho,g);
+
+        Vec result = assembler.Solve(); 
+        ReplicatableVector result_repl(result);
+        
+        for(int i=0; i<mesh.GetNumNodes(); i++)
+        {
+           // TS_ASSERT_DELTA(result_repl[i], 0.0, 1e-6);
+           //double x = mesh.GetNodeAt(i)->GetPoint()[0];
         }
     }
 };
