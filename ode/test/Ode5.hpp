6c4094471174996b50216ee757d51d36293baaea
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2006-09-22 11:57:00 +0000

    Jon W committed Ode5.hpp.  He's broken the build and gone to lunch. I (Joe) hopes this fixes it.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1405 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ ode/test/Ode5.hpp
@@ -0,0 +1,26 @@
+#ifndef ODE5_HPP_
+#define ODE5_HPP_
+
+#include "AbstractOdeSystem.hpp"
+
+
+class Ode5 : public AbstractOdeSystem
+{
+public :
+    Ode5()
+        : AbstractOdeSystem(1)  // 1 here is the number of unknowns
+    {
+        mInitialConditions.push_back(0.2);
+    }    
+    
+    std::vector<double> EvaluateYDerivatives (double time, const std::vector<double> &rY)
+    {
+        std::vector<double> y_derivatives(GetNumberOfStateVariables());
+        double alpha = 100;
+        y_derivatives[0]=alpha*rY[0]*(1-rY[0]);
+        return y_derivatives;
+    }
+    
+};
+
+#endif /*ODE5_HPP_*/
