36d4da3a7e23f532bc73558d41adcf727eefc1ef
Gary Mirams <gary.mirams@gmail.com>
2006-09-25 11:01:08 +0000

    mending it
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1425 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ ode/test/OdeThirdOrderWithEvents.hpp
@@ -0,0 +1,36 @@
+#/**
+ * Concrete OdeThirdOrder class with events
+ */
+#ifndef _ODETHIRDORDERWITHEVENTS_HPP
+#define _ODETHIRDORDERWITHEVENTS_HPP
+#include "AbstractOdeSystem.hpp"
+
+
+class OdeThirdOrderWithEvents : public AbstractOdeSystem
+{
+public :
+    OdeThirdOrderWithEvents()
+        : AbstractOdeSystem(3) // 3 here is the number of unknowns
+    {
+        mInitialConditions.push_back(0.0);
+        mInitialConditions.push_back(1.0);
+        mInitialConditions.push_back(0.0);
+    }
+    
+    
+    std::vector<double> EvaluateYDerivatives (double time, const std::vector<double> &rY)
+    {
+        std::vector<double> y_derivatives(GetNumberOfStateVariables());
+        y_derivatives[0]=rY[0]-rY[1]+rY[2];
+        y_derivatives[1]=rY[1]-rY[2];
+        y_derivatives[2]=2*rY[1]-rY[2];
+        return y_derivatives;
+    }
+    
+    bool CalculateStoppingEvent(double time, const std::vector<double> &rY)
+    {
+        return (rY[0]<-0.5);
+    }
+};
+
+#endif //_ODETHIRDORDERWITHEVENTS_HPP
