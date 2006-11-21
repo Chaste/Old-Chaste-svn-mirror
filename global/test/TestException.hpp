49131b3f1d4dcc0852fc02eaab5b7113d96f6525
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2006-11-21 16:54:52 +0000

    Improve coverage of Exception.cpp
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1599 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ global/test/TestException.hpp
@@ -0,0 +1,26 @@
+#ifndef _TESTEXCEPTION_HPP_
+#define _TESTEXCEPTION_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include "Exception.hpp"
+
+
+class TestException : public CxxTest::TestSuite
+{
+public:
+    void TestGetMessage()
+    {
+        std::string msg("This is an exception");
+
+        try {
+            EXCEPTION(msg);
+        } catch (Exception e) {
+            std::string e_msg = e.GetMessage();
+            std::string::size_type e_len = e_msg.length();
+            std::string::size_type len = msg.length();
+            TS_ASSERT_EQUALS(e_msg.substr(e_len - len), msg);
+        }
+    }
+};
+
+#endif //_TESTEXCEPTION_HPP_
