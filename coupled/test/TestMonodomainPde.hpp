41e414033143a955d13f7719f15b89b20a383cbc
Lee Momtahan <leemo@comlab.ox.ac.uk>
2007-04-13 10:44:36 +0000

    Refactoring - making use of !VectorPortion and tidying Monodomain tests
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@1966 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- coupled/test/TestMonodomainPde.hpp
+++ coupled/test/TestMonodomainPde.hpp
@@ -10,6 +10,7 @@
 #include "OdeSolution.hpp"
 #include "PetscSetupAndFinalize.hpp"
 #include "AbstractCardiacCellFactory.hpp"
+#include "VectorPortion.hpp"
 #include <petsc.h>
 #include <cxxtest/TestSuite.h>
 
@@ -91,110 +92,75 @@ public:
         Vec voltage;
         VecCreate(PETSC_COMM_WORLD, &voltage);
         VecSetSizes(voltage, PETSC_DECIDE, num_nodes);
-        //VecSetType(initialCondition, VECSEQ);
         VecSetFromOptions(voltage);
-        
-        double* p_voltage_array;
-        VecGetArray(voltage, &p_voltage_array);
-        
-        unsigned lo, hi;
-        PetscInt temp_lo, temp_hi;
-        VecGetOwnershipRange(voltage,&temp_lo,&temp_hi);
-        lo=(unsigned) temp_lo;
-        hi=(unsigned) temp_hi;
-        
-        for (unsigned global_index = 0; global_index < num_nodes; global_index++ )
+
+        VectorPortion portion(voltage);
+        for (VectorPortion::Iterator index = portion.Begin();
+             index != portion.End();
+             ++index)
         {
-            unsigned local_index = global_index - lo;
-            // initial voltage condition of a constant everywhere on the mesh
-            if (lo<=global_index && global_index<hi)
-            {
-                p_voltage_array[local_index] = initial_voltage;
-            }
+            *index = initial_voltage;
         }
-        VecRestoreArray(voltage, &p_voltage_array);
-        VecAssemblyBegin(voltage);
-        VecAssemblyEnd(voltage);
+        portion.Restore();
         
         monodomain_pde.SolveCellSystems(voltage, start_time, start_time+big_time_step);
         
-        
         double value1 = monodomain_pde.GetIionicCacheReplicated()[0];
-        
         LuoRudyIModel1991OdeSystem ode_system_stimulated(solver, small_time_step, stimulus);
-        
         OdeSolution SolutionNewStimulated = ode_system_stimulated.ComputeExceptVoltage(
                                                 start_time,
                                                 start_time + big_time_step);
         std::vector<double> solutionSetStimT_05 = SolutionNewStimulated.rGetSolutions()[ SolutionNewStimulated.rGetSolutions().size()-1 ];
         
         double value2 = ode_system_stimulated.GetIIonic();
-        
         TS_ASSERT_DELTA(value1, value2, 0.000001);
         
         // shouldn't be different when called again as reset not yet been called
         value1 = monodomain_pde.GetIionicCacheReplicated()[0];
         TS_ASSERT_DELTA(value1, value2, 0.000001);
-        
+
         LuoRudyIModel1991OdeSystem ode_system_not_stim(solver, small_time_step, zero_stim);
-        
         value1 = monodomain_pde.GetIionicCacheReplicated()[1];
-        
         OdeSolution SolutionNewNotStim = ode_system_not_stim.ComputeExceptVoltage(
                                              start_time,
                                              start_time + big_time_step);
         std::vector<double> solutionSetNoStimT_05 = SolutionNewNotStim.rGetSolutions()[ SolutionNewNotStim.rGetSolutions().size()-1 ];
         value2 = ode_system_not_stim.GetIIonic();
-        
         TS_ASSERT_DELTA(value1, value2, 0.000001);
         
-        
         // Reset
-        VecGetArray(voltage, &p_voltage_array);
-        
-        
-        if (lo<=0 && 0<hi)
+        for (VectorPortion::Iterator index = portion.Begin();
+             index != portion.End();
+             ++index)
         {
-            unsigned local_index = 0 - lo;
-            p_voltage_array[local_index] = solutionSetStimT_05[4];
+            if (index.Global==0)
+            {
+                *index = solutionSetStimT_05[4];
+            }
+            if (index.Global==1)
+            {
+                *index = solutionSetNoStimT_05[4];
+            }
         }
-        if (lo<=1 && 1<hi)
-        {
-            unsigned local_index = 1 - lo;
-            p_voltage_array[local_index] = solutionSetNoStimT_05[4];
-        }
-        
-        VecRestoreArray(voltage, &p_voltage_array);
-        VecAssemblyBegin(voltage);
-        VecAssemblyEnd(voltage);
+        portion.Restore();
         
         monodomain_pde.SolveCellSystems(voltage, start_time, start_time+big_time_step);
-        
         value1 = monodomain_pde.GetIionicCacheReplicated()[0];
-        
         std::vector<double> state_variables = solutionSetStimT_05;
         ode_system_stimulated.SetStateVariables(state_variables);
-        
         OdeSolution SolutionNewStimulatedT_1 = ode_system_stimulated.ComputeExceptVoltage( start_time + big_time_step, start_time + 2*big_time_step );
-        
         std::vector<double> solutionSetStimT_1 = SolutionNewStimulatedT_1.rGetSolutions()[ SolutionNewStimulatedT_1.rGetSolutions().size()-1 ];
         value2 = ode_system_stimulated.GetIIonic();
-        
         TS_ASSERT_DELTA(value1, value2, 1e-10);
         
         state_variables = solutionSetNoStimT_05;
         ode_system_not_stim.SetStateVariables(state_variables);
-        
         OdeSolution SolutionNewNotStimT_1 = ode_system_not_stim.ComputeExceptVoltage( start_time + big_time_step, start_time + 2*big_time_step );
-        
         std::vector<double> solutionSetNoStimT_1 = SolutionNewNotStimT_1.rGetSolutions()[ SolutionNewNotStimT_1.rGetSolutions().size()-1 ];
-        
         value1 = monodomain_pde.GetIionicCacheReplicated()[1];
         value2 = ode_system_not_stim.GetIIonic();
         
-        
         TS_ASSERT_DELTA(value1, value2, 1e-10);
-        
         
         VecDestroy(voltage);
         delete zero_stim;
