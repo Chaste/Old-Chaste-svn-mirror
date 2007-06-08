39868184b997690e3a6ee35c637523a641388d6b
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2007-06-08 17:27:59 +0000

    #399 part one - ignore lines with just a {
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2212 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/DisplayCoverage.py
+++ python/DisplayCoverage.py
@@ -150,7 +150,7 @@ for src_file in src_files:
                     aggregated_count = '-'
                 else:
                     src_line_stripped = src_line.strip()
-                    if not (ignore or src_line_stripped == '}' or
+                    if not (ignore or src_line_stripped in ['{', '}']
                             (src_line_stripped.startswith('return') and
                              src_line_stripped[6] in [';', ' ']) or
                             (src_line_stripped.startswith('catch ') and
