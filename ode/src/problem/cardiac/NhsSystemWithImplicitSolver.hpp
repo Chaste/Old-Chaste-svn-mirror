0b8b56c05d6c5ba6dcf900db70b39327b985b6c2
Pras Pathmanathan <pras@cs.ox.ac.uk>
2007-09-17 18:20:32 +0000

    WP6: finished NhsSystenWithImplicitSolver, except for doxy&cpp
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2573 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- ode/src/problem/cardiac/NhsSystemWithImplicitSolver.hpp
+++ ode/src/problem/cardiac/NhsSystemWithImplicitSolver.hpp
@@ -2,34 +2,41 @@
 #define NHSSYSTEMWITHIMPLICITSOLVER_HPP_
 
 #include "NHSCellularMechanicsOdeSystem.hpp"
-#include "EulerIvpOdeSolver.hpp"
+#include "TimeStepper.hpp"
 
+// todo: doxygen
+
+/** 
+ *  NHS system with build in implicit solver. Jon Whiteley's method, which breaks down
+ *  the multivariable implicit solve into a sequence of 1d implicit solves
+ */
 class NhsSystemWithImplicitSolver : public NHSCellularMechanicsOdeSystem
 {
 private:
     const static double mTolerance = 1e-6; 
     double mDt;
+    bool mUseImplicitExplicitSolveForZ;
 
-    std::vector<double> mStoredStateVariables;
+    std::vector<double> mTempStoredStateVariables;
+    std::vector<double> mCurrentStateVars;
+    
     double mActiveTensionInitialGuess;
     double mActiveTensionSolution;
 
     void ImplicitSolveForActiveTension()
     {
         double current_active_tension = mActiveTensionInitialGuess;
-        
         double residual = CalcActiveTensionResidual(current_active_tension);
-        std::cout << "ImplicitSolveForActiveTension: " << residual << "\n";
 
         unsigned counter = 0;
         while ((fabs(residual)>mTolerance) && (counter++<15))
         {
-            double h = current_active_tension/100;
+            double h = std::max(fabs(current_active_tension/100),1e-8);
+
             double jac = (CalcActiveTensionResidual(current_active_tension+h) - CalcActiveTensionResidual(current_active_tension))/h;
             
-            current_active_tension -= jac/residual;
+            current_active_tension -= residual/jac;
             residual = CalcActiveTensionResidual(current_active_tension);
-            std::cout << "ImplicitSolveForActiveTension: " << residual << "\n";
         } 
         assert(counter<15);
         
@@ -42,18 +49,26 @@ private:
         // solve for new Ca_trop implicitly
         double new_Ca_Trop = ImplicitSolveForCaTrop(activeTensionGuess);
         
-        mStoredStateVariables[0] = new_Ca_Trop;
+        mTempStoredStateVariables[0] = new_Ca_Trop;
         
-        // solve for new Ca_trop implicitly
-        double new_z = ImplicitSolveForZ(new_Ca_Trop);
-
-        mStoredStateVariables[1] = new_z;
+        // solve for new Ca_trop implicitly (or with a mixed implicit-explicit scheme, if asked for)
+        double new_z;
+        if(!mUseImplicitExplicitSolveForZ)
+        {
+            new_z = ImplicitSolveForZ(new_Ca_Trop);
+        }
+        else
+        {
+            new_z = ImplicitExplicitSolveForZ(new_Ca_Trop);
+        }
+        
+        mTempStoredStateVariables[1] = new_z;
 
         // get the new T0 
-        double new_T0 = CalculateT0(new_z); std::cout << "new T0 = " << new_T0 <<"\n";
+        double new_T0 = CalculateT0(new_z); 
     
         // solve for the new Qi's (and therefore Q) implicitly
-        double new_Q = ImplicitSolveForQ(); std::cout << "new Q = " << new_Q <<"\n";  // can be moved up
+        double new_Q = ImplicitSolveForQ();  // should be moved up
                 
         double new_active_tension = 0;
         if(new_Q > 0)
@@ -72,32 +87,29 @@ private:
     
     double ImplicitSolveForCaTrop(double newActiveTension)
     {
-        double old_Ca_trop = mStateVariables[0];
+        double old_Ca_trop = mCurrentStateVars[0];
         
         double numer = old_Ca_trop + mDt * mKon * mCalciumI  * mCalciumTroponinMax;
         double denom = 1 + mDt*mKon*mCalciumI + mDt*mKrefoff*(1-newActiveTension/(mGamma*mTref));
 
-        std::cout << "ImplicitSolveForCaTrop: ca=" << numer/denom  << "\n";
-        
         return numer/denom;
     }
 
 
     double ImplicitSolveForZ(double newCaTrop)
     {
-        double current_z = mStateVariables[1];
+        double current_z = mCurrentStateVars[1];
         double residual = CalcZResidual(current_z, newCaTrop);
-            std::cout << "ImplicitSolveForZ: z,resid=" <<current_z << ", " << residual << "\n";
         unsigned counter = 0;
+        
         while ((fabs(residual)>mTolerance) && (counter++<15))
         {
+            double h = std::max(fabs(current_z/100),1e-8);
+            double jac = (CalcZResidual(current_z + h, newCaTrop) - CalcZResidual(current_z, newCaTrop));
+            jac/= h;
             
-            double h = current_z/100;
-            double jac = (CalcZResidual(current_z + h, newCaTrop) - CalcZResidual(current_z, newCaTrop))/h;
-            
-            current_z -= jac/residual;
+            current_z -= residual/jac;
             residual = CalcZResidual(current_z, newCaTrop);
-            std::cout << "ImplicitSolveForZ: resid=" <<residual << "\n";
         } 
         assert(counter<15);
         
@@ -111,31 +123,37 @@ private:
                      - mAlphaR1 * z
                      - mAlphaR2 * pow(z,mNr) / (pow(z,mNr) + pow(mKZ,mNr));
 
-        return z - mStateVariables[1] - mDt*dzdt;                     
+        return z - mCurrentStateVars[1] - mDt*dzdt;                     
     }
 
-//    double ImplicitExplicitSolveForZ(double newCaTrop)
-//    {
-//        // fill in -    
-//    }
+
+    double ImplicitExplicitSolveForZ(double newCaTrop)
+    {
+        double ca_trop_to_ca_trop50_ratio_to_n = pow(newCaTrop/mCalciumTrop50, mN);
+        double old_z = mCurrentStateVars[1];
+
+        double numer =   old_z + mDt * mAlpha0 * ca_trop_to_ca_trop50_ratio_to_n 
+                       - mDt * mAlphaR2 * pow(old_z,mNr) / (pow(old_z,mNr) + pow(mKZ,mNr));
+        double denom =   1 + mDt * mAlpha0 * ca_trop_to_ca_trop50_ratio_to_n + mAlphaR1 *mDt;
+        
+        return numer/denom;
+    }
 
 
 
     double ImplicitSolveForQ()
     {
-        double old_Q1 = mStateVariables[2];
-        double old_Q2 = mStateVariables[3];
-        double old_Q3 = mStateVariables[4];
+        double old_Q1 = mCurrentStateVars[2];
+        double old_Q2 = mCurrentStateVars[3];
+        double old_Q3 = mCurrentStateVars[4];
         
         double new_Q1 = (old_Q1 + mDt*mA1*mDLambda1Dt)/(1 + mAlpha1*mDt);
         double new_Q2 = (old_Q2 + mDt*mA2*mDLambda1Dt)/(1 + mAlpha2*mDt);
         double new_Q3 = (old_Q3 + mDt*mA3*mDLambda1Dt)/(1 + mAlpha3*mDt);
         
-        mStoredStateVariables[2] = new_Q1;
-        mStoredStateVariables[3] = new_Q2;
-        mStoredStateVariables[4] = new_Q3;
-        
-        std::cout << "ImplicitSolveForQ: Q=" << new_Q1 + new_Q2 + new_Q3  << "\n";
+        mTempStoredStateVariables[2] = new_Q1;
+        mTempStoredStateVariables[3] = new_Q2;
+        mTempStoredStateVariables[4] = new_Q3;
         
         return new_Q1 + new_Q2 + new_Q3; 
     }
@@ -146,8 +164,9 @@ public :
     NhsSystemWithImplicitSolver()
         : NHSCellularMechanicsOdeSystem()
     {
-        mStoredStateVariables.resize(GetNumberOfStateVariables());
+        mTempStoredStateVariables.resize(GetNumberOfStateVariables());
         mActiveTensionInitialGuess = 0.0;
+        mUseImplicitExplicitSolveForZ = false;
     }
     
     void SetActiveTensionInitialGuess(double activeTensionInitialGuess)
@@ -162,38 +181,32 @@ public :
         assert(startTime < endTime);
         
         mDt = timestep;
-        
-        // store the initial state vars
-        std::vector<double> old_state_vars = rGetStateVariables();
-        
-//todo: proper implicit solve
-        // solve
-//        EulerIvpOdeSolver solver;
-  //      solver.SolveAndUpdateStateVariable(this, startTime, endTime, timestep);
 
-        double time = startTime;
-        while(time < endTime)
+        // the implicit routines look in mCurrentStateVars for the current values
+        mCurrentStateVars = mStateVariables;   
+
+        // loop in time        
+        TimeStepper stepper(startTime, endTime, timestep);
+        while ( !stepper.IsTimeAtEnd() )
         {
-            std::cout << " ** TIME = " << time << "\n";
             ImplicitSolveForActiveTension();
-            time += timestep;
+            mCurrentStateVars = mTempStoredStateVariables;
+
+            stepper.AdvanceOneTimeStep();
         }
-
-
-        
-        // rewrite with the old state vars
-        for(unsigned i=0; i<rGetStateVariables().size(); i++)
-        {
-            rGetStateVariables()[i] = old_state_vars[i];
-        }        
     }
     
-    void UpdateSystem()
+    void UpdateStateVariables()
     {
         for(unsigned i=0; i<mStateVariables.size(); i++)
         {
-            mStateVariables[i] = mStoredStateVariables[i];
+            mStateVariables[i] = mCurrentStateVars[i];
         }      
+    }
+    
+    void UseImplicitExplicitSolveForZ(bool useImplicitExplicitSolveForZ = true)
+    {
+        mUseImplicitExplicitSolveForZ = useImplicitExplicitSolveForZ;
     }
 };
 
--- ode/test/TestNhsSystemWithImplicitSolver.hpp
+++ ode/test/TestNhsSystemWithImplicitSolver.hpp
@@ -10,48 +10,148 @@
 
 class TestNhsSystemWithImplicitSolver : public CxxTest::TestSuite
 {
-public:
-    void TestSolver()
+private:
+    double GetSampleCaIValue()
     {
-        NhsSystemWithImplicitSolver system_with_solver;
-        system_with_solver.SetLambda1AndDerivative(0.5, 0.1);
-        
-        unsigned num_vars = system_with_solver.GetNumberOfStateVariables();
-
-        // the following is just to get a realistic Ca_I value
         EulerIvpOdeSolver euler_solver;
         ZeroStimulus zero_stimulus;
         LuoRudyIModel1991OdeSystem lr91(&euler_solver, 0.01, &zero_stimulus);
-        unsigned Ca_i_index = lr91.GetStateVariableNumberByName("CaI");
-        double Ca_I = lr91.rGetStateVariables()[Ca_i_index];
-        
+        return lr91.rGetStateVariables()[lr91.GetStateVariableNumberByName("CaI")];
+    }        
+
+public:
+    void TestSolverSingleTimestep()
+    {
+        NhsSystemWithImplicitSolver system_with_solver;
+
+        // lam=const, dlamdt not const doesn't make much sense, just for testing purposes
+        system_with_solver.SetLambda1AndDerivative(0.5, 0.1);
+        double Ca_I = GetSampleCaIValue();
         system_with_solver.SetIntracellularCalciumConcentration(Ca_I);
 
-        system_with_solver.SolveDoNotUpdate(0,0.1,0.1);
+        // solve system (but don't update state vars yet 
+        system_with_solver.SolveDoNotUpdate(0, 0.1, 0.1); // one timestep
         
-        NHSCellularMechanicsOdeSystem system_no_solver;
-        system_no_solver.SetLambda1AndDerivative(0.5, 0.1);
-        system_no_solver.SetIntracellularCalciumConcentration(Ca_I);
+        NHSCellularMechanicsOdeSystem system_for_euler_solver;
         
+        unsigned num_vars = system_with_solver.GetNumberOfStateVariables();
         for(unsigned i=0; i<num_vars; i++)
         {
+            // both should be the same (ie initial values)
             TS_ASSERT_DELTA(system_with_solver.rGetStateVariables()[i],  
-                            system_no_solver.rGetStateVariables()[i],
+                            system_for_euler_solver.rGetStateVariables()[i],
                             1e-12);
         }                             
 
-        euler_solver.SolveAndUpdateStateVariable(&system_no_solver, 0, 0.1, 0.1);
-        system_with_solver.UpdateSystem();
+        // solve system with euler
+        system_for_euler_solver.SetLambda1AndDerivative(0.5, 0.1); 
+        system_for_euler_solver.SetIntracellularCalciumConcentration(Ca_I);
+        EulerIvpOdeSolver euler_solver;
+        euler_solver.SolveAndUpdateStateVariable(&system_for_euler_solver, 0, 0.1, 0.1);  // one timestep
+
+        // update state vars on implicit system
+        system_with_solver.UpdateStateVariables();
 
         for(unsigned i=0; i<num_vars; i++)
         {
+            //std::cout << system_with_solver.rGetStateVariables()[i] << " "
+            //          << system_for_euler_solver.rGetStateVariables()[i] << "\n"; 
+            
             // we want these within 10% of each other. Note we expect the implicit 
-            // solver to be more accurate than the explicit one so can't expect them
+            // solver to be more accurate than the explicit solver, and the timestep 
+            // is quite large (as we want non-zero solutions), so can't expect them
             // to be too close.
             TS_ASSERT_DELTA(system_with_solver.rGetStateVariables()[i],  
-                            system_no_solver.rGetStateVariables()[i],
+                            system_for_euler_solver.rGetStateVariables()[i],
                             fabs(system_with_solver.rGetStateVariables()[i]*1e-1));
         }     
     }
+
+
+    void TestSolverManyTimestepsCompareWithEuler()
+    {
+        for(unsigned run=0; run<2; run++)
+        {
+            clock_t ck_start, ck_end;
+    
+            NhsSystemWithImplicitSolver system_with_solver;
+    
+            // lam=const, dlamdt not const doesn't make much sense, just for testing purposes
+            system_with_solver.SetLambda1AndDerivative(0.5, 0.1);
+            double Ca_I = GetSampleCaIValue();
+            // bigger Ca_I, so we get some active tension (and so the a few iterations are
+            // needed when solving for T_a and z
+            system_with_solver.SetIntracellularCalciumConcentration(10*Ca_I);
+
+            // IF THE SECOND RUN, use the implicit-explicit mixed method for z
+            if(run==1)
+            {
+                system_with_solver.UseImplicitExplicitSolveForZ();
+            }
+    
+            // solve system and update
+            ck_start = clock();
+            system_with_solver.SolveDoNotUpdate(0, 100, 0.01); 
+            ck_end = clock();
+            double implicit_solve_time = (double)(ck_end - ck_start)/CLOCKS_PER_SEC;
+        
+            system_with_solver.UpdateStateVariables();
+    
+            // solve system with euler
+            NHSCellularMechanicsOdeSystem system_for_euler_solver;
+            system_for_euler_solver.SetLambda1AndDerivative(0.5, 0.1); 
+            system_for_euler_solver.SetIntracellularCalciumConcentration(10*Ca_I);
+            EulerIvpOdeSolver euler_solver;
+
+            ck_start = clock();
+            euler_solver.SolveAndUpdateStateVariable(&system_for_euler_solver, 0, 100, 0.01); 
+            ck_end = clock();
+            double explicit_solve_time = (double)(ck_end - ck_start)/CLOCKS_PER_SEC;
+    
+            run==0 ? std::cout<<"\nImplicit vs Explicit\n" : std::cout<<"\nImplicitExplicit vs Explicit\n"; 
+            unsigned num_vars = system_with_solver.GetNumberOfStateVariables();
+            for(unsigned i=0; i<num_vars; i++)
+            {
+                std::cout << system_with_solver.rGetStateVariables()[i] << " "
+                          << system_for_euler_solver.rGetStateVariables()[i] << "\n"; 
+                
+                // small timesteps, want these to be very close (1%). and they are
+                TS_ASSERT_DELTA(system_with_solver.rGetStateVariables()[i],  
+                                system_for_euler_solver.rGetStateVariables()[i],
+                                fabs(system_with_solver.rGetStateVariables()[i]*1e-2));
+            }     
+            std::cout << "TIMES: " << implicit_solve_time << " " << explicit_solve_time << "\n\n";
+        }
+    }
+    
+//// test how large a timestep the implicit solver can get away with. needs more study
+//    void TestImplicitSolverWithLargeTimeSteps()
+//    {
+//        NhsSystemWithImplicitSolver system_with_solver;
+//        system_with_solver.SetLambda1AndDerivative(0.5, 0.1);
+//        system_with_solver.SetIntracellularCalciumConcentration(10*GetSampleCaIValue());
+//
+//        system_with_solver.SolveDoNotUpdate(0, 100, 0.01); 
+//        system_with_solver.UpdateStateVariables();
+//
+//        NhsSystemWithImplicitSolver system_with_solver2;
+//        system_with_solver2.SetLambda1AndDerivative(0.5, 0.1);
+//        system_with_solver2.SetIntracellularCalciumConcentration(10*GetSampleCaIValue());
+//
+//        system_with_solver2.SolveDoNotUpdate(0, 100, 1); 
+//        system_with_solver2.UpdateStateVariables();
+//
+//        unsigned num_vars = system_with_solver.GetNumberOfStateVariables();
+//        for(unsigned i=0; i<num_vars; i++)
+//        {
+//            std::cout << system_with_solver.rGetStateVariables()[i] << " "
+//                      << system_with_solver2.rGetStateVariables()[i] << "\n"; 
+//            
+//            TS_ASSERT_DELTA(system_with_solver.rGetStateVariables()[i],  
+//                            system_with_solver2.rGetStateVariables()[i],
+//                            fabs(system_with_solver.rGetStateVariables()[i]*1e-2));
+//        }
+//    }
 };
+
 #endif /*TESTNHSSYSTEMWITHIMPLICITSOLVER_HPP_*/
