f871e4a235d51ba0482f549d52644c416e3e2f42
Lee Momtahan <leemo@comlab.ox.ac.uk>
2007-09-19 14:26:06 +0000

    Added some tests designed to investigate assembly performance
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2578 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ coupled/test/TestPerformance01.hpp
@@ -0,0 +1,38 @@
+#ifndef TESTPERFORMANCE_HPP_
+#define TESTPERFORMANCE_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include "BidomainProblem.hpp"
+#include "MonodomainProblem.hpp"
+#include <petscvec.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#include "BackwardEulerLuoRudyIModel1991.hpp"
+#include "LuoRudyIModel1991OdeSystem.hpp"
+#include "PerformanceTester.hpp"
+
+class TestPerformance : public CxxTest::TestSuite
+{   
+public:
+
+    
+    void TestPerf() throw(Exception)
+    {
+        // write headings
+        PerformanceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<2>, 2>::DisplayHeadings();
+        EventHandler::Headings();
+
+        // base line
+        PerformanceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<2>, 2> tester;
+        tester.SimTime=0.0025;
+        tester.MeshNum=1;
+        tester.Run();
+        
+        EventHandler::Report();
+    }
+};
+
+#endif /*TESTPERFORMANCE_HPP_*/
