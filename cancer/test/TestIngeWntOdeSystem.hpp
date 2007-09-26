cd3e0ca208c1600bafab43ed3312d4a1e0832387
Gary Mirams <gary.mirams@gmail.com>
2007-09-26 15:50:31 +0000

    #494 Inge's Wnt Odes fully tested.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2626 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cancer/test/TestIngeWntOdeSystem.hpp
+++ cancer/test/TestIngeWntOdeSystem.hpp
@@ -170,176 +170,128 @@ public:
         }
     }
     
-    void xTestWntCellCycleSolver() throw(Exception)
+    void TestWntCellCycleSolver() throw(Exception)
     {
-        double WntLevel = 1.0;
+        double WntLevel = 0.0;
         IngeWntOdeSystem wnt_system(WntLevel,LABELLED);
+        
         // Solve system using rk4 solver
         // Matlab's strictest bit uses 0.01 below and relaxes it on flatter bits.
         
-        double h_value=0.001;
+        double h_value=0.0001;
         
         RungeKutta4IvpOdeSolver rk4_solver;
         RungeKuttaFehlbergIvpOdeSolver rkf_solver;
-        BackwardEulerIvpOdeSolver back_solver(9);
         
         OdeSolution solutions;
         //OdeSolution solutions2;
         
         std::vector<double> initial_conditions = wnt_system.GetInitialConditions();
-                
-        double start_time, end_time, elapsed_time = 0.0;
-        start_time = std::clock();
-        solutions = rk4_solver.Solve(&wnt_system, initial_conditions, 0.0, 100.0, h_value, h_value);
-        end_time = std::clock();
-        elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
-        std::cout <<  "1. Runge-Kutta Elapsed time = " << elapsed_time << "\n";
-        
-        h_value = 0.1;
-        
-        initial_conditions = wnt_system.GetInitialConditions();
-        start_time = std::clock();
-        solutions = rkf_solver.Solve(&wnt_system, initial_conditions, 0.0, 100.0, h_value, 1e-4);
-        end_time = std::clock();
-        elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
-        std::cout <<  "2. Runge-Kutta-Fehlberg Elapsed time = " << elapsed_time << "\n";
-        
-//        IngeWntOdeSystem wnt_system_2(WntLevel);
-//        initial_conditions = wnt_system.GetInitialConditions();
-//
-//        h_value = 0.001;
-//
-//        start_time = std::clock();
-//        solutions = back_solver.Solve(&wnt_system_2, initial_conditions, 0.0, 100.0, h_value, h_value);
-//        end_time = std::clock();
-//        elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
-//        std::cout <<  "1. BackwardEuler Elapsed time = " << elapsed_time << "\n";
-
-        // Test solutions are OK for a small time increase...
-        int end = solutions.rGetSolutions().size() - 1;
-        // Tests the simulation is ending at the right time...(going into S phase at 5.971 hours)
-        TS_ASSERT_DELTA(solutions.rGetTimes()[end] , 5.971 , 1e-2);
-        // Proper values from MatLab ode15s - shocking tolerances to pass though.
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0],2.880603485931000e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1],1.000220438771564e+00, 1.01e-2);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2],2.453870380958196e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][3],1.446185835615586e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][4],1.383272155041549e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][5],4.975124378109454e-03, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][6]+solutions.rGetSolutions()[end][7],6.002649406788524e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][8],1.00, 1e-3);
-    }
-    
-    void xTestWntCellCycleSolverWithAPCSingleHit() throw(Exception)
-    {
-        double WntLevel = 1.0;
-        IngeWntOdeSystem wnt_system(WntLevel,APC_ONE_HIT);
-        // Solve system using rk4 solver
-        // Matlab's strictest bit uses 0.01 below and relaxes it on flatter bits.
-        
-        double h_value=0.001;
-        
-        RungeKutta4IvpOdeSolver rk4_solver;
-        BackwardEulerIvpOdeSolver back_solver(9);
-        
-        OdeSolution solutions;
-        //OdeSolution solutions2;
-        
-        std::vector<double> initial_conditions = wnt_system.GetInitialConditions();
-                
-        solutions = rk4_solver.Solve(&wnt_system, initial_conditions, 0.0, 100.0, h_value, h_value);
-        
-        // Test solutions are OK for a small time increase...
-        int end = solutions.rGetSolutions().size() - 1;
-        // Tests the simulation is ending at the right time...(going into S phase at 3.94 hours)
-        TS_ASSERT_DELTA(solutions.rGetTimes()[end] , 4.804 , 1e-2);
-        // Proper values from MatLab ode15s - shocking tolerances to pass though.
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0],2.493601889546602e-01, 1e-2);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1],1.0, 1e-2);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2],2.922278616458120e+00, 1e-2);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][3],1.913484075138688e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][4],1.583557222931072e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][5],4.975124378109454e-03, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][6],0.375, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][7],0.375, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][8],1.00, 1e-3);
-    }
-    
-    void xTestWntCellCycleSolverWithBetaCateninHit() throw(Exception)
-    {
-        double WntLevel = 0.0;
-        IngeWntOdeSystem wnt_system(WntLevel,BETA_CATENIN_ONE_HIT);
-        // Solve system using rk4 solver
-        // Matlab's strictest bit uses 0.01 below and relaxes it on flatter bits.
-        
-        double h_value=0.001;
-        
-        RungeKutta4IvpOdeSolver rk4_solver;
-        BackwardEulerIvpOdeSolver back_solver(9);
-        
-        OdeSolution solutions;
-        //OdeSolution solutions2;
-        
-        std::vector<double> initial_conditions = wnt_system.GetInitialConditions();
+        // Set Wnt to one at time zero...
+        initial_conditions[16] = 1.0;
         
         //double start_time, end_time, elapsed_time = 0.0;
         //start_time = std::clock();
-        solutions = rk4_solver.Solve(&wnt_system, initial_conditions, 0.0, 100.0, h_value, h_value);
+        solutions = rk4_solver.Solve(&wnt_system, initial_conditions, 0.0, 8.0, h_value, h_value);
         //end_time = std::clock();
         //elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
         //std::cout <<  "1. Runge-Kutta Elapsed time = " << elapsed_time << "\n";
         
-
+        h_value = 0.1;
+        initial_conditions = wnt_system.GetInitialConditions();
+        // Set Wnt to one at time zero...
+        initial_conditions[16] = 1.0;
+        
+        //start_time = std::clock();
+        solutions = rkf_solver.Solve(&wnt_system, initial_conditions, 0.0, 8.0, h_value, 1e-4);
+        //end_time = std::clock();
+        //elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
+        //std::cout <<  "2. Runge-Kutta-Fehlberg Elapsed time = " << elapsed_time << "\n";
+        
         // Test solutions are OK for a small time increase...
         int end = solutions.rGetSolutions().size() - 1;
-        // Tests the simulation is ending at the right time...(going into S phase at 7.81 hours)
-        TS_ASSERT_DELTA(solutions.rGetTimes()[end] , 7.82 , 1e-2);
         // Proper values from MatLab ode15s - shocking tolerances to pass though.
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0],3.242663439545868e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1],0.999, 1e-2);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2],2.153277726022381e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][3],1.145736207245425e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][4],1.234257806221668e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][5],1.0, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][6],3.707764665147012e-03, 1e-5);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][7],0.5, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][8],0.00, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0],0.1428, 1e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1],0.0286, 1.01e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2],0.1959, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][3],10.907, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][4],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][5],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][6],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][7],8.1325, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][8],63.3473, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][9],0.0, 1.01e-2);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][10],23.5696, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][11],10.2824, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][12],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][13],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][14],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][15],1.6411, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][16],1.00, 1e-3);
     }
     
-    void xTestWntCellCycleSolverWithAPCDoubleHit() throw(Exception)
+    void TestWntCellCycleSolverHypothesisTwo() throw(Exception)
     {
         double WntLevel = 0.0;
-        IngeWntOdeSystem wnt_system(WntLevel,APC_TWO_HIT);
+        IngeWntOdeSystem wnt_system(WntLevel,LABELLED);
+        wnt_system.SetUseHypothesisTwo(true);
+        
         // Solve system using rk4 solver
         // Matlab's strictest bit uses 0.01 below and relaxes it on flatter bits.
         
-        double h_value=0.001;
+        double h_value=0.0001;
         
         RungeKutta4IvpOdeSolver rk4_solver;
-        BackwardEulerIvpOdeSolver back_solver(9);
+        RungeKuttaFehlbergIvpOdeSolver rkf_solver;
         
         OdeSolution solutions;
         //OdeSolution solutions2;
         
         std::vector<double> initial_conditions = wnt_system.GetInitialConditions();
+        // Set Wnt to one at time zero...
+        initial_conditions[16] = 1.0;
         
-        solutions = rk4_solver.Solve(&wnt_system, initial_conditions, 0.0, 100.0, h_value, h_value);
-
+        //double start_time, end_time, elapsed_time = 0.0;
+        //start_time = std::clock();
+        solutions = rk4_solver.Solve(&wnt_system, initial_conditions, 0.0, 8.0, h_value, h_value);
+        //end_time = std::clock();
+        //elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
+        //std::cout <<  "1. Runge-Kutta Elapsed time = " << elapsed_time << "\n";
+        
+//        h_value = 0.0001;
+//        initial_conditions = wnt_system.GetInitialConditions();
+//        // Set Wnt to one at time zero...
+//        initial_conditions[16] = 1.0;
+//        
+//        start_time = std::clock();
+//        solutions = rkf_solver.Solve(&wnt_system, initial_conditions, 0.0, 8.0, h_value, 1e-3);
+//        end_time = std::clock();
+//        elapsed_time = (end_time - start_time)/(CLOCKS_PER_SEC);
+//        std::cout <<  "2. Runge-Kutta-Fehlberg Elapsed time = " << elapsed_time << "\n";
+        
         // Test solutions are OK for a small time increase...
         int end = solutions.rGetSolutions().size() - 1;
-        // Tests the simulation is ending at the right time...(going into S phase at 3.94 hours)
-        TS_ASSERT_DELTA(solutions.rGetTimes()[end] , 3.9435 , 1e-2);
         // Proper values from MatLab ode15s - shocking tolerances to pass though.
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0],2.058373151310055e-01, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1],0.999, 1e-2);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2],3.699024514542648e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][3],2.687523235896298e+00, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][4],1.834144555072084e-01, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][0],0.1428, 1e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][1],0.0286, 1.01e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][2],0.2113, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][3],0.9385, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][4],13.2706, 1e-3);
         TS_ASSERT_DELTA(solutions.rGetSolutions()[end][5],0.0, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][6],0.5, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][7],0.5, 1e-3);
-        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][8],0.00, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][6],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][7],10.0016, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][8],6.705, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][9],0.0, 1.01e-2);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][10],23.8358, 1e-4);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][11],0.8948, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][12],12.6524, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][13],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][14],0.0, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][15],2.1008, 1e-3);
+        TS_ASSERT_DELTA(solutions.rGetSolutions()[end][16],1.00, 1e-3);
     }
+    
+    
     
 };
 
