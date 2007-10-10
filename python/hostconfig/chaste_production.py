5128821789aae53e944c3519322a6c1c4bdda132
Miguel Bernabeu <miguel.bernabeu@ucl.ac.uk>
2007-10-10 16:16:02 +0000

    Adding a production build configuration. For the time-being, use as follows:  * Do InstallPetscAndMpiForProductionBuild  * Copy python/hostconfig/chaste_production.py to python/hostconfig/chaste.py  * Build with {{{scons build=IntelP3}}} (or any other optimised build type)
    A proper production build type should follow shortly  
    
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2725 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/hostconfig/chaste_production.py
+++ python/hostconfig/chaste_production.py
@@ -6,14 +6,15 @@ petsc_2_2_path = '../../../petsc-2.2.1/'
 petsc_2_3_path = '../../../petsc-2.3.2-p4/'
 petsc_build_name = 'linux-gnu'
 petsc_build_name_profile = 'linux-gnu-profile'
-petsc_build_name_optimized = 'linux-gnu-opt'
+petsc_build_name_optimized = 'linux-intel-opt-mkl'
 dealii_path = '../../../deal.II/'
 metis_path = '../../../metis-4.0/'
 intel_path = '/opt/intel/cc/9.1.039'
 
 other_includepaths = ['../../../xsd-2.3.1-i686-linux-gnu/libxsd']
-other_libpaths = [os.path.join(petsc_2_3_path, 'externalpackages/f2cblaslapack/linux-gnu/')]
-blas_lapack = ['f2clapack', 'f2cblas']
+other_libpaths = ['/opt/intel/mkl/9.1.023/lib/32', '/opt/intel/cc/9.1.039/lib/']
+blas_lapack = ['mkl_lapack', 'mkl']
 other_libraries = ['boost_serialization', 'xerces-c']
+
 
 tools = {}
