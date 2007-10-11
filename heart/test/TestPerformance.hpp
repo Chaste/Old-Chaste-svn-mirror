a47ae68733b35d92b09cd9c21dcd2fc214e19a82
Lee Momtahan <leemo@comlab.ox.ac.uk>
2007-10-11 16:00:28 +0000

    Added some performance tests to overnight build
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2734 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestPerformance.hpp
+++ heart/test/TestPerformance.hpp
@@ -25,16 +25,65 @@ public:
         PerformanceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<2>, 2>::DisplayHeadings();
         EventHandler::Headings();
 
-        // base line
+        // base line test
         PerformanceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<2>, 2> tester;
-        tester.SimTime=0.0025;
+        tester.MeshNum=2;
+        tester.SimTime=4.0;
+        tester.Run();
+        EventHandler::Report();
         
-        for (unsigned mesh_num=0; mesh_num<3; mesh_num++)
-        {
-            tester.MeshNum=mesh_num;
-            tester.Run();
-            EventHandler::Report();      
-        }
+        // vary simulation time
+        tester.SimTime=0.0025;
+        tester.Run();
+        EventHandler::Report();
+        
+        tester.SimTime=8.0;
+        tester.Run();
+        EventHandler::Report();
+        
+        tester.SimTime=4.0;
+        
+        // vary pde time step
+        tester.PdeTimeStep=0.005;
+        tester.Run();
+        EventHandler::Report();
+        
+        tester.PdeTimeStep=0.01;
+        tester.Run();
+        EventHandler::Report();
+                
+        tester.PdeTimeStep=0.0025;        
+        // vary ode time step
+        tester.OdeTimeStep=0.0025/2;
+        tester.Run();
+        EventHandler::Report();        
+ 
+        tester.OdeTimeStep=0.0025/4;
+        tester.Run();
+        EventHandler::Report();        
+
+        tester.OdeTimeStep=0.0025;
+        
+        // vary printing time step
+        tester.PrintingTimeStep = 0.02;
+        tester.Run();
+        EventHandler::Report();
+        
+        tester.PrintingTimeStep = 0.01;
+        tester.Run();
+        EventHandler::Report();
+
+        tester.PrintingTimeStep = 0.04;
+        
+        // vary mesh size
+        tester.MeshNum++;
+        tester.Run();
+        EventHandler::Report();   
+        
+        tester.MeshNum++;
+        tester.Run();
+        EventHandler::Report();      
+        
         
     }
 };
---
+++ heart/test/TestPerformanceOfAssembly.hpp
@@ -0,0 +1,42 @@
+#ifndef TESTPERFORMANCEOFASSEMBLY_HPP_
+#define TESTPERFORMANCEOFASSEMBLY_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include "BidomainProblem.hpp"
+#include "MonodomainProblem.hpp"
+#include <petscvec.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#include "BackwardEulerLuoRudyIModel1991.hpp"
+#include "LuoRudyIModel1991OdeSystem.hpp"
+#include "PerformanceTester.hpp"
+
+class TestPerformanceOfAssembly : public CxxTest::TestSuite
+{   
+public:
+
+    
+    void TestPerf() throw(Exception)
+    {
+        // write headings
+        PerformanceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<2>, 2>::DisplayHeadings();
+        EventHandler::Headings();
+
+        // base line
+        PerformanceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<2>, 2> tester;
+        tester.SimTime=0.0025;
+        
+        for (unsigned mesh_num=0; mesh_num<3; mesh_num++)
+        {
+            tester.MeshNum=mesh_num;
+            tester.Run();
+            EventHandler::Report();      
+        }
+        
+    }
+};
+
+#endif /*TESTPERFORMANCE_HPP_*/
