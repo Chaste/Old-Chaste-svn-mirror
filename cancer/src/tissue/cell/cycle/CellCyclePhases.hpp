1145366e2f1e74aa6e10f2816618a458f9cb4a3a
Pras Pathmanathan <pras@cs.ox.ac.uk>
2007-10-25 11:02:41 +0000

    Comments added for ticket:519
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2793 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cancer/src/tissue/cell/cycle/CellCyclePhases.hpp
+++ cancer/src/tissue/cell/cycle/CellCyclePhases.hpp
@@ -2,7 +2,13 @@
 #define CELLCYCLEPHASES_HPP_
 
 /**
- * Possible phases of the cell cycle
+ * Possible phases of the cell cycle.
+ * 
+ * When our cells 'divide' they are actually entering M phase,
+ * so a cell progresses round the cell cycle in the following sequence from its birth time
+ * Divide-> M -> G0/G1 -> S -> G2 -> Divide.
+ * 
+ * G0 is a cell which stays in the G1 phase and is not going to divide. (i.e. quiescent or differentiated.)
  */
 typedef enum CellCyclePhase_
 {
