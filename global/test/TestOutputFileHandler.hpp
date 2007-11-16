2a1b805b6f85789266fe6f67c70cd972b01516dc
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2007-11-16 09:55:46 +0000

    Hopefully fix TestOutputFileHandler in parallel.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2944 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- global/test/TestOutputFileHandler.hpp
+++ global/test/TestOutputFileHandler.hpp
@@ -24,20 +24,24 @@ public:
         TS_ASSERT_EQUALS(full_dir.substr(full_dir.length()-dir.length()-1), dir+"/");
         TS_ASSERT_EQUALS(full_dir, handler2.GetOutputDirectoryFullPath());
         
-        out_stream p_file_stream;
-        TS_ASSERT_THROWS_NOTHING(p_file_stream = handler.OpenOutputFile("test_file",
+        // Only the master process should write to disk
+        if (handler.IsMaster())
+        {
+            out_stream p_file_stream;
+            TS_ASSERT_THROWS_NOTHING(p_file_stream = handler.OpenOutputFile("test_file",
                                                  std::ios::out));
                                                  
-        TS_ASSERT_THROWS_NOTHING(p_file_stream = handler.OpenOutputFile("test_file"));
+            TS_ASSERT_THROWS_NOTHING(p_file_stream = handler.OpenOutputFile("test_file"));
         
-        TS_ASSERT_THROWS_NOTHING(p_file_stream = handler2.OpenOutputFile("test_file"));
+            TS_ASSERT_THROWS_NOTHING(p_file_stream = handler2.OpenOutputFile("test_file"));
  
-        TS_ASSERT_THROWS_NOTHING(p_file_stream = handler2.OpenOutputFile("test_",34,".txt"));
+            TS_ASSERT_THROWS_NOTHING(p_file_stream = handler2.OpenOutputFile("test_",34,".txt"));
         
-        // This should try to write files to /, which isn't allowed (we hope!)
-        OutputFileHandler handler3("../../../../../../../../../../../../../../../",
-                                   false);
-        TS_ASSERT_THROWS_ANYTHING(p_file_stream = handler3.OpenOutputFile("test_file"));
+            // This should try to write files to /, which isn't allowed (we hope!)
+            OutputFileHandler handler3("../../../../../../../../../../../../../../../",
+                                       false);
+            TS_ASSERT_THROWS_ANYTHING(p_file_stream = handler3.OpenOutputFile("test_file"));
+        }
         
         // Test that the Chaste directory is meaningful, just for coverage purposes
         
@@ -45,13 +49,13 @@ public:
         
         setenv("CHASTE_TEST_OUTPUT", "", 1/*Overwrite*/);
         
-        handler3.GetOutputDirectoryFullPath("whatever");
+        handler.GetOutputDirectoryFullPath("whatever");
         
         rmdir("whatever");
         
         setenv("CHASTE_TEST_OUTPUT", "somewhere_without_trailing_forward_slash", 1/*Overwrite*/);
         
-        handler3.GetOutputDirectoryFullPath("whatever");
+        handler.GetOutputDirectoryFullPath("whatever");
         
         rmdir("somewhere_without_trailing_forward_slash/whatever");
         rmdir("somewhere_without_trailing_forward_slash");
