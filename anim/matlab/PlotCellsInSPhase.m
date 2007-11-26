dc584dee9288b40c84627c6a97410c57e66acb29
Gary Mirams <gary.mirams@gmail.com>
2007-11-26 15:20:40 +0000

    #521 What is really happening vs. virtual dissections.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@2977 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- anim/matlab/PlotCellsInSPhase.m
+++ anim/matlab/PlotCellsInSPhase.m
@@ -1,8 +1,27 @@
-% PlotCellsInSPhase
+% PlotCellsInSPhase.m
+%
+% This code needs FirstAndLastScript.m to have been run on the relevant set
+% of results.
+%
+% It reads in the positions of all cells at the beginning and end of a 
+% Meineke-stlye labelling experiment and plots the percentages of cells 
+% that are labelled in ranges 
+%
 
+close all
+clear
+
+% Experiment Setup
+title_string = 'Meineke Cells in Sunter iii) Geometry';
+runs = 4;
 Start_time = 300;
-Num_experiments = 50;
-crypt_height = 30;
+Num_experiments = 51;
+crypt_height = 20;
+file_path(1,:) = '/local/pmxgm/Simulation_Results/16_stem_cell_Meineke_recreate/sunter3/2007-11-22-19-15/MeinekeLabellingExperiment';
+file_path(2,:) = '/local/pmxgm/Simulation_Results/16_stem_cell_Meineke_recreate/sunter3/2007-11-22-19-17/MeinekeLabellingExperiment';
+file_path(3,:) = '/local/pmxgm/Simulation_Results/16_stem_cell_Meineke_recreate/sunter3/2007-11-22-19-19/MeinekeLabellingExperiment';
+file_path(4,:) = '/local/pmxgm/Simulation_Results/16_stem_cell_Meineke_recreate/sunter3/2007-11-22-19-20/MeinekeLabellingExperiment';
+% End of setup
 
 y_all_40min = [];
 y_all_9hrs = [];
@@ -14,57 +33,59 @@ total_num_in_each_bucket_9hrs = 0*buckets(1:end-1);
 marked_num_in_each_bucket_40min = 0*buckets(1:end-1);
 marked_num_in_each_bucket_9hrs = 0*buckets(1:end-1);
 
-%	Get data for each exp
-for i=1:Num_experiments
-	disp('')
-	Experiment_time = Start_time + 10*(i-1);
-	FileName = ['/local/pmxaw/MeinekeLabellingExperiment/results_from_time_' int2str(Experiment_time) '.667/vis_results/first_line.txt'];
-	
-	vis_nodes_40min = LoadNonConstantLengthData(FileName);
-	
-	%	Loop over nodes and if it is marked get y value and plonk in a bucket
-	num_nodes_40min = (length(vis_nodes_40min{1})-1)/3;
-	
-	for j = 1:num_nodes_40min
-		y_val = vis_nodes_40min{1}(3*j);
-		cell_type = vis_nodes_40min{1}(3*j + 1);
-		for k = 1:length(buckets)
-			if y_val >= buckets(k) && y_val < buckets(k+1)
-				total_num_in_each_bucket_40min(k) = total_num_in_each_bucket_40min(k) + 1;
-				if  cell_type == 5
-					marked_num_in_each_bucket_40min(k) = marked_num_in_each_bucket_40min(k) + 1;
-				end
-				
-				break;
-			end
-		end
-	end
-	
-	
-	FileName = ['/local/pmxaw/MeinekeLabellingExperiment/results_from_time_' int2str(Experiment_time) '.667/vis_results/last_line.txt'];
-	
-	vis_nodes_9hrs = LoadNonConstantLengthData(FileName);
-	
-	num_nodes_9hrs = (length(vis_nodes_9hrs{1})-1)/3;
-	
-	
-	for j = 1:num_nodes_9hrs
-		y_val = vis_nodes_9hrs{1}(3*j);
-		cell_type = vis_nodes_9hrs{1}(3*j + 1);
-		for k = 1:length(buckets)
-			if y_val >= buckets(k) && y_val < buckets(k+1)
-				total_num_in_each_bucket_9hrs(k) = total_num_in_each_bucket_9hrs(k) + 1;
-				if  cell_type == 5
-					marked_num_in_each_bucket_9hrs(k) = marked_num_in_each_bucket_9hrs(k) + 1;
-				end
-				
-				break;
-			end
-		end
-	end
-	
-	
-	
+for run = 1:runs
+    %	Get data for each exp
+    for i=1:Num_experiments
+        disp('')
+        Experiment_time = Start_time + 10*(i-1);
+        temp_string = deblank(file_path(run,:));
+        FileName = [temp_string '/results_from_time_' int2str(Experiment_time) '.667/vis_results/first_line.txt'];
+
+        vis_nodes_40min = LoadNonConstantLengthData(FileName);
+
+        %	Loop over nodes and if it is marked get y value and plonk in a bucket
+        num_nodes_40min = (length(vis_nodes_40min{1})-1)/3;
+
+        for j = 1:num_nodes_40min
+            y_val = vis_nodes_40min{1}(3*j);
+            cell_type = vis_nodes_40min{1}(3*j + 1);
+            for k = 1:length(buckets)
+                if y_val >= buckets(k) && y_val < buckets(k+1)
+                    total_num_in_each_bucket_40min(k) = total_num_in_each_bucket_40min(k) + 1;
+                    if  cell_type == 5
+                        marked_num_in_each_bucket_40min(k) = marked_num_in_each_bucket_40min(k) + 1;
+                    end
+
+                    break;
+                end
+            end
+        end
+
+
+        FileName = [temp_string '/results_from_time_' int2str(Experiment_time) '.667/vis_results/last_line.txt'];
+
+        vis_nodes_9hrs = LoadNonConstantLengthData(FileName);
+
+        num_nodes_9hrs = (length(vis_nodes_9hrs{1})-1)/3;
+
+
+        for j = 1:num_nodes_9hrs
+            y_val = vis_nodes_9hrs{1}(3*j);
+            cell_type = vis_nodes_9hrs{1}(3*j + 1);
+            for k = 1:length(buckets)
+                if y_val >= buckets(k) && y_val < buckets(k+1)
+                    total_num_in_each_bucket_9hrs(k) = total_num_in_each_bucket_9hrs(k) + 1;
+                    if  cell_type == 5
+                        marked_num_in_each_bucket_9hrs(k) = marked_num_in_each_bucket_9hrs(k) + 1;
+                    end
+
+                    break;
+                end
+            end
+        end
+
+    end
+
 end
 
 percent_in_each_bucket_40min = 0*total_num_in_each_bucket_40min;
@@ -80,7 +101,10 @@ for i = 1:length(buckets)-1
 end
 
 figure;
-subplot(2,1,1)
-bar(buckets(1:end-1)+0.5,percent_in_each_bucket_40min)
-subplot(2,1,2)
-bar(buckets(1:end-1)+0.5,percent_in_each_bucket_9hrs)
+bar(buckets(1:end-1)+0.5,percent_in_each_bucket_9hrs,'r')
+hold on
+bar(buckets(1:end-1)+0.5,percent_in_each_bucket_40min,'b')
+title([title_string '. After 40 minutes and 9 hours for ' int2str(runs*Num_experiments) ' experiments.']);
+xlabel('Height up crypt (cells)');
+ylabel('% of labelled cells');
+ylim([0 100]);
