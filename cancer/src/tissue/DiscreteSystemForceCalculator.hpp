d80c383f75ac53a669f74e7eaebc808428e8daea
Gary Mirams <gary.mirams@gmail.com>
2007-12-04 18:53:20 +0000

    #581 finished coding up the class `DiscreteSystemForceCalculator`
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3027 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cancer/src/tissue/DiscreteSystemForceCalculator.hpp
+++ cancer/src/tissue/DiscreteSystemForceCalculator.hpp
@@ -6,10 +6,25 @@
 
 class DiscreteSystemForceCalculator
 {
-public:
-    Meineke2001SpringSystem<2>& mrMeinekeSpringSystem; 
+    friend class TestDiscreteSystemForceCalculator;
     
-    std::vector<double> CalculateFtAndFn(unsigned index, double theta)
+private:
+    
+    /**
+     * Spring system, used to get mesh and calculate forces between nodes.
+     */ 
+    Meineke2001SpringSystem<2>& mrMeinekeSpringSystem;
+    
+    /**
+     * Small parameter, used in GetSamplingAngles().
+     */ 
+    double mEpsilon; 
+    
+    
+    /**
+     * Given a node index, returns the set of neighbouring node indices.
+     */ 
+    std::set<unsigned> GetNeighbouringNodeIndices(unsigned index)
     {
         ConformingTetrahedralMesh<2,2>& r_mesh = mrMeinekeSpringSystem.GetTissue().rGetMesh();
         Node<2>* p_node = r_mesh.GetNode(index);
@@ -30,6 +45,18 @@ public:
                 }
             }
         }
+        return neighbouring_node_indices;
+    }
+    
+    
+    /**
+     * Given a node index and angle of intersecting line, returns the tangential and normal forces.
+     */ 
+    std::vector<double> CalculateFtAndFn(unsigned index, double theta)
+    {
+        ConformingTetrahedralMesh<2,2>& r_mesh = mrMeinekeSpringSystem.GetTissue().rGetMesh();
+        
+        std::set<unsigned> neighbouring_node_indices = GetNeighbouringNodeIndices(index);
         
         double total_tangential_force = 0.0;
         double total_normal_force = 0.0;
@@ -43,12 +70,14 @@ public:
             if(sin(alpha-theta)>0)
             {
                 c_vector<double,2> force_between_nodes = mrMeinekeSpringSystem.CalculateForceBetweenNodes(index,*iter);
-
-                c_vector<double,2> unit_vec_betweeen_nodes;
-                unit_vec_betweeen_nodes[0] = cos(alpha);
-                unit_vec_betweeen_nodes[1] = sin(alpha);
-
-                double plusminus_norm_force = inner_prod(force_between_nodes,unit_vec_betweeen_nodes);         
+                //std::cout << "force between nodes = " << force_between_nodes[0] << "," <<  force_between_nodes[1] << "\n" << std::flush;
+                
+                c_vector<double,2> unit_vec_between_nodes;
+                unit_vec_between_nodes[0] = cos(alpha);
+                unit_vec_between_nodes[1] = sin(alpha);
+                //std::cout << "unit vector = " << unit_vec_between_nodes[0] << "," <<  unit_vec_between_nodes[1] << "\n" << std::flush;
+                double plusminus_norm_force = inner_prod(force_between_nodes,unit_vec_between_nodes);         
+                
                 total_tangential_force += plusminus_norm_force * cos(alpha-theta);
                 total_normal_force += plusminus_norm_force * sin(alpha-theta);
             }
@@ -60,12 +89,254 @@ public:
         
         return ret;
     }
+    
+    
+    /**
+     * Given a node index, returns a vector of sampling angles which may be used
+     * by GetExtremalAngles() to find the locations of local extrema of the normal force.
+     */  
+    std::vector<double> GetSamplingAngles(unsigned index)
+    {
+        ConformingTetrahedralMesh<2,2>& r_mesh = mrMeinekeSpringSystem.GetTissue().rGetMesh();
+        std::set<unsigned> neighbouring_node_indices = GetNeighbouringNodeIndices(index);
+        
+        std::vector<double> sampling_angles(4*neighbouring_node_indices.size());
+        
+        unsigned i=0;
+        for(std::set<unsigned>::iterator iter = neighbouring_node_indices.begin();
+            iter != neighbouring_node_indices.end();
+            iter++)
+        {
+            double alpha = r_mesh.GetAngleBetweenNodes(index, *iter);
+            double alpha_minus_epsilon = alpha - mEpsilon;
+            double alpha_plus_epsilon = alpha + mEpsilon;
+            double alpha_minus_epsilon_plus_pi = alpha - mEpsilon + M_PI;
+            double alpha_plus_epsilon_plus_pi = alpha + mEpsilon + M_PI;
+            
+            if (alpha_minus_epsilon > 2*M_PI)
+            {
+                sampling_angles[i] = alpha_minus_epsilon - 2*M_PI;
+            }
+            else if (alpha_minus_epsilon < 0)
+            {
+                sampling_angles[i] = alpha_minus_epsilon + 2*M_PI;
+            }
+            else
+            {
+                sampling_angles[i] = alpha_minus_epsilon;
+            }
+            assert(sampling_angles[i] < 2*M_PI);
+            assert(sampling_angles[i] >= 0);
+            i++;
+            
+            if (alpha_plus_epsilon > 2*M_PI)
+            {
+                sampling_angles[i] = alpha_plus_epsilon - 2*M_PI;
+            }
+            else if (alpha_plus_epsilon < 0)
+            {
+                sampling_angles[i] = alpha_plus_epsilon + 2*M_PI;
+            }
+            else
+            {
+                sampling_angles[i] = alpha_plus_epsilon;
+            }
+            assert(sampling_angles[i] < 2*M_PI);
+            assert(sampling_angles[i] >= 0);
+            i++;
+            
+            if (alpha_minus_epsilon_plus_pi > 2*M_PI)
+            {
+                sampling_angles[i] = alpha_minus_epsilon_plus_pi - 2*M_PI;
+            }
+            else if (alpha_minus_epsilon_plus_pi < 0)
+            {
+                sampling_angles[i] = alpha_minus_epsilon_plus_pi + 2*M_PI;
+            }
+            else
+            {
+                sampling_angles[i] = alpha_minus_epsilon_plus_pi;
+            }
+            assert(sampling_angles[i] < 2*M_PI);
+//            std::cout << sampling_angles[i] << "\n" << std::flush;
+            assert(sampling_angles[i] >= 0);
+            i++;
+            
+            if (alpha_plus_epsilon_plus_pi > 2*M_PI)
+            {
+                sampling_angles[i] = alpha_plus_epsilon_plus_pi - 2*M_PI;
+            }
+            else if (alpha_plus_epsilon_plus_pi < 0)
+            {
+                sampling_angles[i] = alpha_plus_epsilon_plus_pi + 2*M_PI;
+            }
+            else
+            {
+                sampling_angles[i] = alpha_plus_epsilon_plus_pi;
+            }
+            assert(sampling_angles[i] < 2*M_PI);
+            assert(sampling_angles[i] >= 0);
+            i++;            
+        }
+        
+        sort(sampling_angles.begin(), sampling_angles.end());
+        
+        return sampling_angles;
+    }
+    
+    
+    /**
+     * Given a node index and two sampling angles, finds the location of the root 
+     * of the tangential force in the interval between the two angles.
+     */ 
+    double GetLocalExtremum(unsigned index, double angle1, double angle2)
+    {        
+        assert(angle1 < angle2);
+        
+        double tolerance = 1e-5;
+        unsigned counter = 0;
+                
+        double previous_angle;        
+        double current_error;     
+        double current_angle = angle1; 
+        
+        current_error = angle2 - angle1;            
+        std::vector<double> current_ft_and_fn(2);
+        
+        while (current_error>tolerance)
+        {
+            previous_angle = current_angle;
+            current_ft_and_fn = CalculateFtAndFn(index,current_angle);
+            current_angle -= current_ft_and_fn[0]/current_ft_and_fn[1];
+            current_error = fabs(current_angle - previous_angle);
+            counter++;
+        }
+        
+        assert( current_angle>angle1 && current_angle<angle2 );            
+        assert( current_error < tolerance );        
+        
+        if (current_angle<0.0)
+        {
+            current_angle += 2*M_PI;
+        }
+        return current_angle;        
+    }
+    
 
-
+    /**
+     * Given a vector of sampling angles, returns a vector of extremal angles, i.e. angles 
+     * at which local extrema of the normal force occur.
+     */ 
+    std::vector<double> GetExtremalAngles(unsigned index, std::vector<double> samplingAngles)
+    {
+        std::vector<double> extremal_angles;
+        std::vector<double> ft_and_fn(2);
+        std::vector<double> tangential_force(samplingAngles.size());
+        
+        for (unsigned i=0; i<samplingAngles.size(); i++)
+        {
+            ft_and_fn = CalculateFtAndFn(index, samplingAngles[i]);
+            tangential_force[i] = ft_and_fn[0];
+        }         
+        
+        for (unsigned i=0; i<samplingAngles.size()-1; i++)
+        {
+            if ( ( (tangential_force[i] > 0) && (tangential_force[i+1] < 0) ) ||  
+                 ( (tangential_force[i] < 0) && (tangential_force[i+1] > 0) ) )
+            {
+                double next_extremal_angle;
+                
+                // if we jump through zero, then we know exactly where the local extremum is
+                if (samplingAngles[i+1] - samplingAngles[i] < 2*mEpsilon + 1e-6 )
+                {
+                    next_extremal_angle = (samplingAngles[i+1] + samplingAngles[i])/2.0;
+                    extremal_angles.push_back(next_extremal_angle);
+                }
+                else
+                {
+                    next_extremal_angle = GetLocalExtremum(index, samplingAngles[i], samplingAngles[i+1]);
+                    extremal_angles.push_back(next_extremal_angle);
+                }
+            }
+        }
+        
+        // separate bit of code for the interval from the last sampling angel back round to the first...
+        
+        samplingAngles[samplingAngles.size()-1] -= 2*M_PI;
+        
+        if ( ( (tangential_force[samplingAngles.size()-1] > 0) && (tangential_force[0] < 0) ) ||  
+                 ( (tangential_force[samplingAngles.size()-1] < 0) && (tangential_force[0] > 0) ) )
+        {
+            double next_extremal_angle;
+            
+            // if we jump through zero, then we know exactly where the local extremum is
+            if ( samplingAngles[0] - samplingAngles[samplingAngles.size()-1] < 2*mEpsilon + 1e-6 )
+            {
+                next_extremal_angle = (samplingAngles[0] + samplingAngles[samplingAngles.size()-1])/2.0;
+                extremal_angles.push_back(next_extremal_angle);
+            }
+            else
+            {
+                next_extremal_angle = GetLocalExtremum(index, samplingAngles[samplingAngles.size()-1], samplingAngles[0]);
+                extremal_angles.push_back(next_extremal_angle);
+            }
+        }
+        
+        return extremal_angles;        
+    }
+    
 public:
+    
     DiscreteSystemForceCalculator(Meineke2001SpringSystem<2>& rMeinekeSpringSystem)
-        : mrMeinekeSpringSystem(rMeinekeSpringSystem)
+        : mrMeinekeSpringSystem(rMeinekeSpringSystem),
+          mEpsilon(0.01)
     {
     }
+    
+    std::vector< std::vector<double> > CalculateExtremalNormalForces()
+    {
+        std::vector< std::vector<double> > extremal_normal_forces;
+        
+        ConformingTetrahedralMesh<2,2>& r_mesh = mrMeinekeSpringSystem.GetTissue().rGetMesh();
+        
+        std::vector<double> minimum_normal_forces(r_mesh.GetNumNodes());
+        std::vector<double> maximum_normal_forces(r_mesh.GetNumNodes());
+                
+        for (unsigned i=0; i<r_mesh.GetNumNodes(); i++)
+        {
+            std::vector<double> sampling_angles = GetSamplingAngles(i);            
+            std::vector<double> extremal_angles = GetExtremalAngles(i,sampling_angles);
+
+            double minimum_normal_force_for_node_i = DBL_MAX;
+            double maximum_normal_force_for_node_i = -DBL_MAX;
+            
+            double current_normal_force;
+            
+            for (unsigned j=0; j<extremal_angles.size(); j++)
+            {
+                current_normal_force = CalculateFtAndFn(i,extremal_angles[j])[1];
+                
+                if (current_normal_force > maximum_normal_force_for_node_i)
+                {
+                    maximum_normal_force_for_node_i = current_normal_force;
+                }
+                
+                if (current_normal_force < minimum_normal_force_for_node_i)
+                {
+                    minimum_normal_force_for_node_i = current_normal_force;
+                }
+            }   
+            
+            minimum_normal_forces[i] = minimum_normal_force_for_node_i;
+            maximum_normal_forces[i] = maximum_normal_force_for_node_i;
+            
+        } 
+                
+        extremal_normal_forces.push_back(minimum_normal_forces);
+        extremal_normal_forces.push_back(maximum_normal_forces);
+        
+        return extremal_normal_forces;
+    }
+
 };
 #endif /*DISCRETESYSTEMFORCECALCULATOR_HPP_*/
--- cancer/test/TestDiscreteSystemForceCalculator.hpp
+++ cancer/test/TestDiscreteSystemForceCalculator.hpp
@@ -3,39 +3,181 @@
 
 
 #include <cxxtest/TestSuite.h>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 #include <cmath>
 #include <vector>
+#include "CryptSimulation2d.hpp"
 #include "DiscreteSystemForceCalculator.hpp"
 #include "TrianglesMeshReader.cpp"
 #include "HoneycombMeshGenerator.hpp"
 #include "CellsGenerator.hpp"
+#include "OutputFileHandler.hpp"
+
 
 class TestDiscreteSystemForceCalculator : public CxxTest::TestSuite
 {
 public:
-    void TestCalculateFtAndFn() throw (Exception)
+
+    void TestPrivateMethods() throw (Exception)
     {
-        CancerParameters::Instance()->Reset();
-        SimulationTime::Instance()->SetStartTime(0.0);
-        SimulationTime::Instance()->SetEndTimeAndNumberOfTimeSteps(1.0,1);
-        
-        HoneycombMeshGenerator generator(7, 5, 0, false);
-        ConformingTetrahedralMesh<2,2>* p_mesh=generator.GetMesh();
-        std::set<unsigned> ghost_node_indices = generator.GetGhostNodeIndices();
-        
+        // Set up a tissue 
+        CancerParameters::Instance();
+        SimulationTime::Instance()->SetStartTime(0.0);        
+                
+        HoneycombMeshGenerator mesh_generator(7, 5, 0, false, 2.0);
+        ConformingTetrahedralMesh<2,2>* p_mesh = mesh_generator.GetMesh();
+        std::set<unsigned> ghost_node_indices = mesh_generator.GetGhostNodeIndices();
+                
+        CellsGenerator<2> cells_generator;
         std::vector<TissueCell> cells;
-        CellsGenerator<2>::GenerateForCrypt(cells, *p_mesh, FIXED, true);
+        cells_generator.GenerateBasic(cells, *p_mesh);
+        
         Tissue<2> tissue(*p_mesh, cells);
         tissue.SetGhostNodes(ghost_node_indices);
 
+        // Need to create a spring system explicitly so we can pass 
+        // it in to the force calculator
         Meineke2001SpringSystem<2> meineke_spring_system(tissue);
         
+        // Create a force calculator
+        DiscreteSystemForceCalculator calculator(meineke_spring_system); 
+                      
+        unsigned node_index = 8;
+        
+        // Test GetNeighbouringNodeIndices
+        std::set<unsigned> expected_node_indices;
+        expected_node_indices.insert(1u);
+        expected_node_indices.insert(2u);
+        expected_node_indices.insert(7u);
+        expected_node_indices.insert(9u);
+        expected_node_indices.insert(15u);
+        expected_node_indices.insert(16u);
+        
+        std::set<unsigned> neighbouring_node_indices = calculator.GetNeighbouringNodeIndices(node_index);
+        
+        TS_ASSERT(neighbouring_node_indices==expected_node_indices);
+                
+        // Test CalculateFtAndFn
+        double spring_stiffness = CancerParameters::Instance()->GetSpringStiffness();
+        double expected_ft = spring_stiffness*(cos(M_PI/12.0) + cos(5.0*M_PI/12.0) - cos(3.0*M_PI/12.0));
+        double expected_fn = spring_stiffness*(sin(M_PI/12.0) + sin(5.0*M_PI/12.0) + sin(3.0*M_PI/12.0));
+        std::vector<double> Ft_and_Fn = calculator.CalculateFtAndFn(node_index,M_PI/4.0);
+        TS_ASSERT_DELTA(Ft_and_Fn[0], expected_ft, 1e-4);
+        TS_ASSERT_DELTA(Ft_and_Fn[1], expected_fn, 1e-4);
+                
+        // Test GetSamplingAngles
+        std::vector<double> expected_sampling_angles;
+        double epsilon = calculator.mEpsilon;
+        
+        expected_sampling_angles.push_back(epsilon);
+        expected_sampling_angles.push_back(epsilon);
+        
+        for (unsigned i=1; i<6; i++)
+        {
+            expected_sampling_angles.push_back(((double) i)*M_PI/3.0 - epsilon);
+            expected_sampling_angles.push_back(((double) i)*M_PI/3.0 - epsilon);
+            expected_sampling_angles.push_back(((double) i)*M_PI/3.0 + epsilon);
+            expected_sampling_angles.push_back(((double) i)*M_PI/3.0 + epsilon);
+        }
+        expected_sampling_angles.push_back(2*M_PI - epsilon);
+        expected_sampling_angles.push_back(2*M_PI - epsilon);
+                
+        std::vector<double> sampling_angles = calculator.GetSamplingAngles(node_index);
+        for (unsigned i=0; i<sampling_angles.size(); i++)
+        {
+            TS_ASSERT_DELTA(sampling_angles[i], expected_sampling_angles[i], 1e-6);
+        }
+                         
+        // Test GetLocalExtremum
+        double expected_extremal_angle = M_PI/6.0;
+        double calculated_extremal_angle = calculator.GetLocalExtremum(node_index, sampling_angles[1], sampling_angles[2]);
+        
+        TS_ASSERT_DELTA(calculated_extremal_angle, expected_extremal_angle, 1e-4);
+                        
+        // Test GetExtremalAngles
+        std::vector<double> calculated_extremal_angles = calculator.GetExtremalAngles(node_index, sampling_angles);
+        
+        TS_ASSERT_DELTA(M_PI/6.0, calculated_extremal_angles[0], 1e-4);
+        TS_ASSERT_DELTA(M_PI/3.0, calculated_extremal_angles[1], 1e-4);
+        TS_ASSERT_DELTA(M_PI/2.0, calculated_extremal_angles[2], 1e-4);
+        TS_ASSERT_DELTA(2.0*M_PI/3.0, calculated_extremal_angles[3], 1e-4);
+        TS_ASSERT_DELTA(5.0*M_PI/6.0, calculated_extremal_angles[4], 1e-4);
+        
+        SimulationTime::Destroy();
+        RandomNumberGenerator::Destroy();
+    }
+    
+    void TestCalculateExtremalNormalForces() throw (Exception)
+    {
+        // Set up a tissue 
+        CancerParameters::Instance();
+        SimulationTime::Instance()->SetStartTime(0.0);        
+                
+        HoneycombMeshGenerator mesh_generator(7, 5, 0, false, 2.0);
+        ConformingTetrahedralMesh<2,2>* p_mesh = mesh_generator.GetMesh();
+        std::set<unsigned> ghost_node_indices = mesh_generator.GetGhostNodeIndices();
+                
+        CellsGenerator<2> cells_generator;
+        std::vector<TissueCell> cells;
+        cells_generator.GenerateBasic(cells, *p_mesh);
+        
+        Tissue<2> tissue(*p_mesh, cells);
+        tissue.SetGhostNodes(ghost_node_indices);
+
+        // Need to create a spring system explicitly so we can pass 
+        // it in to the force calculator
+        Meineke2001SpringSystem<2> meineke_spring_system(tissue);
+        
+        // Create a force calculator
         DiscreteSystemForceCalculator calculator(meineke_spring_system); 
         
-        std::vector<double> Ft_and_Fn = calculator.CalculateFtAndFn(8,1.0);
-        TS_ASSERT_DELTA(Ft_and_Fn[0], 0.0, 1e-5);
-        TS_ASSERT_DELTA(Ft_and_Fn[1], 0.0, 1e-5);
+        // Test CalculateExtremalNormalForces
+        std::vector< std::vector<double> > calculated_results = calculator.CalculateExtremalNormalForces();
+        TS_ASSERT_EQUALS(calculated_results.size(), 2u);
+        TS_ASSERT_EQUALS(calculated_results[0].size(), p_mesh->GetNumNodes());
+        TS_ASSERT_EQUALS(calculated_results[1].size(), p_mesh->GetNumNodes());
+        
+        double spring_stiffness = CancerParameters::Instance()->GetSpringStiffness();
+        double expected_minimum_interior = spring_stiffness*( 2.0*sin(M_PI/3.0) );
+        double expected_maximum_interior = spring_stiffness*( sin(M_PI/6.0) + sin(M_PI/2.0) + sin(5.0*M_PI/6.0) );
+                                
+        for (unsigned i=0; i<p_mesh->GetNumNodes(); i++)
+        {
+            if ( !(p_mesh->GetNode(i)->IsBoundaryNode()) )
+            {
+                TS_ASSERT_DELTA( calculated_results[0][i], expected_minimum_interior, 1e-4);
+                TS_ASSERT_DELTA( calculated_results[1][i], expected_maximum_interior, 1e-4);
+            }
+            else
+            {
+                // Separate cases for the boundary nodes...
+                if (i==29 || i==30 || i==31 || i==32 || i==33)
+                {
+                    TS_ASSERT_DELTA( calculated_results[0][i], 0.0, 1e-4);
+                    TS_ASSERT_DELTA( calculated_results[1][i], expected_maximum_interior, 1e-4);
+                }
+                if (i==7 || i==20 || i==21)
+                {
+                    TS_ASSERT_DELTA( calculated_results[0][i], spring_stiffness*sin(M_PI/3.0), 1e-4);
+                    TS_ASSERT_DELTA( calculated_results[1][i], expected_maximum_interior, 1e-4);
+              
+                }
+                if (i==1 || i==2 || i==3 || i==4 || i==5)
+                {
+                    TS_ASSERT_DELTA( calculated_results[0][i], 0.0, 1e-4);
+                    TS_ASSERT_DELTA( calculated_results[1][i], expected_maximum_interior, 1e-4);
+              
+                }
+                if (i==13 || i==14 || i==27)
+                {
+                    TS_ASSERT_DELTA( calculated_results[0][i], expected_maximum_interior, 1e-4);
+                    TS_ASSERT_DELTA( calculated_results[1][i], expected_maximum_interior, 1e-4);              
+                }
+            }
+        }
     }
+    
 };
 
 #endif /*TESTDISCRETESYSTEMFORCECALCULATOR_HPP_*/
