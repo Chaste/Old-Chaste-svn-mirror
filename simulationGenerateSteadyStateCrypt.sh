9d212a89a10456f0b316ab9675d0c40ddde15726
Gary Mirams <gary.mirams@gmail.com>
2008-01-10 14:16:26 +0000

    Closes #554 with 3 scripts (and a change to the archive generator)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3173 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- simulationGenerateSteadyStateCrypt.sh
+++ simulationGenerateSteadyStateCrypt.sh
@@ -2,6 +2,19 @@
 #$ -cwd
 #$ -m be 
 #$ -M gary.mirams@gmail.com
-export LD_LIBRARY_PATH=/opt/petsc-2.2.1-with-mpi/lib/libO_c++/linux-gnu:/usr/local/gcc-3.4.5/lib/:/home/pmxgm/lib
+export LD_LIBRARY_PATH=/opt/petsc-2.2.1-with-mpi/lib/libO_c++/linux-gnu:/usr/local/gcc-3.4.5/lib/:/home/pmxgm/lib:/opt/boost/lib/
+#Make sure all the work goes on the local disk in a unique folder
+export CHASTE_TEST_OUTPUT=/tmp/$JOB_ID
+mkdir $CHASTE_TEST_OUTPUT
+date
+echo "Writing to local disk at " $CHASTE_TEST_OUTPUT
+
+
+
+
 ./TestGenerateSteadyStateCryptRunner
 
+date
+#Copy results back and clean up
+cp -r $CHASTE_TEST_OUTPUT . 
+rm -rf $CHASTE_TEST_OUTPUT
