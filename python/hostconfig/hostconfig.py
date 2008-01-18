f35ce03d22021a65eb73b49b0a98e8142ff6919b
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-01-18 14:51:06 +0000

    Adding new server hostname (from new server)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3224 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/hostconfig/hostconfig.py
+++ python/hostconfig/hostconfig.py
@@ -34,7 +34,8 @@ machine_fqdn = socket.getfqdn()
 if machine_fqdn in ["userpc30.comlab.ox.ac.uk", "userpc33.comlab.ox.ac.uk"]:
     import joe as conf
 elif machine_fqdn in ["userpc44.comlab.ox.ac.uk", "userpc60.comlab.ox.ac.uk",
-                      "userpc58.comlab.ox.ac.uk", "userpc59.comlab.ox.ac.uk"]:
+                      "userpc58.comlab.ox.ac.uk", "userpc59.comlab.ox.ac.uk",
+                      "chaste-bob.comlab.ox.ac.uk" ]:
     import chaste as conf
 elif machine_fqdn == "zuse.osc.ox.ac.uk":
     import zuse as conf
