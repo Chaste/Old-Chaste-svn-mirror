171aa4fa9781b609f9dfd0aba7a944d3d6cd4db5
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-02-13 16:32:54 +0000

    #517 Writing a PETSc vec to an HDF file
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3412 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- io/test/TestHdf5DataWriter.hpp
+++ io/test/TestHdf5DataWriter.hpp
@@ -15,7 +15,8 @@ public:
     void TestSimpleParallelWriteDirectlyWithHdf5()
     {
         // File to write
-        std::string results_dir = OutputFileHandler::GetChasteTestOutputDirectory();
+        OutputFileHandler oh("hdf5");
+        std::string results_dir = oh.GetOutputDirectoryFullPath();
         std::string file_name = results_dir + "test.h5";
         
         // Set up a property list saying how we'll open the file
@@ -52,11 +53,11 @@ public:
         H5Sclose(filespace);
 
         // Define a dataset in memory for this process
-        hsize_t count[2] = {X, Y};
+        hsize_t count[DIMS] = {X, Y};
         hid_t memspace = H5Screate_simple(DIMS, count, NULL);
         
         // Select hyperslab in the file.
-        hsize_t offset[2] = {PetscTools::GetMyRank()*X, 0};
+        hsize_t offset[DIMS] = {PetscTools::GetMyRank()*X, 0};
         filespace = H5Dget_space(dset_id);
         H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
         
@@ -73,6 +74,82 @@ public:
         H5Sclose(memspace);
         H5Pclose(plist_id);
         H5Fclose(file_id);
+    }
+    
+    void TestPetscWriteDirectlyWithHdf5()
+    {
+        const int data_size=17;
+        
+        //Initialise a PETSc vector
+        Vec a_vec=PetscTools::CreateVec(data_size);
+        double* p_a_vec;
+        VecGetArray(a_vec, &p_a_vec);
+        int lo, hi;
+        VecGetOwnershipRange(a_vec, &lo, &hi);
+        for (int global_index=lo; global_index<hi; global_index++)
+        {
+            unsigned local_index = global_index - lo;
+            p_a_vec[local_index] = global_index + 100*PetscTools::GetMyRank();
+        }
+        VecRestoreArray(a_vec, &p_a_vec);
+        VecAssemblyBegin(a_vec);
+        VecAssemblyEnd(a_vec);
+        
+        //VecView(a_vec, PETSC_VIEWER_STDOUT_WORLD);
+
+        // File to write
+        OutputFileHandler oh("hdf5", false);
+        std::string results_dir = oh.GetOutputDirectoryFullPath();
+        std::string file_name = results_dir + "vec.h5";
+        
+        // Set up a property list saying how we'll open the file
+        hid_t plist_id = H5Pcreate(H5P_FILE_ACCESS);
+        H5Pset_fapl_mpio(plist_id, PETSC_COMM_WORLD, MPI_INFO_NULL);
+        
+        // Create a file (collectively) and free the property list
+        hid_t file_id = H5Fcreate(file_name.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
+        H5Pclose(plist_id);
+        
+        const unsigned DIMS = 1;
+        
+        // Create the dataspace for the dataset.
+        //TS_ASSERT_EQUALS(data_size, hi-lo);
+        hsize_t dimsf[DIMS]={data_size}; // dataset dimensions
+        
+        hid_t filespace = H5Screate_simple(DIMS, dimsf, NULL);
+        
+        // Create the dataset with default properties and close filespace.
+        hid_t dset_id = H5Dcreate(file_id, "TheVector", H5T_NATIVE_DOUBLE, filespace, H5P_DEFAULT);
+        H5Sclose(filespace);
+
+        // Define a dataset in memory for this process
+        hsize_t count[DIMS] = {hi-lo};
+        hid_t memspace = H5Screate_simple(DIMS, count, NULL);
+        
+        // Select hyperslab in the file.
+        hsize_t offset[DIMS] = {lo};
+        filespace = H5Dget_space(dset_id);
+        H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
+        
+        // Create property list for collective dataset write, and write!  Finally.
+        plist_id = H5Pcreate(H5P_DATASET_XFER);
+        H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+    
+        VecGetArray(a_vec, &p_a_vec);
+        herr_t status = H5Dwrite(dset_id, H5T_NATIVE_DOUBLE, memspace, filespace, plist_id, p_a_vec);
+        VecRestoreArray(a_vec, &p_a_vec);
+        
+        TS_ASSERT_EQUALS(status, 0);
+        
+        // Release resources and close the file
+        H5Dclose(dset_id);
+        H5Sclose(filespace);
+        H5Sclose(memspace);
+        H5Pclose(plist_id);
+        H5Fclose(file_id);
+
+
+       
     }
 };
 #endif /*TESTHDF5DATAWRITER_HPP_*/
