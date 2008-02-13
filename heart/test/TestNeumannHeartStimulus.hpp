e59defb2447ab2a947ddf9b20aff050e46bca6fe
Lee Momtahan <leemo@comlab.ox.ac.uk>
2008-02-13 17:42:39 +0000

    ticket:612 added test with square wave neumann boundary condition
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3418 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestNeumannHeartStimulus.hpp
+++ heart/test/TestNeumannHeartStimulus.hpp
@@ -10,6 +10,8 @@
 #include "AbstractCardiacCellFactory.hpp"
 #include "LuoRudyIModel1991OdeSystem.hpp"
 #include "ReplicatableVector.hpp"
+#include "InitialStimulus.hpp"
+#include "StimulusBoundaryCondition.hpp"
 
 
 class NoStimulus2dCellFactory : public AbstractCardiacCellFactory<1>
@@ -95,6 +97,66 @@ public:
         TS_ASSERT_DELTA(voltage_replicated[7], 21.6782, atol);
         TS_ASSERT_DELTA(voltage_replicated[9], -33.9983, atol);
         TS_ASSERT_DELTA(voltage_replicated[10], -52.2396, atol);
+              
+    }
+    
+    void TestMonodomainSquareStimulus() throw(Exception)
+    {
+        NoStimulus2dCellFactory cell_factory;
+        MonodomainProblem<1> monodomain_problem( &cell_factory );
+        
+        monodomain_problem.SetMeshFilename("mesh/test/data/1D_0_to_1mm_10_elements");
+        monodomain_problem.SetEndTime(2);   // ms
+        monodomain_problem.SetOutputDirectory("MonoNeumanSquare");
+        monodomain_problem.SetOutputFilenamePrefix("NewMonodomainLR91_1d");        
+        monodomain_problem.SetIntracellularConductivities(0.0005);        
+        monodomain_problem.Initialise();
+        
+        // create boundary conditions container
+        BoundaryConditionsContainer<1,1,1> bcc;
+        InitialStimulus stim(4.0, 0.5);
+        StimulusBoundaryCondition<1> *p_bc_stim = new StimulusBoundaryCondition<1>(&stim);
+        ConstBoundaryCondition<1> *p_bc_no_flux = new ConstBoundaryCondition<1>(0);
+                
+        // get mesh
+        ConformingTetrahedralMesh<1,1> &mesh = monodomain_problem.rGetMesh();
+        // loop over boundary elements
+        ConformingTetrahedralMesh<1, 1>::BoundaryElementIterator iter;
+        iter = mesh.GetBoundaryElementIteratorBegin();
+        while (iter != mesh.GetBoundaryElementIteratorEnd())
+        {
+            if (((*iter)->GetNodeLocation(0))[0]==0.0)
+            {
+                bcc.AddNeumannBoundaryCondition(*iter, p_bc_stim);
+            }
+            else
+            {
+                bcc.AddNeumannBoundaryCondition(*iter, p_bc_no_flux);
+            }
+            
+            //AddDirichletBoundaryCondition(*iter, p_boundary_condition, indexOfUnknown);
+            iter++;
+        }
+            // if the element is on the left of the mesh, add a stimulus to the bcc
+        
+        // pass the bcc to the monodomain problem
+        monodomain_problem.SetBoundaryConditionsContainer(&bcc);
+        
+        monodomain_problem.GetMonodomainPde()->SetSurfaceAreaToVolumeRatio(1.0);
+        monodomain_problem.GetMonodomainPde()->SetCapacitance(1.0);
+        
+        monodomain_problem.Solve();
+        
+        // check some voltages    
+        ReplicatableVector voltage_replicated(monodomain_problem.GetVoltage());
+        double atol=5e-3;
+        
+        TS_ASSERT_DELTA(voltage_replicated[1], 22.4940, atol);
+        TS_ASSERT_DELTA(voltage_replicated[3], 22.6008, atol);
+        TS_ASSERT_DELTA(voltage_replicated[5], 23.3054, atol);
+        TS_ASSERT_DELTA(voltage_replicated[7], 24.4932, atol);
+        TS_ASSERT_DELTA(voltage_replicated[9], 14.5184, atol);
+        TS_ASSERT_DELTA(voltage_replicated[10],3.7081, atol);
               
     }
 };
