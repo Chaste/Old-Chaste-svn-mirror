84638188ab7a9853f2e632347ced832614ac2aa1
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-02-14 16:28:26 +0000

    * Data for nightly crypt test (broken in r3419)  * Move failing test to failing test pack  * Update my config file for HDF5
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3427 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/hostconfig/joe.py
+++ python/hostconfig/joe.py
@@ -11,11 +11,13 @@ dealii_path = None
 metis_path = None
 intel_path = '/opt/intel/cc/9.1.039/'
 
-other_includepaths = ['/home/jmpf/xsd-2.3.1-i686-linux-gnu/libxsd']
-other_libpaths = [os.path.join(petsc_2_3_path, 'externalpackages/f2cblaslapack/linux-gnu/'), '/opt/intel/mkl/9.1.023/lib/32']
+other_includepaths = ['/home/jmpf/xsd-2.3.1-i686-linux-gnu/libxsd', '/home/jmpf/hdf5/include']
+other_libpaths = [os.path.join(petsc_2_3_path, 'externalpackages/f2cblaslapack/linux-gnu/'), 
+                    '/opt/intel/mkl/9.1.023/lib/32', '/home/jmpf/hdf5/lib']
 blas_lapack = ['f2clapack', 'f2cblas']
 blas_lapack_production = ['mkl_lapack', 'mkl', 'svml']
-other_libraries = ['boost_serialization', 'xerces-c']
+other_libraries = ['boost_serialization', 'xerces-c', 'z', 'hdf5']
 
 tools = {'mpirun': '/home/jmpf/mpi/bin/mpirun',
          'mpicxx': '/home/jmpf/mpi/bin/mpicxx'}
+                                    
