d91a5469b108d1c24dc1a17dd3a2185dc4a93ad6
Gary Mirams <gary.mirams@gmail.com>
2008-02-29 16:27:49 +0000

    Try to check memory situation on deimos (Nottingham cluster).
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3515 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- simulationMeinekeLabellingExperiments.sh
+++ simulationMeinekeLabellingExperiments.sh
@@ -1,17 +1,22 @@
-#$ -j y
+$ -j y
 #$ -cwd
 #$ -m be 
 #$ -M gary.mirams@gmail.com
 export LD_LIBRARY_PATH=/opt/petsc-2.2.1-with-mpi/lib/libO_c++/linux-gnu:/usr/local/gcc-3.4.5/lib/:/home/pmxgm/lib:/opt/boost/lib/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/ufs/pmxgm/lib
 #Make sure all the work goes on the local disk in a unique folder
 export CHASTE_TEST_OUTPUT=/scratch/pmxgm/$JOB_ID
 mkdir $CHASTE_TEST_OUTPUT
 date
+echo "Memory before run is: "
+df -h
 echo "Writing to local disk at " $CHASTE_TEST_OUTPUT
 #Copy archive across
 cp -r MeinekeLabellingExperiment $CHASTE_TEST_OUTPUT
 ./TestMeinekeLabellingExperimentsRunner
 date
+echo "Memory after run is: "
+df -h
 #Copy results back and clean up
 cp -r $CHASTE_TEST_OUTPUT . 
 rm -rf $CHASTE_TEST_OUTPUT
