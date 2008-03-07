12a1505b177e7094b58d60f77815662adc5a0970
Alex Walter <obselete@comlab.ox.ac.uk>
2008-03-07 16:01:57 +0000

    [AlexW] scripts for makefile.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3556 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ simulationMakeNiceCryptSimsAlexW.sh
@@ -0,0 +1,25 @@
+#$ -j y
+#$ -cwd
+#$ -m be 
+#$ -M pmxaw@nottingham.ac.uk
+export LD_LIBRARY_PATH=/opt/petsc-2.2.1-with-mpi/lib/libO_c++/linux-gnu:/usr/local/gcc-3.4.5/lib/:/home/pmxaw/lib:/opt/boost/lib/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/ufs/pmxaw/lib
+#Make sure all the work goes on the local disk in a unique folder
+export CHASTE_TEST_OUTPUT=/tmp/$JOB_ID
+mkdir $CHASTE_TEST_OUTPUT
+date
+echo "Memory before run is: "
+df -h
+echo "Writing to local disk at " $CHASTE_TEST_OUTPUT
+
+
+
+
+./TestMakeNiceCryptSimsAlexWRunner
+
+date
+echo "Memory after run is: "
+df -h
+#Copy results back and clean up
+cp -r $CHASTE_TEST_OUTPUT . 
+rm -rf $CHASTE_TEST_OUTPUT
