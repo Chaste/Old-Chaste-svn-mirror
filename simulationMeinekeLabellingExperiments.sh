67f758a57b80420715649cd59ded9aeb15ba24d2
Gary Mirams <gary.mirams@gmail.com>
2008-03-10 14:37:45 +0000

    make cluster simulation results a bit tidier.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3564 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- simulationMeinekeLabellingExperiments.sh
+++ simulationMeinekeLabellingExperiments.sh
@@ -7,17 +7,32 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/ufs/pmxgm/lib
 #Make sure all the work goes on the local disk in a unique folder
 export CHASTE_TEST_OUTPUT=/scratch/pmxgm/$JOB_ID
 mkdir $CHASTE_TEST_OUTPUT
+
 date
-echo "Memory before run is: "
-df -h
+
+#echo "Disk usage before run is: "
+#df -h
+#du -sh /scratch/pmxgm/$JOB_ID
+
 echo "Writing to local disk at " $CHASTE_TEST_OUTPUT
 #Copy archive across
+
 cp -r MeinekeLabellingExperiment $CHASTE_TEST_OUTPUT
 ./TestMeinekeLabellingExperimentsRunner
+
 date
-echo "Memory after run is: "
-df -h
+
+#echo "Disk usage after run is: "
+#df -h
+#du -sh /scratch/pmxgm/$JOB_ID
+
 #Copy results back and clean up
 cp -r $CHASTE_TEST_OUTPUT . 
 rm -rf $CHASTE_TEST_OUTPUT
+
+# Get rid of the $JOB_ID folder and copy things back to the original place.
+rm -rf MeinekeLabellingExperiment/
+mv $JOB_ID/* .
+rm -rf $JOB_ID/
+
 
