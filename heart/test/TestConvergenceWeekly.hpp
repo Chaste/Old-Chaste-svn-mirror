aa5fa6874daa163ac08f6678ddbd27b0429289a6
Lee Momtahan <leemo@comlab.ox.ac.uk>
2008-03-12 16:05:24 +0000

    ticket:574 Attempt to fix weekly convergence test
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3582 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestConvergenceWeekly.hpp
+++ heart/test/TestConvergenceWeekly.hpp
@@ -94,7 +94,7 @@ public:
         SpaceConvergenceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<3>, 3, 2> tester;
         tester.SetKspAbsoluteTolerance(1e-3); 
         tester.Stimulus=NEUMANN;
-        tester.SetMeshWidth(0.15);//cm
+        tester.SetMeshWidth(0.14);//cm
         tester.Converge();
         TS_ASSERT(tester.Converged);
         TS_ASSERT_EQUALS(tester.MeshNum, 4u);
