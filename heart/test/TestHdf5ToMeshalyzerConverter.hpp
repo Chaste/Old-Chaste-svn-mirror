f43c48e1eb6d972b62d62a78898de5e96929e210
Pras Pathmanathan <pras@cs.ox.ac.uk>
2008-04-02 17:19:42 +0000

    tidy and exceptions
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3640 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestHdf5ToMeshalyzerConverter.hpp
+++ heart/test/TestHdf5ToMeshalyzerConverter.hpp
@@ -26,58 +26,80 @@ along with CHASTE.  If not, see <http://www.gnu.org/licenses/>.
 
 class TestHdf5ToMeshalyzerConverter : public CxxTest::TestSuite
 {
-public:
+private :
+    void CopyToTestOutputDirectory(std::string file, std::string dir)
+    {
+        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
+        std::string command = "cp " + file + " " + test_output_directory + dir;
+        int return_value = system(command.c_str());
+        assert(return_value==0);
+    }
+
+public :
     
-    void TestMonodomainConvertion() throw(Exception)
+    void xTestMonodomainConvertion() throw(Exception)
     {
         OutputFileHandler handler("TestHdf5ToMeshalyzerConverter");
         
         // firstly, copy ./heart/test/data/MonoDg01d/*.h5 to CHASTE_TEST_OUTPUT/TestHdf5ToMeshalyzerConverter,
         // as that is where the reader reads from.
-        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
-        std::string command = "cp heart/test/data/Monodomain1d/MonodomainLR91_1d.h5 " + test_output_directory +  "/TestHdf5ToMeshalyzerConverter/";
-        int return_value = system(command.c_str());
-        assert(return_value==0);
-        
+        CopyToTestOutputDirectory("heart/test/data/Monodomain1d/MonodomainLR91_1d.h5",
+                                  "TestHdf5ToMeshalyzerConverter");
+
         // convert
-        std::string h5_output_dir = "TestHdf5ToMeshalyzerConverter";
-        std::string h5_file_name = "MonodomainLR91_1d";
-        
-        Hdf5ToMeshalyzerConverter converter(h5_output_dir, h5_file_name);
+        Hdf5ToMeshalyzerConverter converter("TestHdf5ToMeshalyzerConverter", "MonodomainLR91_1d");
         
         // compare the voltage file
-        command = "cmp " + test_output_directory + "/TestHdf5ToMeshalyzerConverter/MonodomainLR91_1d_V.dat " 
-                         + "heart/test/data/Monodomain1d/MonodomainLR91_1d_V.dat";
+        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
+        std::string command = "cmp " + test_output_directory + "/TestHdf5ToMeshalyzerConverter/MonodomainLR91_1d_V.dat " 
+                                     + "heart/test/data/Monodomain1d/MonodomainLR91_1d_V.dat";
         TS_ASSERT_EQUALS(system(command.c_str()), 0);
     }
 
 
-    void TestBidomainConvertion() throw(Exception)
+    void xTestBidomainConvertion() throw(Exception)
     {
         OutputFileHandler handler("TestHdf5ToMeshalyzerConverter");
 
         // firstly, copy ./heart/test/data/Bidomain1d/*.h5 to CHASTE_TEST_OUTPUT/TestHdf5ToMeshalyzerConverter,
         // as that is where the reader reads from.
-        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
-        std::string command = "cp heart/test/data/Bidomain1d/bidomain.h5 " + test_output_directory +  "/TestHdf5ToMeshalyzerConverter/";
-        int return_value = system(command.c_str());
-        assert(return_value==0);
+        CopyToTestOutputDirectory("heart/test/data/Bidomain1d/bidomain.h5",
+                                  "TestHdf5ToMeshalyzerConverter");
 
         // convert
-        std::string h5_output_dir = "TestHdf5ToMeshalyzerConverter";
-        std::string h5_file_name = "bidomain";
-        
-        Hdf5ToMeshalyzerConverter converter(h5_output_dir, h5_file_name);
+        Hdf5ToMeshalyzerConverter converter("TestHdf5ToMeshalyzerConverter", "bidomain");
 
         // compare the voltage file
-        command = "cmp " + test_output_directory + "/TestHdf5ToMeshalyzerConverter/bidomain_V.dat " 
-                         + "heart/test/data/Bidomain1d/bidomain_V.dat";
+        std::string test_output_directory = OutputFileHandler::GetChasteTestOutputDirectory();
+        std::string command = "cmp " + test_output_directory + "/TestHdf5ToMeshalyzerConverter/bidomain_V.dat " 
+                                     + "heart/test/data/Bidomain1d/bidomain_V.dat";
         TS_ASSERT_EQUALS(system(command.c_str()), 0);
 
         // compare the Phi_e file
         command = "cmp " + test_output_directory + "/TestHdf5ToMeshalyzerConverter/bidomain_Phi_e.dat " 
                          + "heart/test/data/Bidomain1d/bidomain_Phi_e.dat";
         TS_ASSERT_EQUALS(system(command.c_str()), 0);
+    }
+    
+    void TestExceptions() throw(Exception)
+    {
+        OutputFileHandler handler("TestHdf5ToMeshalyzerConverter");
+
+        CopyToTestOutputDirectory("io/test/data/hdf5_test_full_format.h5", // doesn't have one two variables
+                                  "TestHdf5ToMeshalyzerConverter");
+
+        TS_ASSERT_THROWS_ANYTHING( Hdf5ToMeshalyzerConverter converter("TestHdf5ToMeshalyzerConverter", "hdf5_test_full_format") );
+
+        CopyToTestOutputDirectory("heart/test/data/bad_heart_data_1.h5", // monodomain, with "Volt" instead of "V"
+                                  "TestHdf5ToMeshalyzerConverter");
+
+        TS_ASSERT_THROWS_ANYTHING( Hdf5ToMeshalyzerConverter converter2("TestHdf5ToMeshalyzerConverter", "bad_heart_data_1") );
+
+
+        CopyToTestOutputDirectory("heart/test/data/bad_heart_data_2.h5", // bidomain, with "Volt" instead of "V"
+                                  "TestHdf5ToMeshalyzerConverter");
+
+        TS_ASSERT_THROWS_ANYTHING( Hdf5ToMeshalyzerConverter converter2("TestHdf5ToMeshalyzerConverter", "bad_heart_data_2") );
 
     }
 };
