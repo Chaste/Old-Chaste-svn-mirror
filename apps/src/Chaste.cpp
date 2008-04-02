67ac80a038f5276c6db4f2deb9a64fda931b2de1
Lee Momtahan <leemo@comlab.ox.ac.uk>
2008-04-02 14:06:56 +0000

    ticket:221 Added copyright notices to python source files and made the chaste executable print it to std::out
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3636 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- apps/src/Chaste.cpp
+++ apps/src/Chaste.cpp
@@ -311,6 +311,21 @@ void CreateSlab(ConformingTetrahedralMesh<3,3>* pMesh)
 
 int main(int argc, char *argv[]) 
 {
+    std::cout << "Copyright (C) Oxford University 2008 \n\n\
+\
+CHASTE is free software: you can redistribute it and/or modify \n\
+it under the terms of the Lesser GNU General Public License as published by \n\
+the Free Software Foundation, either version 2.1 of the License, or \n\
+(at your option) any later version. \n\n\
+\
+CHASTE is distributed in the hope that it will be useful, \n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of \n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n\
+Lesser GNU General Public License for more details. \n\n\
+\
+You should have received a copy of the Lesser GNU General Public License \n\
+along with CHASTE.  If not, see <http://www.gnu.org/licenses/>.\n\n ";
+
     try
     {
         PETSCEXCEPT(PetscInitialize(&argc, &argv, PETSC_NULL, PETSC_NULL) );
--- python/CheckForOrphanedTests.py
+++ python/CheckForOrphanedTests.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+"""Copyright (C) Oxford University 2008
+
+This file is part of CHASTE.
+
+CHASTE is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+CHASTE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with CHASTE.  If not, see <http://www.gnu.org/licenses/>."""
+
 # This script checks all the test folders to see if there are any
 # Test*.hpp files that aren't listed in a test pack.
 # It expects to be run from the trunk of the Chaste distribution.
