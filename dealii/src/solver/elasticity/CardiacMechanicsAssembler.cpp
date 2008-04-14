5d087e927af09017260c6297f41f65a8dca4350f
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-04-14 14:01:39 +0000

    Fixes r3665
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3667 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- dealii/src/solver/elasticity/CardiacMechanicsAssembler.cpp
+++ dealii/src/solver/elasticity/CardiacMechanicsAssembler.cpp
@@ -1,11 +1,24 @@
-#ifndef JOE
-#define JOE
-/*
- * 
-*/
-
 #ifndef CARDIACMECHANICSASSEMBLER_CPP_
 #define CARDIACMECHANICSASSEMBLER_CPP_
+/*
+Copyright (C) Oxford University 2008
+
+This file is part of CHASTE.
+
+CHASTE is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+CHASTE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with CHASTE.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include "CardiacMechanicsAssembler.hpp"
 #include "MooneyRivlinMaterialLaw.hpp"
@@ -510,4 +523,3 @@ void CardiacMechanicsAssembler<DIM>::AssembleOnElement(typename DoFHandler<DIM>:
 }
 
 #endif /*CARDIACMECHANICSASSEMBLER_CPP_*/
-#endif
