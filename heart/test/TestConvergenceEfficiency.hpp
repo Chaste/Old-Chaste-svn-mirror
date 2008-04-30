5cd24eb17c913f95cabd05467cdbd8184bbb783e
Miguel Bernabeu <miguel.bernabeu@ucl.ac.uk>
2008-04-30 14:17:53 +0000

    #606 and #574 Added test for efficiency
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3762 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ heart/test/TestConvergenceEfficiency.hpp
@@ -0,0 +1,66 @@
+/*
+Copyright (C) University of Oxford, 2008
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as published by
+the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with Chaste.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TESTCONVERGENCEEFFICIENCY_HPP_
+#define TESTCONVERGENCEEFFICIENCY_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include "BidomainProblem.hpp"
+#include "MonodomainProblem.hpp"
+#include <petscvec.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#include "BackwardEulerLuoRudyIModel1991.hpp"
+#include "LuoRudyIModel1991OdeSystem.hpp"
+#include "PdeConvergenceTester.hpp"
+#include "SpaceConvergenceTester.hpp"
+#include "KspConvergenceTester.hpp"
+#include "OdeConvergenceTester.hpp"
+//#include "StimulusConvergenceTester.hpp"
+
+class TestConvergenceEfficiency : public CxxTest::TestSuite
+{
+
+public:
+    void Test3DSpace10() throw(Exception)
+    {
+        ///\todo Remove line 357
+        ///cardiac_problem.SetOutputNodes(nodes_to_be_output); 
+        ///from AbstractConvergenceTester.hpp
+        ///to see if it made a difference
+        PetscOptionsSetValue("-ksp_type", "symmlq");
+        PetscOptionsSetValue("-pc_type", "bjacobi");
+        PetscOptionsSetValue("-options_table", "");
+        SpaceConvergenceTester<BackwardEulerLuoRudyIModel1991, BidomainProblem<3>, 3, 2> tester;
+        tester.SetKspRelativeTolerance(1e-10);
+        tester.OdeTimeStep /= 2.0;
+        tester.PdeTimeStep /= 2.0;
+        tester.SetMeshWidth(0.10);//cm
+        
+        tester.Converge(__FUNCTION__);
+        TS_ASSERT(tester.Converged);
+        TS_ASSERT_EQUALS(tester.MeshNum, 3u);
+    }
+
+};
+
+#endif /*TESTCONVERGENCEEFFICIENCY_HPP_*/
