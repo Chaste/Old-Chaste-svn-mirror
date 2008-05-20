95b67e067367943143bb1d22ebbdeabf928fbbae
Miguel Bernabeu <miguel.bernabeu@ucl.ac.uk>
2008-05-20 11:06:56 +0000

    #671 - Data for the comparison imported (ii)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3862 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ apps/simulations/upf-mesh/carp2triangles.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <mesh_prefix>"
+    exit
+fi
+
+mesh_prefix=$1
+
+#
+# Create .node file
+#
+original_file=${mesh_prefix}.pts
+
+if [ ! -f "$original_file" ]; then
+    echo "File $original_file does not exist!"
+    exit
+fi
+
+new_file=${mesh_prefix}.node
+
+# Header of the file
+num_nodes=`head -n 1 $original_file`
+echo "$num_nodes 3 0 0" > $new_file
+
+# Coordinates in um should be converted to cm. Add index starting from
+awk '{if (NF == 3) printf("%d %f %f %f\n", NR-2, $1/10000, $2/10000, $3/10000)}' $original_file >> $new_file
+
+
+#
+# Create .ele file
+#
+original_file=${mesh_prefix}.tetras
+
+if [ ! -f "$original_file" ]; then
+    echo "File $original_file does not exist!"
+    exit
+fi
+
+new_file=${mesh_prefix}.ele
+
+# Header of the file
+num_nodes=`head -n 1 $original_file`
+echo "$num_nodes 4 0" > $new_file
+
+# Add index starting from 
+awk '{if (NF == 4) printf("%d %d %d %d %d\n", NR-2, $1, $2, $3, $4)}' $original_file >> $new_file
+
+
+#
+# Create empty .face file
+#
+new_file=${mesh_prefix}.face
+
+echo "0" > $new_file
