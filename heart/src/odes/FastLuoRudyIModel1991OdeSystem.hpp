5ca7067469c142be136d5c9ac3998aa1c3b6394a
Raf Bordas <rafb@cs.ox.ac.uk>
2008-05-28 10:17:28 +0000

    #721 Added FastLuoRudyIModel1991OdeSystem and test.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@3884 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ heart/src/odes/FastLuoRudyIModel1991OdeSystem.hpp
@@ -0,0 +1,89 @@
+/*
+
+Copyright (C) University of Oxford, 2008
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/#ifndef _FASTLUORUDYIMODEL1991ODESYSTEM_HPP_
+#define _FASTLUORUDYIMODEL1991ODESYSTEM_HPP_
+
+#include "AbstractCardiacCell.hpp"
+#include "AbstractStimulusFunction.hpp"
+#include <vector>
+
+/**
+ * This class sets up the FastLuoRudyIModel1991OdeSystem system of equations.
+ */
+class FastLuoRudyIModel1991OdeSystem : public AbstractCardiacCell
+{
+private:
+
+    /*< Constants for the LuoRudyIModel1991OdeSystem model */
+    static const double membrane_C = 1.0;
+    static const double membrane_F = 96484.6;
+    static const double membrane_R = 8314;
+    static const double membrane_T = 310.0;
+    static const double background_current_E_b = -59.87;
+    static const double background_current_g_b = 0.03921;
+    static const double fast_sodium_current_g_Na = 23.0;
+    static const double ionic_concentrations_Ki = 145.0;
+    static const double ionic_concentrations_Ko = 5.4;
+    static const double ionic_concentrations_Nai = 18.0;
+    static const double ionic_concentrations_Nao = 140.0;
+    static const double plateau_potassium_current_g_Kp = 0.0183;
+    static const double time_dependent_potassium_current_PR_NaK = 0.01833;
+
+    /*< Another parameter, which is a function of the above */
+    double fast_sodium_current_E_Na;
+
+    /* Values for slow ionic currents interpolated from the coarse mesh. */
+    std::vector<double> mSlowValues;
+
+    /** 
+     *  Range-checking on the current values of the state variables. Make sure
+     *  all gating variables have are within zero and one, and all concentrations
+     *  are positive
+     */
+    void VerifyStateVariables();
+    
+public:
+    // Constructor
+    FastLuoRudyIModel1991OdeSystem(AbstractIvpOdeSolver *pSolver,
+                                   double dt,
+                                   AbstractStimulusFunction *pIntracellularStimulus,
+                                   AbstractStimulusFunction *pExtracellularStimulus = NULL);
+                               
+    // Destructor
+    ~FastLuoRudyIModel1991OdeSystem();
+        
+    // This method will compute the RHS of the LuoRudyIModel1991OdeSystem model
+    void EvaluateYDerivatives(double time, const std::vector<double> &rY, std::vector<double> &rDY);
+    
+    void SetSlowValues(const std::vector<double> &rSlowValues);
+    
+    double GetIIonic();
+    
+    double GetIntracellularCalciumConcentration();
+};
+
+#endif // _FASTLUORUDYIMODEL1991ODESYSTEM_HPP_
---
+++ heart/test/TestFastLuoRudyIModel1991OdeSystem.hpp
@@ -0,0 +1,75 @@
+/*
+
+Copyright (C) University of Oxford, 2008
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef TESTFASTLUORUDYIMODEL1991ODESYSTEM_HPP_
+#define TESTFASTLUORUDYIMODEL1991ODESYSTEM_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include "FastLuoRudyIModel1991OdeSystem.hpp"
+#include "LuoRudyIModel1991OdeSystem.hpp"
+#include "EulerIvpOdeSolver.hpp"
+#include "ZeroStimulus.hpp"
+
+class TestFastLuoRudyIModel1991OdeSystem : public CxxTest::TestSuite
+{
+public:
+    void TestFastLuoRudyCalculateDerivatives(void) throw(Exception)
+    {
+        //Set up normal cell model and evaluate Y derivatives
+        ZeroStimulus stimulus;
+        EulerIvpOdeSolver solver;
+        double time_step = 0.01;
+        
+        LuoRudyIModel1991OdeSystem luo_rudy(&solver, time_step, &stimulus);
+        
+        std::vector<double> DY_normal(8);
+
+        luo_rudy.EvaluateYDerivatives(0.0, luo_rudy.rGetStateVariables(), DY_normal);
+
+        //Set up fast cell model and evaluate Y derivatives
+        FastLuoRudyIModel1991OdeSystem fast_luo_rudy(&solver, time_step, &stimulus);
+
+        std::vector<double> DY_fast(6);
+        std::vector<double> slow_values(2);
+        slow_values[0] = luo_rudy.rGetStateVariables()[5];
+        slow_values[1] = luo_rudy.rGetStateVariables()[6];
+        
+        fast_luo_rudy.SetSlowValues(slow_values);
+                
+        fast_luo_rudy.EvaluateYDerivatives(0.0, fast_luo_rudy.rGetStateVariables(), DY_fast);
+        
+        //Compare the resulting Y derivatives
+        for (unsigned i = 0; i < 5; ++i)
+        {
+            TS_ASSERT_DELTA(DY_normal[i], DY_fast[i], 1e-5);
+        }
+        
+        TS_ASSERT_DELTA(DY_normal[7], DY_fast[5], 1e-5);
+    }
+};
+
+#endif /*TESTFASTLUORUDYIMODEL1991ODESYSTEM_HPP_*/
