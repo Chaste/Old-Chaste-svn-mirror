be79657544c0d7dafdfcedc90343dd7c32d0b92f
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-06-18 16:33:22 +0000

    Get things working on Miguel's machine
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@4007 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/hostconfig/hostconfig.py
+++ python/hostconfig/hostconfig.py
@@ -56,12 +56,11 @@ import sys
 
 machine_fqdn = socket.getfqdn()
 
-
 if machine_fqdn in ["userpc30.comlab.ox.ac.uk", "userpc33.comlab.ox.ac.uk"]:
     import joe as conf
 elif machine_fqdn in [ "userpc60.comlab.ox.ac.uk", "userpc58.comlab.ox.ac.uk" ]:
     import chaste32 as conf
-elif machine_fqdn in ["userpc44.comlab.ox.ac.uk", "userpc59.comlab.ox.ac.uk" ]:
+elif machine_fqdn in ["userpc44.comlab.ox.ac.uk", "userpc59.comlab.ox.ac.uk", "userpc36.comlab.ox.ac.uk" ]:
     import chaste64 as conf
 elif machine_fqdn == "chaste-bob.comlab.ox.ac.uk":
     import chastebob as conf
