1932cff9fb263d879fc6a4191346f3131929b5d1
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-08-18 16:46:28 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@4178 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/src/odes/BackwardEulerNobleVargheseKohlNoble1998.hpp
+++ heart/src/odes/BackwardEulerNobleVargheseKohlNoble1998.hpp
@@ -183,7 +183,12 @@ public:
         assert(rGetStateVariables()[14]>=0);
         assert(rGetStateVariables()[15]>=0);
         assert(rGetStateVariables()[16]>=0);
-        assert(rGetStateVariables()[17]>=0);
+        //assert(rGetStateVariables()[17]>=0);
+        if (rGetStateVariables()[17]<=0)
+        {
+            std::cout<<"Warning "<<rGetStateVariables()[17]<<"\n";
+            //rGetStateVariables()[17]=0;
+        }
         assert(rGetStateVariables()[18]>=0);
         assert(rGetStateVariables()[19]>=0);
         assert(rGetStateVariables()[20]>=0);
