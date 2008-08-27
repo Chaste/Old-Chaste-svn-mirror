d2ead68d9647772147d3f37fa1aafd4d3231486c
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-08-27 17:12:43 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@4198 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestMonodomainProblem.hpp
+++ heart/test/TestMonodomainProblem.hpp
@@ -399,7 +399,7 @@ public:
         PetscTools::Barrier();
         
         //Need to find pts, tri, transmembrane, xml
-        char *test_file_names[4]={"monodomain2d_mesh.pts", "monodomain2d_mesh.tri", "monodomain2d_V.dat", "monodomain2d_parameters.xml"};
+        std::string test_file_names[4]={"monodomain2d_mesh.pts", "monodomain2d_mesh.tri", "monodomain2d_V.dat", "monodomain2d_parameters.xml"};
         for (unsigned i=0; i<4; i++)
         {
             std::string compare_command = "cmp -s ";
