e0723c2ba318f3cc227bcd5c1f1df5f4b34f0b9c
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2008-12-22 12:13:30 +0000

    Fix nightly_only test and compilation of acceptance tests
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@4778 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- apps/src/Chaste.cpp
+++ apps/src/Chaste.cpp
@@ -26,7 +26,7 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-
+#include "TetrahedralMesh.hpp"
 #include "MonodomainProblem.hpp"
 #include "BidomainProblem.hpp"
 #include "PetscTools.hpp"
