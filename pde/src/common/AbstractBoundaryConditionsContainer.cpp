ed4c6957da5c71eed72da1315effd5818f83f002
Pras Pathmanathan <pras@cs.ox.ac.uk>
2009-01-15 14:06:22 +0000

    Added AbstractBccImplementation and BccImplementation so that if projects or tests use PROBLEM_DIM>3 they can include these (only PROBLEM_DIM<=3 is explicitly instantiated).
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@4869 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pde/src/common/AbstractBoundaryConditionsContainer.cpp
+++ pde/src/common/AbstractBoundaryConditionsContainer.cpp
@@ -25,81 +25,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 
 */
-#include "AbstractBoundaryConditionsContainer.hpp"
 
-template<unsigned ELEM_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
-AbstractBoundaryConditionsContainer<ELEM_DIM,SPACE_DIM,PROBLEM_DIM>::AbstractBoundaryConditionsContainer()
-{
-    for (unsigned index_of_unknown=0; index_of_unknown<PROBLEM_DIM; index_of_unknown++)
-    {
-        mpDirichletMap[index_of_unknown] =  new std::map< const Node<SPACE_DIM> *, const AbstractBoundaryCondition<SPACE_DIM>*, LessThanNode<SPACE_DIM> >;
-    }
-}
-
-template<unsigned ELEM_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
-AbstractBoundaryConditionsContainer<ELEM_DIM,SPACE_DIM,PROBLEM_DIM>::~AbstractBoundaryConditionsContainer()
-{
-    DeleteDirichletBoundaryConditions();
-}
-
-template<unsigned ELEM_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
-bool AbstractBoundaryConditionsContainer<ELEM_DIM,SPACE_DIM,PROBLEM_DIM>::HasDirichletBoundaryConditions()
-{
-    for (unsigned i=0; i<PROBLEM_DIM; i++)
-    {
-        if (!mpDirichletMap[i]->empty())
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-template<unsigned ELEM_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
-void AbstractBoundaryConditionsContainer<ELEM_DIM,SPACE_DIM,PROBLEM_DIM>::DeleteDirichletBoundaryConditions(std::set<const AbstractBoundaryCondition<SPACE_DIM>*> deletedConditions)
-{
-    for (unsigned i=0; i<PROBLEM_DIM; i++)
-    {
-        if (mpDirichletMap[i])
-        {
-            mDirichIterator = mpDirichletMap[i]->begin();
-            while (mDirichIterator != mpDirichletMap[i]->end() )
-            {
-                if (deletedConditions.count(mDirichIterator->second) == 0)
-                {
-                    deletedConditions.insert(mDirichIterator->second);
-                    delete mDirichIterator->second;
-                }
-                mDirichIterator++;
-            }
-
-            delete(mpDirichletMap[i]);
-            mpDirichletMap[i] = NULL;
-        }
-    }
-}
-
-template<unsigned ELEM_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
-double AbstractBoundaryConditionsContainer<ELEM_DIM,SPACE_DIM,PROBLEM_DIM>::GetDirichletBCValue(const Node<SPACE_DIM>* pBoundaryNode, unsigned indexOfUnknown)
-{
-    assert(indexOfUnknown < PROBLEM_DIM);
-    //assert(pBoundaryNode->IsBoundaryNode());
-
-    mDirichIterator = mpDirichletMap[indexOfUnknown]->find(pBoundaryNode);
-    assert(mDirichIterator != mpDirichletMap[indexOfUnknown]->end());
-
-    return mDirichIterator->second->GetValue(pBoundaryNode->GetPoint());
-}
-
-template<unsigned ELEM_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
-bool AbstractBoundaryConditionsContainer<ELEM_DIM,SPACE_DIM,PROBLEM_DIM>::HasDirichletBoundaryCondition(const Node<SPACE_DIM>* pNode, unsigned indexOfUnknown)
-{
-    assert(indexOfUnknown < PROBLEM_DIM);
-
-    this->mDirichIterator = this->mpDirichletMap[indexOfUnknown]->find(pNode);
-
-    return (this->mDirichIterator != this->mpDirichletMap[indexOfUnknown]->end());
-}
+#include "AbstractBoundaryConditionsContainerImplementation.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Explicit instantiation
