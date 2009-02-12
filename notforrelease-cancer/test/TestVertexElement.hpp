Chaste/Old-Chaste-svn-mirror

commit 81a3f8877996714f449dc9b430374a4f868188e2

coverage
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5080 b5f69653-2bf5-0310-8201-b0dafb8e1405

notforrelease-cancer/test/TestVertexElement.hpp
@@ -50,7 +50,7 @@ public:
         }
 
         // Create element        
-        VertexElement<2,2> vertex_element(INDEX_IS_NOT_USED, nodes);
+        VertexElement<2,2> vertex_element(0, nodes);
         
         TS_ASSERT_DELTA(vertex_element.GetArea(), 3*sqrt(3)/2.0, 1e-4);
         TS_ASSERT_DELTA(vertex_element.GetPerimeter(), 6.0, 1e-4);
@@ -112,6 +112,57 @@ public:
             delete nodes[i];
         }
         delete p_new_node;
+    }
+    
+    
+    void TestMarkAsDeleted()
+    {
+        // Create nodes
+        std::vector<Node<2>*> nodes;
+        nodes.push_back(new Node<2>(0, false, 0.0, 0.0));
+        nodes.push_back(new Node<2>(1, false, 1.0, 0.0));
+        nodes.push_back(new Node<2>(2, false, 1.0, 1.0));
+        nodes.push_back(new Node<2>(3, false, 0.0, 1.0));
+
+        // Create element
+        VertexElement<2,2> vertex_element(0, nodes);
+        vertex_element.RegisterWithNodes();
+
+        for (unsigned i=0; i<nodes.size(); i++)
+        {
+            TS_ASSERT_EQUALS(vertex_element.GetNode(i)->GetNumContainingElements(), 1u);
+        }
+
+        vertex_element.MarkAsDeleted();
+
+        for (unsigned i=0; i<nodes.size(); i++)
+        {
+            TS_ASSERT_EQUALS(vertex_element.GetNode(i)->GetNumContainingElements(), 0u);
+        }        
+    }
+    
+    
+    void TestUpdateNode()
+    {
+        // Create nodes
+        std::vector<Node<2>*> nodes;
+        nodes.push_back(new Node<2>(0, false, 0.0, 0.0));
+        nodes.push_back(new Node<2>(1, false, 1.0, 0.0));
+        nodes.push_back(new Node<2>(2, false, 1.0, 1.0));
+        nodes.push_back(new Node<2>(3, false, 0.0, 1.0));
+
+        // Create element
+        VertexElement<2,2> vertex_element(0, nodes);
+        vertex_element.RegisterWithNodes();
+
+        TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[0], 1.0, 1e-12);
+        TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[0], 1.0, 1e-12);
+        
+        // Update location of node 2        
+        vertex_element.UpdateNode(2, new Node<2>(4, false, 1.2, 1.3));
+        
+        TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[0], 1.2, 1e-12);
+        TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[1], 1.3, 1e-12);
     }
 
 

notforrelease-cancer/src/mesh/VertexElement.cpp
@@ -121,8 +121,10 @@ void VertexElement<ELEMENT_DIM, SPACE_DIM>::AddNode(const unsigned& rIndex, Node
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>    
 void VertexElement<ELEMENT_DIM, SPACE_DIM>::CalculateVertexElementAreaAndPerimeter()
 {
+#define COVERAGE_IGNORE
     assert(SPACE_DIM == 2);
-    
+#undef COVERAGE_IGNORE
+
     c_vector<double, SPACE_DIM> current_node;
     c_vector<double, SPACE_DIM> anticlockwise_node; 
     
@@ -167,9 +169,9 @@ double VertexElement<ELEMENT_DIM, SPACE_DIM>::GetArea()
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM> VertexElement<ELEMENT_DIM, SPACE_DIM>::GetAreaGradientAtNode(unsigned localIndex)
 {
-    #define COVERAGE_IGNORE
+#define COVERAGE_IGNORE
     assert(SPACE_DIM==2);
-    #undef COVERAGE_IGNORE
+#undef COVERAGE_IGNORE
 
     c_vector<double, SPACE_DIM> area_gradient;
     
@@ -189,9 +191,9 @@ c_vector<double, SPACE_DIM> VertexElement<ELEMENT_DIM, SPACE_DIM>::GetAreaGradie
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM> VertexElement<ELEMENT_DIM, SPACE_DIM>::GetPerimeterGradientAtNode(unsigned localIndex)
 {
-    #define COVERAGE_IGNORE
+#define COVERAGE_IGNORE
     assert(SPACE_DIM==2);
-    #undef COVERAGE_IGNORE
+#undef COVERAGE_IGNORE
 
     c_vector<double, SPACE_DIM> perimeter_gradient;
     
@@ -260,7 +262,9 @@ double VertexElement<ELEMENT_DIM, SPACE_DIM>::GetPerimeter()
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>  
 c_vector<double, 3> VertexElement<ELEMENT_DIM, SPACE_DIM>::CalculateMoments()
 {
+#define COVERAGE_IGNORE
     assert(SPACE_DIM == 2);
+#undef COVERAGE_IGNORE
     
     c_vector<double, 3> moments = zero_vector<double>(3);
     
@@ -305,7 +309,9 @@ c_vector<double, 3> VertexElement<ELEMENT_DIM, SPACE_DIM>::CalculateMoments()
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM> VertexElement<ELEMENT_DIM, SPACE_DIM>::CalculateCentroid()
 {
+#define COVERAGE_IGNORE
     assert(SPACE_DIM == 2);
+#undef COVERAGE_IGNORE
     
     c_vector<double, SPACE_DIM> centroid = zero_vector<double>(SPACE_DIM);
     c_vector<double, SPACE_DIM> current_node;
@@ -341,7 +347,9 @@ c_vector<double, SPACE_DIM> VertexElement<ELEMENT_DIM, SPACE_DIM>::CalculateCent
 template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
 c_vector<double, SPACE_DIM> VertexElement<ELEMENT_DIM, SPACE_DIM>::CalculateShortAxis()
 {
+#define COVERAGE_IGNORE
     assert(SPACE_DIM == 2);
+#undef COVERAGE_IGNORE
     
     c_vector<double, SPACE_DIM> short_axis = zero_vector<double>(SPACE_DIM);
     

commit 381d5837ff905f660b0cfbd69dd9f840a9f01f93

Fix memory leaks introduced in r5080
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5111 b5f69653-2bf5-0310-8201-b0dafb8e1405

notforrelease-cancer/test/TestVertexElement.hpp
@@ -139,6 +139,11 @@ public:
         {
             TS_ASSERT_EQUALS(vertex_element.GetNode(i)->GetNumContainingElements(), 0u);
         }        
+        // Tidy up
+        for (unsigned i=0; i<nodes.size(); ++i)
+        {
+            delete nodes[i];
+        }
     }
     
     
@@ -158,11 +163,18 @@ public:
         TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[0], 1.0, 1e-12);
         TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[0], 1.0, 1e-12);
         
-        // Update location of node 2        
-        vertex_element.UpdateNode(2, new Node<2>(4, false, 1.2, 1.3));
+        // Update location of node 2 
+        Node <2> *p_node=new Node<2>(4, false, 1.2, 1.3);   
+        vertex_element.UpdateNode(2, p_node);
         
         TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[0], 1.2, 1e-12);
         TS_ASSERT_DELTA(vertex_element.GetNode(2)->rGetLocation()[1], 1.3, 1e-12);
+         // Tidy up
+        for (unsigned i=0; i<nodes.size(); ++i)
+        {
+            delete nodes[i];
+        }
+        delete p_node;
     }
 
 