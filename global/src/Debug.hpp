41faa7892b5f14a94d7a1601648a6fc55b8ceff5
Pras Pathmanathan <pras@cs.ox.ac.uk>
2009-03-06 18:09:59 +0000

    corrected doxygen
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5375 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- global/src/Debug.hpp
+++ global/src/Debug.hpp
@@ -38,22 +38,22 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef NDEBUG
-    /*< Print the given message */
+    /** Print the given message */
     #define TRACE(stuff) std::cout << "DEBUG: " << stuff << std::endl << std::flush;
     
-    /*< Print the name and value of the given variables */
+    /** Print the name and value of the given variables */
     #define PRINT_VARIABLE(var) std::cout << "DEBUG: " #var " = " << var << std::endl << std::flush;
     #define PRINT_VARIABLES(var1,var2) std::cout << "DEBUG: " #var1 " = " << var1 << ", " #var2 " = " << var2 << std::endl << std::flush;
     #define PRINT_3_VARIABLES(var1,var2,var3) std::cout << "DEBUG: " #var1 " = " << var1 << ", " #var2 " = " << var2 << ", " #var3 " = " << var3 << std::endl << std::flush;
     #define PRINT_4_VARIABLES(var1,var2,var3,var4) std::cout << "DEBUG: " #var1 " = " << var1 << ", " #var2 " = " << var2 << ", " #var3 " = " << var3 << ", " #var4 " = " << var4 << std::endl << std::flush;
     
-    /*< Quit (assert(0)) on the n-th time this line is reached, for the given n */ 
+    /** Quit (assert(0)) on the n-th time this line is reached, for the given n */ 
     #define QUIT_AFTER_N_VISITS(n) { static unsigned counter=1; if(counter++==(n)) {TRACE("User-forced quit."); assert(0);} }
     
-    /*< Print how many times this line has been reached, everytime it is reached */
+    /** Print how many times this line has been reached, everytime it is reached */
     #define HOW_MANY_TIMES_HERE(message) { static unsigned counter=1; std::cout << "DEBUG: Num times here ("<< message << "): " << counter++ << std::endl << std::flush; }
 #else
-    /*< macros do nothing in NDEBUG mode */
+    /** macros do nothing in NDEBUG mode */
     #define TRACE(stuff)
     #define PRINT_VARIABLE(var)
     #define PRINT_VARIABLES(var1,var2)
