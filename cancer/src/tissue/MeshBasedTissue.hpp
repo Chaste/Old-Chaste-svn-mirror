Chaste/Old-Chaste-svn-mirror

commit daf141486c270b00e2f1e7b2d9e238ab8705adb8

Documentation/coverage
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5380 b5f69653-2bf5-0310-8201-b0dafb8e1405

cancer/src/tissue/MeshBasedTissue.hpp
@@ -100,6 +100,7 @@ protected:
     /** Whether to use a viscosity that is linear in the cell area, rather than constant. */
     bool mUseAreaBasedDampingConstant;
 
+    /** Needed for serialization. */
     friend class boost::serialization::access;
     /**
      * Serialize the facade.

notforrelease_cancer/test/simulation/TestTissueSimulationWithNutrients.hpp
@@ -517,6 +517,9 @@ public:
         simulator.SetOutputDirectory("TestCoarseNutrientMesh");
         simulator.SetEndTime(0.05);
 
+        // Coverage
+        simulator.SetAveragedSinksPde(&pde);
+
         // Set up cell killer and pass into simulation
         OxygenBasedCellKiller<2> killer(&tissue);
         simulator.AddCellKiller(&killer);
@@ -876,81 +879,66 @@ public:
         CellwiseData<2>::Destroy();
     }
 
+    void Test3DTissueSimulationWithNutrients() throw(Exception)
+    {
+        EXIT_IF_PARALLEL; //defined in PetscTools
 
-///// seems to work ok:
-//    void xTestWithOxygen3D() throw(Exception)
-//    {
-//        EXIT_IF_PARALLEL; //defined in PetscTools
-//
-//        CancerParameters::Instance()->SetHepaOneParameters();
-//
-//        TrianglesMeshReader<3,3> mesh_reader("mesh/test/data/cube_136_elements");
-//        MutableMesh<3,3> mesh;
-//        mesh.ConstructFromMeshReader(mesh_reader);
-//
-//        TrianglesMeshWriter<3,3> mesh_writer("TestSolveMethodSpheroidSimulation3DMesh","StartMesh");
-//        mesh_writer.WriteFilesUsingMesh(mesh);
-//
-//        // Set up cells
-//        std::vector<TissueCell> cells;
-//        FixedCellCycleModelCellsGenerator<3> generator;
-//        generator.GenerateBasic(cells, mesh.GetNumNodes());
-//
-//        // Set up tissue
-//        MeshBasedTissue<3> tissue(mesh, cells);
-//
-//        // Set up CellwiseData and associate it with the tissue
-//        CellwiseData<3>* p_data = CellwiseData<3>::Instance();
-//        p_data->SetNumNodesAndVars(mesh.GetNumNodes(),1);
-//        p_data->SetTissue(tissue);
-//
-//        // Since values are first passed in to CellwiseData before it is updated in PostSolve(),
-//        // we need to pass it some initial conditions to avoid memory errors
-//        for (unsigned i=0; i<mesh.GetNumNodes(); i++)
-//        {
-//            p_data->SetValue(1.0, mesh.GetNode(i));
-//        }
-//
-//        // Set up PDE
-//        SimpleNutrientPde<3> pde(0.1);
-//
-//        Meineke2001SpringSystem<3>* p_spring_system = new Meineke2001SpringSystem<3>(tissue);
-//        p_spring_system->UseCutoffPoint(1.5);
-//
-//        // Set up tissue simulation
-//        TissueSimulationWithNutrients<3> simulator(tissue, p_spring_system, &pde);
-//        simulator.SetOutputDirectory("TissueSimulationWithOxygen3d");
-//        simulator.SetEndTime(0.5);
-//
-//        // Set up cell killer and pass into simulation
-//        AbstractCellKiller<3>* p_killer = new OxygenBasedCellKiller<3>(&tissue);
-//        simulator.AddCellKiller(p_killer);
-//
-//        // Run tissue simulation
-//        TS_ASSERT_THROWS_NOTHING(simulator.Solve());
-////
-//
-////        // Test positions
-////        std::vector<double> node_5_location = simulator.GetNodeLocation(5);
-////        TS_ASSERT_DELTA(node_5_location[0], 0.4968, 1e-4);
-////        TS_ASSERT_DELTA(node_5_location[1], 0.8635, 1e-4);
-////
-////        std::vector<double> node_15_location = simulator.GetNodeLocation(15);
-////        TS_ASSERT_DELTA(node_15_location[0], 0.4976, 1e-4);
-////        TS_ASSERT_DELTA(node_15_location[1], 2.5977, 1e-4);
-////
-////        // Test the CellwiseData result
-////        TissueCell* p_cell = &(simulator.rGetTissue().rGetCellUsingLocationIndex(5));
-////        TS_ASSERT_DELTA(CellwiseData<2>::Instance()->GetValue(p_cell), 0.9604, 1e-4);
-////
-////        p_cell = &(simulator.rGetTissue().rGetCellUsingLocationIndex(15));
-////        TS_ASSERT_DELTA(CellwiseData<2>::Instance()->GetValue(p_cell), 0.9584, 1e-4);
-//
-//        // Tidy up
-//        delete p_spring_system;
-//        delete p_killer;
-//        CellwiseData<2>::Destroy();
-//    }
+        CancerParameters::Instance()->SetHepaOneParameters();
+
+        TrianglesMeshReader<3,3> mesh_reader("mesh/test/data/cube_136_elements");
+        MutableMesh<3,3> mesh;
+        mesh.ConstructFromMeshReader(mesh_reader);
+
+        TrianglesMeshWriter<3,3> mesh_writer("TestSolveMethodSpheroidSimulation3DMesh","StartMesh");
+        mesh_writer.WriteFilesUsingMesh(mesh);
+
+        // Set up cells
+        std::vector<TissueCell> cells;
+        FixedCellCycleModelCellsGenerator<3> generator;
+        generator.GenerateBasic(cells, mesh.GetNumNodes());
+
+        // Set up tissue
+        MeshBasedTissue<3> tissue(mesh, cells);
+
+        // Set up CellwiseData and associate it with the tissue
+        CellwiseData<3>* p_data = CellwiseData<3>::Instance();
+        p_data->SetNumNodesAndVars(mesh.GetNumNodes(),1);
+        p_data->SetTissue(tissue);
+
+        // Since values are first passed in to CellwiseData before it is updated in PostSolve(),
+        // we need to pass it some initial conditions to avoid memory errors
+        for (unsigned i=0; i<mesh.GetNumNodes(); i++)
+        {
+            p_data->SetValue(1.0, mesh.GetNode(i));
+        }
+
+        // Set up PDE
+        SimpleNutrientPde<3> pde(0.1);
+
+        // Set up force law
+        GeneralisedLinearSpringForce<3> linear_force;
+        linear_force.UseCutoffPoint(1.5);
+        std::vector<AbstractForce<3>*> force_collection;
+        force_collection.push_back(&linear_force);
+
+        // Set up tissue simulation
+        TissueSimulationWithNutrients<3> simulator(tissue, force_collection, &pde);
+        simulator.SetOutputDirectory("TissueSimulationWithOxygen3d");
+        simulator.SetEndTime(0.5);
+
+        // Set up cell killer and pass into simulation
+        AbstractCellKiller<3>* p_killer = new OxygenBasedCellKiller<3>(&tissue);
+        simulator.AddCellKiller(p_killer);
+
+        // Coverage
+        TS_ASSERT_THROWS_ANYTHING(simulator.CreateCoarseNutrientMesh(10.0));
+
+        // Run tissue simulation
+        TS_ASSERT_THROWS_NOTHING(simulator.Solve());
+
+        // Tidy up
+        CellwiseData<3>::Destroy();
+    }
 
 
 };

cancer/src/common/CancerParameters.hpp
@@ -556,7 +556,7 @@ private:
      */
     double mCellCellAdhesionEnergyParameter;
 
-
+    /** Needed for serialization. */
     friend class boost::serialization::access;
     /**
      * As with other singleton classes, ensure the instance of this

cancer/src/common/SimulationTime.hpp
@@ -206,6 +206,7 @@ private:
      */
     bool mStartTimeSet;
 
+    /** Needed for serialization. */
     friend class boost::serialization::access;
     /**
      * Serialization of a SimulationTime object must be done with care.

cancer/src/mesh/Cylindrical2dMesh.hpp
@@ -190,6 +190,8 @@ private:
      */
     bool IsThisIndexInList(const unsigned& rNodeIndex, const std::vector<unsigned>& rListOfNodes);
 
+    /** Needed for serialization. */
+    friend class boost::serialization::access;
     /**
      * Archives the member variables of the Cylindrical2dMesh class which
      * have to be preserved during the lifetime of the mesh.
@@ -197,7 +199,6 @@ private:
      * The remaining member variables are re-initialised before being used
      * by each ReMesh() call so they do not need to be archived.
      */
-    friend class boost::serialization::access;
     template<class Archive>
     void serialize(Archive & archive, const unsigned int version)
     {

cancer/src/odes/IngeWntSwatCellCycleOdeSystem.hpp
@@ -243,7 +243,7 @@ public:
      * Calculate whether the conditions for the cell cycle to finish have been met.
      *
      * @param time at which to calculate whether the stopping event has occurred
-     * @param rY value of the solution vector used to evaluate the RHS.
+     * @param rY value of the solution vector used to evaluate the RHS
      * 
      * @return whether or not stopping conditions have been met
      */
@@ -252,6 +252,9 @@ public:
     /**
      * When using CVODE this function is called instead of CalculateStoppingEvent.
      * It allows the point at which rY[1] reaches 1 to be found to greater precision.
+     * 
+     * @param time at which to calculate whether the stopping event has occurred
+     * @param rY value of the solution vector used to evaluate the RHS
      * 
      * @return How close we are to the root of the stopping condition
      */

cancer/src/odes/TysonNovak2001OdeSystem.hpp
@@ -34,50 +34,92 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 #include "AbstractOdeSystemWithAnalyticJacobian.hpp"
 
 /**
- * Represents the Tyson & Novak 2001 system of ODEs.
+ * Represents the Tyson & Novak (2001) system of ODEs.
+ * [doi:10.1006/jtbi.2001.2293]
  */
 class TysonNovak2001OdeSystem : public AbstractOdeSystemWithAnalyticJacobian
 {
 private:
 
-    // Parameters for the Tyson Novak 2001 model
+    /**
+     * Parameters for the Tyson & Novak (2001) model.
+     */
+
+    /** Dimensional parameter k_1. */
     double mK1;
+    /** Dimensional parameter k_2'. */
     double mK2d;
+    /** Dimensional parameter k_2''. */
     double mK2dd;
+    /** Dimensional parameter k_2'''. */
     double mK2ddd;
+    /** Dimensionless parameter [CycB]_threshold. */
     double mCycB_threshold;
+    /** Dimensional parameter k_3'. */
     double mK3d;
+    /** Dimensional parameter k_3''. */
     double mK3dd;
+    /** Dimensional parameter k_4'. */
     double mK4d;
+    /** Dimensional parameter k_4. */
     double mK4;
+    /** Dimensionless parameter J_3. */
     double mJ3;
+    /** Dimensionless parameter J_4. */
     double mJ4;
+    /** Dimensional parameter k_5'. */
     double mK5d;
+    /** Dimensional parameter k_5''. */
     double mK5dd;
+    /** Dimensional parameter k_6. */
     double mK6;
+    /** Dimensionless parameter J_5. */
     double mJ5;
+    /** Dimensionless parameter n. */
     double mN;
+    /** Dimensional parameter k_7. */
     double mK7;
+    /** Dimensional parameter k_8. */
     double mK8;
+    /** Dimensionless parameter J_7. */
     double mJ7;
+    /** Dimensionless parameter J_8. */
     double mJ8;
+    /** Dimensionless parameter [Mad]. */
     double mMad;
+    /** Dimensional parameter k_9. */
     double mK9;
+    /** Dimensional parameter k_10. */
     double mK10;
+    /** Dimensional parameter k_11. */
     double mK11;
+    /** Dimensional parameter k_12'. */
     double mK12d;
+    /** Dimensional parameter k_12''. */
     double mK12dd;
+    /** Dimensional parameter k_12'''. */
     double mK12ddd;
+    /** Dimensionless parameter K_eq. */
     double mKeq;
+    /** Dimensional parameter k_13. */
     double mK13;
+    /** Dimensional parameter k_14. */
     double mK14;
+    /** Dimensional parameter k_15'. */
     double mK15d;
+    /** Dimensional parameter k_15''. */
     double mK15dd;
+    /** Dimensional parameter k_16'. */
     double mK16d;
+    /** Dimensional parameter k_16''. */
     double mK16dd;
+    /** Dimensionless parameter J_15. */
     double mJ15;
+    /** Dimensionless parameter J_16. */
     double mJ16;
+    /** Dimensional parameter mu. */
     double mMu;
+    /** Dimensionless parameter m_star. */
     double mMstar;
 
 public:

cancer/src/odes/WntCellCycleOdeSystem.hpp
@@ -55,31 +55,59 @@ class WntCellCycleOdeSystem : public AbstractOdeSystem
 {
 private:
 
-    // Parameters for the Swat et al. (2004) Model
+    /**
+     * Parameters for the Swat et al. (2004) model
+     */
+
+    /** Dimensional parameter k_2. */
     double mk2d;
+    /** Dimensional parameter k_3. */
     double mk3d;
+    /** Dimensional parameter k_34. */
     double mk34d;
+    /** Dimensional parameter k_2. */
     double mk43d;
+    /** Dimensional parameter k_23. */
     double mk23d;
+    /** Dimensional parameter a. */
     double mad;
+    /** Dimensional parameter J_11. */
     double mJ11d;
+    /** Dimensional parameter J_12. */
     double mJ12d;
+    /** Dimensional parameter J_13. */
     double mJ13d;
+    /** Dimensional parameter J_13. */
     double mJ61d;
+    /** Dimensional parameter J_62. */
     double mJ62d;
+    /** Dimensional parameter J_63. */
     double mJ63d;
+    /** Dimensional parameter K_m1. */
     double mKm1d;
+    /** Dimensional parameter k_p. */
     double mkpd;
+    /** Dimensionless parameter phi_r. */
     double mphi_r;
+    /** Dimensionless parameter phi_i. */
     double mphi_i;
+    /** Dimensionless parameter phi_j. */
     double mphi_j;
+    /** Dimensionless parameter phi_p. */
     double mphi_p;
+    /** Dimensional parameter a_2. */
     double ma2d;
+    /** Dimensional parameter a_3. */
     double ma3d;
+    /** Dimensional parameter a_4. */
     double ma4d;
+    /** Dimensional parameter a_5. */
     double ma5d;
+    /** Dimensional parameter k_16. */
     double mk16d;
+    /** Dimensional parameter k_61. */
     double mk61d;
+    /** Dimensionless parameter phi_E2F1. */
     double mPhiE2F1;
 
     /** The mutation state of the cell - Wnt pathway behaviour (and hence cell cycle time) changes depending on this */
@@ -138,7 +166,10 @@ public:
     /**
      * This also contains a calculation of dY[1], copied from EvaluateYDerivatives.
      * Ensure they do not get out of sync!
-     *
+     * 
+     * @param time at which to calculate whether the stopping event has occurred
+     * @param rY value of the solution vector used to evaluate the RHS
+     * 
      * @return whether we have reached the stopping event
      */
     bool CalculateStoppingEvent(double time, const std::vector<double> &rY);
@@ -147,6 +178,9 @@ public:
      * When using CVODE this function is called instead of CalculateStoppingEvent.
      * It allows the point at which rY[1] reaches 1 to be found to greater precision.
      *
+     * @param time at which to calculate whether the stopping event has occurred
+     * @param rY value of the solution vector used to evaluate the RHS
+     * 
      * @return function value - giving CVODE an estimate of how close we are to the root.
      */
     double CalculateRootFunction(double time, const std::vector<double> &rY);

commit 430503eb80fcc6fff2ce5396b5a7b2d144ad48f6

Fix memory leak introduced in r5380.  Fix coverage
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5401 b5f69653-2bf5-0310-8201-b0dafb8e1405

cancer/src/tissue/MeshBasedTissue.hpp
@@ -53,7 +53,7 @@ class MeshBasedTissue : public AbstractCellCentreBasedTissue<DIM>
     friend class TestMeshBasedTissue;
 
 protected:
-
+#define COVERAGE_IGNORE //Avoid prototypes being treated as code by gcov
     /** Reference to the mesh. */
     MutableMesh<DIM, DIM>& mrMesh;
 
@@ -99,6 +99,7 @@ protected:
 
     /** Whether to use a viscosity that is linear in the cell area, rather than constant. */
     bool mUseAreaBasedDampingConstant;
+#undef COVERAGE_IGNORE //Avoid prototypes being treated as code by gcov
 
     /** Needed for serialization. */
     friend class boost::serialization::access;
@@ -150,6 +151,7 @@ protected:
     virtual void Validate();
 
 public:
+#define COVERAGE_IGNORE //Avoid prototypes being treated as code by gcov
 
     /**
      * Create a new tissue facade from a mesh and collection of cells.
@@ -481,6 +483,7 @@ public:
     void UnmarkSpring(TissueCell&, TissueCell&);
 
 };
+#undef COVERAGE_IGNORE //Avoid prototypes being treated as code by gcov
 
 
 #include "TemplatedExport.hpp"

notforrelease_cancer/test/simulation/TestTissueSimulationWithNutrients.hpp
@@ -938,6 +938,8 @@ public:
 
         // Tidy up
         CellwiseData<3>::Destroy();
+        
+        delete p_killer;
     }
 
 