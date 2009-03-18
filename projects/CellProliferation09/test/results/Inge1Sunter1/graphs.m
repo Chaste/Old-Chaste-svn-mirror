a7577d01e609cc692c7cf361f6380c62a660a919
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2009-03-18 12:39:48 +0000

    We'll defer this for now.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/tags/release_1@5544 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- projects/CellProliferation09/test/results/Inge1Sunter1/graphs.m
+++
@@ -1,9 +0,0 @@
-% Calls the plotting function specifying a title and this file path.
-close all
-clear
-
-title_string = 'Van Leeuwen Hypothesis I Cells in Sunter i) Geometry';
-this_path = pwd;
-
-addpath('../');
-plot_40_9_data(title_string, this_path);
--- projects/CellProliferation09/test/results/StochasticWntSunter1/graphs.m
+++
@@ -1,9 +0,0 @@
-% Calls the plotting function specifying a title and this file path.
-close all
-clear
-
-title_string = 'Wnt ODE Model Cells in Sunter i) Geometry';
-this_path = pwd;
-
-addpath('../');
-plot_40_9_data(title_string, this_path);
