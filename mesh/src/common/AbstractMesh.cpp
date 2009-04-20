700beb137cb0a4b06593f8c441629381e27aaf0e
Alex Fletcher <fletcher@maths.ox.ac.uk>
2009-04-20 17:26:44 +0000

    Try some mesh EI (it's a win-win situation - if this breaks the build then I get to experience the new build-break email)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5757 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ mesh/src/common/AbstractMesh.cpp
@@ -0,0 +1,236 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "AbstractMesh.hpp"
+
+///////////////////////////////////////////////////////////////////////////////////
+// Implementation
+///////////////////////////////////////////////////////////////////////////////////
+
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::SetElementOwnerships(unsigned lo, unsigned hi)
+{
+    assert(hi >= lo);
+    for (unsigned element_index=0; element_index<this->mElements.size(); element_index++)
+    {
+        Element<ELEMENT_DIM, SPACE_DIM>* p_element=this->mElements[element_index];
+        p_element->SetOwnership(false);
+        for (unsigned local_node_index=0; local_node_index< p_element->GetNumNodes(); local_node_index++)
+        {
+            unsigned global_node_index = p_element->GetNodeGlobalIndex(local_node_index);
+            if (lo<=global_node_index && global_node_index<hi)
+            {
+                p_element->SetOwnership(true);
+                break;
+            }
+        }
+    }
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+AbstractMesh<ELEMENT_DIM, SPACE_DIM>::AbstractMesh()
+    : mMeshFileBaseName("")
+{
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+AbstractMesh<ELEMENT_DIM, SPACE_DIM>::~AbstractMesh()
+{
+    // Iterate over nodes and free the memory
+    for (unsigned i=0; i<this->mNodes.size(); i++)
+    {
+        delete this->mNodes[i];
+    }
+    // Iterate over elements and free the memory
+    for (unsigned i=0; i<this->mElements.size(); i++)
+    {
+        delete this->mElements[i];
+    }
+    // Iterate over boundary elements and free the memory
+    for (unsigned i=0; i<this->mBoundaryElements.size(); i++)
+    {
+        delete this->mBoundaryElements[i];
+    }
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumNodes() const
+{
+    return this->mNodes.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumElements() const
+{
+    return this->mElements.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumBoundaryNodes()
+{
+    return this->mBoundaryNodes.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumAllNodes() const
+{
+    return this->mNodes.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumAllElements()
+{
+    return this->mElements.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumAllBoundaryElements()
+{
+    return this->mBoundaryElements.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+unsigned AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNumBoundaryElements() const
+{
+    return this->mBoundaryElements.size();
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+Node<SPACE_DIM>* AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetNode(unsigned index) const
+{
+    unsigned local_index = SolveNodeMapping(index);
+    return this->mNodes[local_index];
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+Element<ELEMENT_DIM, SPACE_DIM>* AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetElement(unsigned index) const
+{
+    unsigned local_index = SolveElementMapping(index);
+    return this->mElements[local_index];
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+BoundaryElement<ELEMENT_DIM-1, SPACE_DIM>* AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetBoundaryElement(unsigned index) const
+{
+    unsigned local_index = SolveBoundaryElementMapping(index);
+    return this->mBoundaryElements[local_index];
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::ReadNodesPerProcessorFile(const std::string& nodesPerProcessorFile)
+{
+    NEVER_REACHED;
+}
+
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+std::vector<unsigned>& AbstractMesh<ELEMENT_DIM, SPACE_DIM>::rGetNodesPerProcessor()
+{
+    return mNodesPerProcessor;
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::PermuteNodes()
+{
+    NEVER_REACHED;
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::ElementIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetElementIteratorBegin() const
+{
+    return mElements.begin();
+}
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::ElementIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetElementIteratorEnd() const
+{
+    return mElements.end();
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::BoundaryElementIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetBoundaryElementIteratorBegin() const
+{
+    return mBoundaryElements.begin();
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::BoundaryElementIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetBoundaryElementIteratorEnd() const
+{
+    return mBoundaryElements.end();
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::BoundaryNodeIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetBoundaryNodeIteratorBegin() const
+{
+    return mBoundaryNodes.begin();
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::BoundaryNodeIterator AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetBoundaryNodeIteratorEnd() const
+{
+    return mBoundaryNodes.end();
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetInverseJacobianForElement(unsigned elementIndex, c_matrix<double, SPACE_DIM, SPACE_DIM>& rJacobian, double &rJacobianDeterminant, c_matrix<double, SPACE_DIM, SPACE_DIM>& rInverseJacobian) const
+{
+    mElements[SolveElementMapping(elementIndex)]->CalculateInverseJacobian(rJacobian, rJacobianDeterminant, rInverseJacobian);
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetWeightedDirectionForBoundaryElement(unsigned elementIndex, c_vector<double, SPACE_DIM>& rWeightedDirection, double &rJacobianDeterminant) const
+{
+    mBoundaryElements[SolveBoundaryElementMapping(elementIndex)]->CalculateWeightedDirection(rWeightedDirection, rJacobianDeterminant );
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+std::string AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetMeshFileBaseName() const
+{
+    if (mMeshFileBaseName == "")
+    {
+        EXCEPTION("This mesh was not constructed from a file.");
+    }
+
+    return mMeshFileBaseName;
+}
+
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+std::vector<unsigned>& AbstractMesh<ELEMENT_DIM, SPACE_DIM>::rGetNodePermutation()
+{
+    return mNodesPermutation;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////////////////////
+
+template class AbstractMesh<1,1>;
+template class AbstractMesh<1,2>;
+template class AbstractMesh<2,2>;
+template class AbstractMesh<2,3>;
+template class AbstractMesh<3,3>;
---
+++ mesh/src/common/CuboidMeshConstructor.cpp
@@ -0,0 +1,101 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#include "CuboidMeshConstructor.hpp"
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// Implementation
+///////////////////////////////////////////////////////////////////////////////////
+
+
+template<unsigned DIM>
+void CuboidMeshConstructor<DIM>::ConstructHyperCube(TetrahedralMesh<1,1> &rMesh, unsigned width)
+{
+    rMesh.ConstructLinearMesh(width);
+}
+
+template<unsigned DIM>
+void CuboidMeshConstructor<DIM>::ConstructHyperCube(TetrahedralMesh<2,2> &rMesh, unsigned width)
+{
+    rMesh.ConstructRectangularMesh(width, width);
+}
+
+template<unsigned DIM>
+void CuboidMeshConstructor<DIM>::ConstructHyperCube(TetrahedralMesh<3,3> &rMesh, unsigned width)
+{
+    rMesh.ConstructCuboid(width, width, width);
+}
+
+template<unsigned DIM>
+std::string CuboidMeshConstructor<DIM>::Construct(unsigned meshNum, double meshWidth)
+{
+    mMeshWidth = meshWidth;
+    assert(meshNum < 30); //Sanity
+    const std::string mesh_dir = "ConvergenceMesh";
+    OutputFileHandler output_file_handler(mesh_dir);
+
+    // Create the mesh
+    unsigned mesh_size = (unsigned) pow(2, meshNum+2); // number of elements in each dimension
+    double scaling = mMeshWidth/(double) mesh_size;
+    TetrahedralMesh<DIM,DIM> mesh;
+    ConstructHyperCube(mesh, mesh_size);
+    mesh.Scale(scaling, scaling, scaling);
+    NumElements = mesh.GetNumElements();
+    NumNodes = mesh.GetNumNodes();
+    std::stringstream file_name_stream;
+    file_name_stream << "cube_" << DIM << "D_2mm_" << NumElements << "_elements";
+    std::string mesh_filename = file_name_stream.str();
+
+    if (output_file_handler.IsMaster())
+    {
+        TrianglesMeshWriter<DIM,DIM> mesh_writer(mesh_dir, mesh_filename, false);
+        mesh_writer.WriteFilesUsingMesh(mesh);
+    }
+    PetscTools::Barrier();
+
+    std::string mesh_pathname = output_file_handler.GetOutputDirectoryFullPath() + mesh_filename;
+
+    return mesh_pathname;
+}
+
+template<unsigned DIM>
+double CuboidMeshConstructor<DIM>::GetWidth()
+{
+    return mMeshWidth;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+//////////////////////////////////////////////////////////////////////////
+
+template class CuboidMeshConstructor<1>;
+template class CuboidMeshConstructor<2>;
+template class CuboidMeshConstructor<3>;
