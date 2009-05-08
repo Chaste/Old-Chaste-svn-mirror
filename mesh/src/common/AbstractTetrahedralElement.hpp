Chaste/Old-Chaste-svn-mirror

commit 4432c0951a73b7ab686832df73c9cf8dd87e8df2

#965 - fix issue on cygwin.  Hopefully won't break it for everyone else...
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5897 b5f69653-2bf5-0310-8201-b0dafb8e1405

mesh/src/common/AbstractTetrahedralElement.hpp
@@ -351,4 +351,136 @@ void AbstractTetrahedralElement<ELEMENT_DIM, SPACE_DIM>::GetStiffnessMatrixGloba
     }
 }
 
+
+//////////////////////////////////////////////////////////////////////
+//                  Specialization for 0d elements                  //
+//////////////////////////////////////////////////////////////////////
+
+/**
+ * Specialization for 0d elements so we don't get errors from Boost on some
+ * compilers.
+ */
+template<unsigned SPACE_DIM>
+class AbstractTetrahedralElement<0, SPACE_DIM> : public AbstractElement<0,SPACE_DIM>
+{
+public:
+
+    /**
+     * Constructor which takes in a vector of nodes.
+     *
+     * @param index  the index of the element in the mesh
+     * @param rNodes  the nodes owned by the element
+     */
+    AbstractTetrahedralElement(unsigned index, const std::vector<Node<SPACE_DIM>*>& rNodes);
+
+    /**
+     * Default constructor, which doesn't fill in any nodes.
+     * The nodes must be added later.
+     *
+     * @param index  the index of the element in the mesh (defaults to INDEX_IS_NOT_USED)
+     */
+    AbstractTetrahedralElement(unsigned index=INDEX_IS_NOT_USED);
+
+    /**
+     * Virtual destructor, since this class has virtual methods.
+     */
+    virtual ~AbstractTetrahedralElement()
+    {}
+
+    /**
+     * Get the location of the centroid of the element.
+     */
+    c_vector<double, SPACE_DIM> CalculateCentroid() const;
+
+    /**
+     * Compute the weighted direction for this element.
+     *
+     * @param rWeightedDirection  the weighted direction vector
+     * @param rJacobianDeterminant  the determinant of the Jacobian
+     * @param concreteMove \todo this argument is not used in the method - should it be removed? (defaults to true)
+     */
+    void CalculateWeightedDirection(c_vector<double, SPACE_DIM>& rWeightedDirection, double &rJacobianDeterminant, bool concreteMove=true);
+
+    /**
+     * Place in the pIndices array, the global indices (within the stiffness matrix)
+     * of the degrees of freedom associated with this element.
+     *
+     * @param problemDim the problem dimension e.g. 2 for Bidomain.
+     * @param pIndices where to store results: an unsigned array with ELEMENT_DIM+1 entries.
+     *
+     */
+    void GetStiffnessMatrixGlobalIndices(unsigned problemDim, unsigned* pIndices) const;
+};
+
+
+template<unsigned SPACE_DIM>
+AbstractTetrahedralElement<0, SPACE_DIM>::AbstractTetrahedralElement(unsigned index, const std::vector<Node<SPACE_DIM>*>& rNodes)
+    : AbstractElement<0, SPACE_DIM>(index, rNodes)
+{
+    // Sanity checking
+    unsigned total_nodes = 1;
+    assert(this->mNodes.size() == total_nodes);
+    assert(SPACE_DIM > 0);
+
+    // This is so we know it's the first time of asking
+    // Create Jacobian
+    ///\todo We don't want to create new data, calculation and throw the answer away
+    c_vector<double, SPACE_DIM> weighted_direction;
+    double det;
+    
+    CalculateWeightedDirection(weighted_direction, det);
+
+    // If determinant < 0 then element nodes are listed clockwise.
+    // We want them anticlockwise.
+    assert(det > 0.0);
+}
+
+template<unsigned SPACE_DIM>
+AbstractTetrahedralElement<0, SPACE_DIM>::AbstractTetrahedralElement(unsigned index)
+    : AbstractElement<0, SPACE_DIM>(index)
+{
+}
+
+template<unsigned SPACE_DIM>
+void AbstractTetrahedralElement<0, SPACE_DIM>::CalculateWeightedDirection(
+        c_vector<double, SPACE_DIM>& rWeightedDirection,
+        double &rJacobianDeterminant,
+        bool concreteMove)
+{
+    assert(SPACE_DIM > 0);
+
+    // End point of a line
+    rWeightedDirection = zero_vector<double>(SPACE_DIM);
+    rWeightedDirection(0) = 1.0;
+
+    rJacobianDeterminant = norm_2(rWeightedDirection);
+    if (rJacobianDeterminant < DBL_EPSILON)
+    {
+        EXCEPTION("Jacobian determinant is zero");
+    }
+}
+
+template<unsigned SPACE_DIM>
+c_vector<double, SPACE_DIM> AbstractTetrahedralElement<0, SPACE_DIM>::CalculateCentroid() const
+{
+    c_vector<double, SPACE_DIM> centroid = this->mNodes[0]->rGetLocation();
+    return centroid;
+}
+
+template<unsigned SPACE_DIM>
+void AbstractTetrahedralElement<0, SPACE_DIM>::GetStiffnessMatrixGlobalIndices(unsigned problemDim, unsigned* pIndices) const
+{
+    for (unsigned local_index=0; local_index<1; local_index++)
+    {
+        unsigned node = this->GetNodeGlobalIndex(local_index);
+
+        for (unsigned problem_index=0; problem_index<problemDim; problem_index++)
+        {
+            //std::cout << local_index*problemDim + problem_index << std::endl;
+            pIndices[local_index*problemDim + problem_index] = node*problemDim + problem_index;
+        }
+    }
+}
+
+
 #endif //_ABSTRACTTETRAHEDRALELEMENT_HPP_

mesh/src/common/AbstractMesh.cpp
@@ -196,13 +196,20 @@ typename AbstractMesh<ELEMENT_DIM, SPACE_DIM>::BoundaryNodeIterator AbstractMesh
 }
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
-void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetInverseJacobianForElement(unsigned elementIndex, c_matrix<double, SPACE_DIM, ELEMENT_DIM>& rJacobian, double &rJacobianDeterminant, c_matrix<double, ELEMENT_DIM, SPACE_DIM>& rInverseJacobian) const
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetInverseJacobianForElement(
+        unsigned elementIndex,
+        c_matrix<double, SPACE_DIM, ELEMENT_DIM>& rJacobian,
+        double &rJacobianDeterminant,
+        c_matrix<double, ELEMENT_DIM, SPACE_DIM>& rInverseJacobian) const
 {
     mElements[SolveElementMapping(elementIndex)]->CalculateInverseJacobian(rJacobian, rJacobianDeterminant, rInverseJacobian);
 }
 
 template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
-void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetWeightedDirectionForBoundaryElement(unsigned elementIndex, c_vector<double, SPACE_DIM>& rWeightedDirection, double &rJacobianDeterminant) const
+void AbstractMesh<ELEMENT_DIM, SPACE_DIM>::GetWeightedDirectionForBoundaryElement(
+        unsigned elementIndex,
+        c_vector<double, SPACE_DIM>& rWeightedDirection,
+        double &rJacobianDeterminant) const
 {
     mBoundaryElements[SolveBoundaryElementMapping(elementIndex)]->CalculateWeightedDirection(rWeightedDirection, rJacobianDeterminant );
 }

commit 76d464ed083a20e6d2f1c49f7ab1b1af1958ac4a

Fix profile build error caused by r5897 (Cygwin template for ELEMENT_DIM=0)
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@5906 b5f69653-2bf5-0310-8201-b0dafb8e1405

mesh/src/common/AbstractTetrahedralElement.hpp
@@ -386,8 +386,8 @@ AbstractTetrahedralElement<0, SPACE_DIM>::AbstractTetrahedralElement(unsigned in
     : AbstractElement<0, SPACE_DIM>(index, rNodes)
 {
     // Sanity checking
-    unsigned total_nodes = 1;
-    assert(this->mNodes.size() == total_nodes);
+    //unsigned total_nodes = 1;
+    assert(this->mNodes.size() == 1);
     assert(SPACE_DIM > 0);
 
     // This is so we know it's the first time of asking