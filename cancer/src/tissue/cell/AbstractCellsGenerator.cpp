83cdf5314cdbe36a4865545b74a98438f19d547e
Alex Fletcher <fletcher@maths.ox.ac.uk>
2009-06-29 15:41:32 +0000

    #847 - bit more EI
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@6377 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ cancer/src/tissue/cell/AbstractCellsGenerator.cpp
@@ -0,0 +1,163 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "AbstractCellsGenerator.hpp"
+
+///////////////////////////////////////////////////////////////////////////////////
+// Implementation
+///////////////////////////////////////////////////////////////////////////////////
+
+template<unsigned DIM>
+bool AbstractCellsGenerator<DIM>::CellsCanDifferentiate()
+{
+    return false;
+}
+
+template<unsigned DIM>
+void AbstractCellsGenerator<DIM>::GenerateForCrypt(std::vector<TissueCell>& rCells,
+                                 TetrahedralMesh<2,2>& rMesh,
+                                 const std::vector<unsigned> locationIndices,
+                                 bool randomBirthTimes,
+                                 double y0,
+                                 double y1,
+                                 double y2,
+                                 double y3,
+                                 bool initialiseCells)
+{
+    #define COVERAGE_IGNORE
+    assert(DIM==2);
+    #undef COVERAGE_IGNORE
+
+    RandomNumberGenerator* p_random_num_gen = RandomNumberGenerator::Instance();
+
+    unsigned num_cells = rMesh.GetNumNodes();
+    if (!locationIndices.empty())
+    {
+        num_cells = locationIndices.size();
+    }
+
+    AbstractCellCycleModel* p_cell_cycle_model = NULL;
+    double typical_transit_cycle_time;
+    double typical_stem_cycle_time;
+
+    rCells.clear();
+    rCells.reserve(num_cells);
+
+    for (unsigned i=0; i<rMesh.GetNumNodes(); i++)
+    {
+        CellType cell_type;
+        unsigned generation;
+
+        double y = 0.0;
+        if (!locationIndices.empty())
+        {
+            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
+            {
+                y = rMesh.GetNode(i)->GetPoint().rGetLocation()[1];
+            }
+        }
+        else
+        {
+            y = rMesh.GetNode(i)->GetPoint().rGetLocation()[1];
+        }
+
+        p_cell_cycle_model = CreateCellCycleModel();
+        typical_transit_cycle_time = this->GetTypicalTransitCellCycleTime();
+        typical_stem_cycle_time = GetTypicalStemCellCycleTime();
+
+        double birth_time = 0.0;
+        if (randomBirthTimes)
+        {
+            birth_time = -p_random_num_gen->ranf();
+        }
+
+        if (y <= y0)
+        {
+            cell_type = STEM;
+            generation = 0;
+            birth_time *= typical_stem_cycle_time; // hours
+        }
+        else if (y < y1)
+        {
+            cell_type = TRANSIT;
+            generation = 1;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+        else if (y < y2)
+        {
+            cell_type = TRANSIT;
+            generation = 2;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+        else if (y < y3)
+        {
+            cell_type = TRANSIT;
+            generation = 3;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+        else
+        {
+            cell_type = CellsCanDifferentiate() ? DIFFERENTIATED : TRANSIT;
+            generation = 4;
+            birth_time *= typical_transit_cycle_time; // hours
+        }
+
+        if (dynamic_cast<AbstractSimpleGenerationBasedCellCycleModel*>(p_cell_cycle_model))
+        {
+            static_cast<AbstractSimpleGenerationBasedCellCycleModel*>(p_cell_cycle_model)->SetGeneration(generation);
+        }
+
+        TissueCell cell(cell_type, HEALTHY, p_cell_cycle_model);
+        if (initialiseCells)
+        {
+            cell.InitialiseCellCycleModel();
+        }
+
+        cell.SetBirthTime(birth_time);
+
+        if (!locationIndices.empty())
+        {
+            if ( std::find(locationIndices.begin(), locationIndices.end(), i) != locationIndices.end() )
+            {
+                rCells.push_back(cell);
+            }
+        }
+        else
+        {
+            rCells.push_back(cell);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////////////
+
+template class AbstractCellsGenerator<1>;
+template class AbstractCellsGenerator<2>;
+template class AbstractCellsGenerator<3>;
---
+++ linalg/src/common/FourthOrderTensor.cpp
@@ -0,0 +1,177 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "FourthOrderTensor.hpp"
+
+///////////////////////////////////////////////////////////////////////////////////
+// Implementation
+///////////////////////////////////////////////////////////////////////////////////
+
+
+template<unsigned DIM>
+FourthOrderTensor<DIM>::FourthOrderTensor()
+{
+    // check dim>0 but <4
+    assert(DIM>0);
+    assert(DIM<4);
+
+    mDimSqd = DIM*DIM;
+    mDimCubed = DIM*DIM*DIM;
+    mDimToFour = DIM*DIM*DIM*DIM;
+
+    // allocate memory and zero entries
+    mData.resize(mDimToFour, 0.0);
+}
+
+template<unsigned DIM>
+void FourthOrderTensor<DIM>::SetAsProduct(FourthOrderTensor<DIM>& tensor, const c_matrix<double,DIM,DIM>& matrix, unsigned component)
+{
+    Zero();
+
+    // messy repeated code but needs to be efficiently over neat..
+    switch (component)
+    {
+        case 0:
+        {
+            for (unsigned M=0; M<DIM; M++)
+            {
+                for (unsigned N=0; N<DIM; N++)
+                {
+                    for (unsigned P=0; P<DIM; P++)
+                    {
+                        for (unsigned Q=0; Q<DIM; Q++)
+                        {
+                            unsigned index = M*mDimCubed + N*mDimSqd + P*DIM + Q;
+                            for (unsigned s=0; s<DIM; s++)
+                            {
+                                mData[index] += matrix(M,s) * tensor(s,N,P,Q);
+                            }
+                        }
+                    }
+                }
+            }
+            break;
+        }
+        case 1:
+        {
+            for (unsigned M=0; M<DIM; M++)
+            {
+                for (unsigned N=0; N<DIM; N++)
+                {
+                    for (unsigned P=0; P<DIM; P++)
+                    {
+                        for (unsigned Q=0; Q<DIM; Q++)
+                        {
+                            unsigned index = M*mDimCubed + N*mDimSqd + P*DIM + Q;
+                            for (unsigned s=0; s<DIM; s++)
+                            {
+                                mData[index] += matrix(N,s) * tensor(M,s,P,Q);
+                            }
+                        }
+                    }
+                }
+            }
+            break;
+        }
+        case 2:
+        {
+            for (unsigned M=0; M<DIM; M++)
+            {
+                for (unsigned N=0; N<DIM; N++)
+                {
+                    for (unsigned P=0; P<DIM; P++)
+                    {
+                        for (unsigned Q=0; Q<DIM; Q++)
+                        {
+                            unsigned index = M*mDimCubed + N*mDimSqd + P*DIM + Q;
+                            for (unsigned s=0; s<DIM; s++)
+                            {
+                                mData[index] += matrix(P,s) * tensor(M,N,s,Q);
+                            }
+                        }
+                    }
+                }
+            }
+            break;
+        }
+        case 3:
+        {
+            for (unsigned M=0; M<DIM; M++)
+            {
+                for (unsigned N=0; N<DIM; N++)
+                {
+                    for (unsigned P=0; P<DIM; P++)
+                    {
+                        for (unsigned Q=0; Q<DIM; Q++)
+                        {
+                            unsigned index = M*mDimCubed + N*mDimSqd + P*DIM + Q;
+                            for (unsigned s=0; s<DIM; s++)
+                            {
+                                mData[index] += matrix(Q,s) * tensor(M,N,P,s);
+                            }
+                        }
+                    }
+                }
+            }
+            break;
+        }
+        default:
+        {
+            EXCEPTION("Component not 0, 1, 2, or 3");
+        }
+    }
+}
+
+template<unsigned DIM>
+double& FourthOrderTensor<DIM>::operator()(unsigned M, unsigned N, unsigned P, unsigned Q)
+{
+    assert(M<DIM);
+    assert(N<DIM);
+    assert(P<DIM);
+    assert(Q<DIM);
+
+    unsigned index = M*mDimCubed + N*mDimSqd + P*DIM + Q;
+    return mData[index];
+}
+
+template<unsigned DIM>
+void FourthOrderTensor<DIM>::Zero()
+{
+    for (unsigned i=0; i<mDimToFour; i++)
+    {
+        mData[i] = 0.0;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////
+
+template class FourthOrderTensor<1>;
+template class FourthOrderTensor<2>;
+template class FourthOrderTensor<3>;
