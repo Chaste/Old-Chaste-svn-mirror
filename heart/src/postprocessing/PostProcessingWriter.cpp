Chaste/Old-Chaste-svn-mirror

commit d89e747c1b004c8e65d73cbad903d7336f38e030

Fix failing test after r7064
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@7076 b5f69653-2bf5-0310-8201-b0dafb8e1405

heart/src/postprocessing/PostProcessingWriter.cpp
@@ -131,7 +131,7 @@ void PostProcessingWriter<ELEMENT_DIM, SPACE_DIM>::WriteApdMapFile(double repola
             }
             catch(Exception& e)
             {
-                assert(e.GetShortMessage()=="No full action potential was recorded");
+                assert(e.GetShortMessage()=="No full action potential was recorded" || "No upstroke occurred");
                 apds.push_back(0);
                 assert(apds.size() == 1);
             }

commit bc0c14f3ca82403c7827ce4538f513ee3f8ad04b

Temporary fix (Intel) build broken in r7076
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@7077 b5f69653-2bf5-0310-8201-b0dafb8e1405

heart/src/postprocessing/PostProcessingWriter.cpp
@@ -131,7 +131,6 @@ void PostProcessingWriter<ELEMENT_DIM, SPACE_DIM>::WriteApdMapFile(double repola
             }
             catch(Exception& e)
             {
-                assert(e.GetShortMessage()=="No full action potential was recorded" || "No upstroke occurred");
                 apds.push_back(0);
                 assert(apds.size() == 1);
             }