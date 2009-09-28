41868379d2dd3eaa88df0e48662b171a1da5783b
Alberto Corrias <alberto@nus.edu.sg>
2009-09-28 15:22:09 +0000

    #610 - implemented pseudo-ECG calculator and tested in two simple 1D cases.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@7080 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ heart/src/postprocessing/PseudoEcgCalculator.hpp
@@ -0,0 +1,122 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _PSEUDOECGALCALCULATOR_HPP_
+#define _PSEUDOECGALCALCULATOR_HPP_
+
+#include "AbstractFunctionalCalculator.hpp"
+#include "TetrahedralMesh.hpp"
+#include "ChastePoint.hpp"
+#include "Hdf5DataReader.hpp"
+
+/**
+ * This class implements a pseudo-ECG calculator. It is a concrete class of AbstractFunctionalCalculator.
+ * The pseudo-ECG is defined as the integral over the mesh of the following integrand:
+ * 
+ * - D * grad (solution) dot grad (1/r)  
+ * 
+ *  where D is a diffusion coefficient and r is the distance between a recording electrode
+ *  and a given point in the mesh. 
+ * 
+ *  References for the formula that defines the pseudo-ECG: 
+ * 
+ *  Gima K, Rudy Y Circ Res (2002) 90:889-896 (equation 1)
+ *  Baher et al. Am J Physiol (2007) 292:H180-H189 (equation 5)
+ * 
+ * ///\ todo include definitions and set methods for D somewhere! It is assumed 1 at the moment.
+ */
+template<unsigned ELEMENT_DIM, unsigned SPACE_DIM, unsigned PROBLEM_DIM>
+class PseudoEcgCalculator : public AbstractFunctionalCalculator<ELEMENT_DIM, SPACE_DIM, PROBLEM_DIM>
+{
+private:
+
+    Hdf5DataReader* mpDataReader; /**< An HDF5 reader from which to get the solution*/
+    unsigned mNumberOfNodes; /**< Number of nodes in the mesh (got from the data reader)*/
+    unsigned mNumTimeSteps;/**< Number of time steps in the simulation (got from the data reader)*/
+    TetrahedralMesh<ELEMENT_DIM,SPACE_DIM>& mrMesh;/**< A mesh used by the calculator*/
+    ChastePoint<SPACE_DIM>& mrX; /**<The point from where we want to calculate the pseudoECG*/
+    
+    /**
+     * Get the integrand.
+     *
+     * @param rX The point in space
+     * @param rU The unknown as a vector, u(i) = u_i
+     * @param rGradU The gradient of the unknown as a matrix, rGradU(i,j) = d(u_i)/d(X_j)
+     */
+    double GetIntegrand(ChastePoint<SPACE_DIM>& rX,
+                                c_vector<double,PROBLEM_DIM>& rU,
+                                c_matrix<double,PROBLEM_DIM,SPACE_DIM>& rGradU);
+                        
+                                
+public:
+
+    /**
+     * Constructor
+     *
+     * @param rMesh A reference to the mesh
+     * @param rX The location of the recording electrode
+     * @param directory The directory where the simulation results are stored
+     * @oaram hdf5File The file name  where the simulation results are stored
+     * @param makeAbsolute whether to make the path of directory absolute (using the OutputFileHandler)
+     * 
+     * 
+     * ///\ todo pass in the name of the variable in the hdf5 file? it is currently hardcoded to "V"
+     */
+    PseudoEcgCalculator (TetrahedralMesh<ELEMENT_DIM,SPACE_DIM>& rMesh, 
+                         ChastePoint<SPACE_DIM>& rX,
+                         std::string directory, 
+                         std::string hdf5File, 
+                         bool makeAbsolute = true);
+    
+    /**
+     * Destructor.
+     */
+    ~PseudoEcgCalculator();
+    
+    /**
+     * Calculates the pseudo-ECG and stores the results in a vector
+     * 
+     * @return a standard vector containing the pseudo ECG.
+     * 
+     */
+    std::vector<double> ComputePseudoEcg ();
+    
+    /**
+     * Writes out the pseudo-ECG to file
+     * 
+     * @param fileName the name of the file (in the same directory as the hdf5)
+     * 
+     * ///\ todo think a good way to design this function (not yet implemented) 
+     * 
+     */
+    void WriteFullPseudoEcg (std::string fileName);
+
+};
+
+#endif //_PSEUDOECGALCALCULATOR_HPP_
+
---
+++ heart/test/postprocessing/TestPseudoEcgCalculator.hpp
@@ -0,0 +1,213 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2009
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#ifndef TESTPSEUDOECGCALCULATOR_HPP_
+#define TESTPSEUDOECGCALCULATOR_HPP_
+
+#include <cxxtest/TestSuite.h>
+#include <iostream>
+
+#include "TetrahedralMesh.hpp" //must be first, it gets UblasIncludes from the mesh classes (ChastePoint.hpp)
+#include "PseudoEcgCalculator.hpp"
+#include "ReplicatableVector.hpp"
+#include "Hdf5DataReader.hpp"
+#include "Hdf5DataWriter.hpp"
+#include "OutputFileHandler.hpp"
+#include "TrianglesMeshReader.hpp"
+#include "TrianglesMeshReader.hpp"
+#include "TetrahedralMesh.hpp"
+
+class TestPseudoEcgCalculator : public CxxTest::TestSuite
+{
+    
+public:
+    
+    void TestCalculator1DLinearGradient() throw (Exception)
+    {
+        //read in the 1D mesh, from 0 to 1
+        TrianglesMeshReader<1,1> reader("mesh/test/data/1D_0_to_1_100_elements");
+        TetrahedralMesh<1,1> mesh;
+        mesh.ConstructFromMeshReader(reader);
+        
+        ////////////////////////////////////////////////////
+        //First we write an hdf5 file with a gradient of V
+        // i.e. V(x) = x; We write 4 time steps.
+        ///////////////////////////////////////////////////
+        unsigned number_nodes = mesh.GetNumNodes();
+
+        DistributedVectorFactory factory(number_nodes);
+        Hdf5DataWriter writer(factory, "hdf5", "gradient_V", false);
+        writer.DefineFixedDimension(number_nodes);
+
+        int node_id = writer.DefineVariable("Node", "dimensionless");
+        //we call the variable like the default for the pseudoecg calculator
+        int V_id = writer.DefineVariable("V", "mV");
+        writer.DefineUnlimitedDimension("Time", "msec");
+        writer.EndDefineMode();
+
+        Vec petsc_data_1 = factory.CreateVec();
+        DistributedVector distributed_vector_1 = factory.CreateDistributedVector(petsc_data_1);
+        
+        Vec petsc_data_2 = factory.CreateVec();
+        DistributedVector distributed_vector_2 = factory.CreateDistributedVector(petsc_data_2);
+        
+        //4 time steps
+        for (unsigned time_step=0; time_step<4; time_step++)
+        {
+            // Write some values, the value of V is the same as the x coordinate, i.e. a gradient
+            for (DistributedVector::Iterator index = distributed_vector_1.Begin();
+                 index!= distributed_vector_1.End();
+                 ++index)
+            {
+                distributed_vector_1[index] = index.Global;
+                distributed_vector_2[index] = index.Global/100.0;
+            }
+            distributed_vector_1.Restore();
+            distributed_vector_2.Restore();
+
+            // Write the vector
+            writer.PutVector(node_id, petsc_data_1);
+            writer.PutVector(V_id, petsc_data_2);
+            writer.PutUnlimitedVariable(time_step);
+            writer.AdvanceAlongUnlimitedDimension();
+        }
+
+        writer.Close();
+        
+        VecDestroy(petsc_data_1);
+        VecDestroy(petsc_data_2);
+
+        ///////////////////////////////////////////////////
+        // Now we compute the pseudo ECG. We set an electrode at x=15.
+        ///////////////////////////////////////////////////
+        
+        ChastePoint<1> electrode;
+        electrode.SetCoordinate(0, 15.0);
+        
+        PseudoEcgCalculator<1,1,1> calculator (mesh, electrode, "hdf5", "gradient_V", true);
+        std::vector<double> pseudo_ecg;      
+        
+        pseudo_ecg = calculator.ComputePseudoEcg();
+        
+        // The expected result is the integral of: - d(gradV)*dgrad(1/r) in dx
+        // Because in this simple case d(gradV)=1, the result is simply -1/(15-x) evaluated 
+        // between 0 and 1 
+        double expected_result = -(1/14.0-1/15.0);
+        for (unsigned k = 0; k< pseudo_ecg.size(); k++)
+        {
+            TS_ASSERT_DELTA(pseudo_ecg[k], expected_result,1e-6);
+        } 
+        
+    }
+    
+    void TestCalculator1DParabolic() throw (Exception)
+    {
+        TrianglesMeshReader<1,1> reader("mesh/test/data/1D_0_to_1_100_elements");
+        TetrahedralMesh<1,1> mesh;
+        mesh.ConstructFromMeshReader(reader);
+        
+        ////////////////////////////////////////////////////
+        //First we write an hdf5 file with V as a parabolic function of x
+        // i.e. V(x) = x^2; We write 4 time steps.
+        ///////////////////////////////////////////////////
+        unsigned number_nodes = mesh.GetNumNodes();
+
+        DistributedVectorFactory factory(number_nodes);
+
+        Hdf5DataWriter writer(factory, "hdf5", "parabolic_V", false);
+        writer.DefineFixedDimension(number_nodes);
+
+        int node_id = writer.DefineVariable("Node", "dimensionless");
+        //we call the variable like the default for the pseudoecg calculator
+        int V_id = writer.DefineVariable("V", "mV");
+        writer.DefineUnlimitedDimension("Time", "msec");
+        writer.EndDefineMode();
+
+        Vec petsc_data_1 = factory.CreateVec();
+        DistributedVector distributed_vector_1 = factory.CreateDistributedVector(petsc_data_1);
+        
+        Vec petsc_data_2 = factory.CreateVec();
+        DistributedVector distributed_vector_2 = factory.CreateDistributedVector(petsc_data_2);
+        
+        //4 time steps
+        for (unsigned time_step=0; time_step<4; time_step++)
+        {
+            // Write some values, the value of V is x^2, i.e. a parabola
+            for (DistributedVector::Iterator index = distributed_vector_1.Begin();
+                 index!= distributed_vector_1.End();
+                 ++index)
+            {
+                distributed_vector_1[index] = index.Global;
+                distributed_vector_2[index] = (index.Global/100.0)*(index.Global/100.0);
+            }
+            distributed_vector_1.Restore();
+            distributed_vector_2.Restore();
+
+            // Write the vector
+            writer.PutVector(node_id, petsc_data_1);
+            writer.PutVector(V_id, petsc_data_2);
+            writer.PutUnlimitedVariable(time_step);
+            writer.AdvanceAlongUnlimitedDimension();
+        }
+
+        writer.Close();
+        
+        VecDestroy(petsc_data_1);
+        VecDestroy(petsc_data_2);
+
+        ///////////////////////////////////////////////////
+        // Now we compute the pseudo ECG. We set an electrode at x=15.
+        ///////////////////////////////////////////////////
+        
+        ChastePoint<1> electrode;
+        
+        electrode.SetCoordinate(0, 15.0);
+        
+        PseudoEcgCalculator<1,1,1> calculator (mesh, electrode, "hdf5", "parabolic_V", true);
+        
+        std::vector<double> pseudo_ecg; //stores the results
+        
+        pseudo_ecg = calculator.ComputePseudoEcg();
+        
+        // The expected result is the integral of: - d(gradV)*dgrad(1/r) in dx
+        // In this case d(gradV)/dx=2x, hence the result is the integral of: - (2x * d(1/(15-x))/dx)
+        // Integrating by parts with f = 2x and g = 1/(15-x) and evaluating between 0 and 1
+        
+        double expected_result = -( (2/14.0) - 2.0*log(1/14.0) + 2.0*log(1/15.0));        
+        for (unsigned k = 0; k< pseudo_ecg.size(); k++)
+        {
+            TS_ASSERT_DELTA(pseudo_ecg[k], expected_result,1e-6);
+        } 
+        
+    }
+
+};
+
+
+#endif /*TESTPSEUDOECGCALCULATOR_HPP_*/
