eac43859d8752efef1e09588ae2c19f18d1b9a98
Pras Pathmanathan <pras@cs.ox.ac.uk>
2010-02-15 18:07:13 +0000

    oops
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@8023 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- mesh/src/common/FineCoarseMeshPair.cpp
+++ mesh/src/common/FineCoarseMeshPair.cpp
@@ -147,7 +147,7 @@ void FineCoarseMeshPair<DIM>::ComputeFineElementsAndWeightsForCoarseQuadPoints(G
 
     for(unsigned i=0; i<quad_point_posns.Size(); i++)
     {
-        std::cout << "\r " << i << " of " << quad_point_posns.Size();
+        //std::cout << "\r " << i << " of " << quad_point_posns.Size();
         // get the box this point is in
         unsigned box_for_this_point = mpFineMeshBoxCollection->CalculateContainingBox( quad_point_posns.Get(i) );
         
