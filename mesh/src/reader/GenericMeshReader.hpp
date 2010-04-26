d5ae67a40a380ad999677c9fc9cce9653cc60c25
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2010-04-26 17:49:18 +0000

    #1323 Missed the important file in r8764.  Sorry.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@8765 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ mesh/src/reader/GenericMeshReader.hpp
@@ -0,0 +1,170 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2010
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _GENERICMESHREADER_HPP_
+#define _GENERICMESHREADER_HPP_
+
+#include "AbstractMeshReader.hpp"
+//Possible subclasses to delegate
+#include "TrianglesMeshReader.hpp"
+#include "MemfemMeshReader.hpp"
+
+/**
+ * A generic mesh reader
+ * Uses a delegated member variable of type AbstractMeshReader to probe for files
+ * which may be read via
+ *  - TrianglesMeshReader
+ *  - MemfemMeshReader
+ * 
+ * Probing is done during construction.
+ * 
+ * Thereafter all public methods use the public methods of the delegated class and 
+ * exceptions are passed back to the caller without being caught locally
+ */
+template <unsigned ELEMENT_DIM, unsigned SPACE_DIM>
+class GenericMeshReader : public AbstractMeshReader<ELEMENT_DIM, SPACE_DIM>
+{
+private:
+    /** Delegated mesh reader.
+     *  Used to probe various types of mesh reader
+     */
+    AbstractMeshReader<ELEMENT_DIM, SPACE_DIM>* mpMeshReader;
+public:
+    /**
+     * Constructor.
+     *
+     * @param pathBaseName  the base name of the files from which to read the mesh data
+     */
+    GenericMeshReader(std::string pathBaseName)
+    {
+        try
+        {
+            mpMeshReader=new TrianglesMeshReader<ELEMENT_DIM, SPACE_DIM>(pathBaseName);
+        }
+        catch (Exception triangles_exception)
+        {
+            try
+            {
+                mpMeshReader=new MemfemMeshReader<ELEMENT_DIM, SPACE_DIM>(pathBaseName);
+            }
+            catch (Exception memfem_exception)
+            {
+                throw memfem_exception; ///\todo #1323 a decent throw if they all fail - concantenate messages?
+        
+            }
+        }
+        
+    }
+    
+    
+    /**
+     * Destructor
+     */
+    ~GenericMeshReader()
+    {
+        delete mpMeshReader;
+    }
+
+
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline unsigned GetNumNodes() const
+    {
+        return mpMeshReader->GetNumNodes();
+    }
+
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline unsigned GetNumElements() const
+    {
+        return mpMeshReader->GetNumElements();
+    }
+
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline unsigned GetNumFaces() const
+    {
+        return mpMeshReader->GetNumFaces();
+    }
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline unsigned GetNumElementAttributes() const
+    {
+        return mpMeshReader->GetNumElementAttributes();
+    }
+
+/** \todo #1323 COVER
+ */
+
+//    /** 
+//     * Method uses the public method of the delegated mesh reader
+//     */
+//    inline unsigned GetNumFaceAttributes() const
+//    {
+//        return mpMeshReader->GetNumFaceAttributes();
+//    }
+
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline void Reset()
+    {
+        mpMeshReader->Reset();
+    }
+    
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline std::vector<double> GetNextNode()
+    {
+        return mpMeshReader->GetNextNode();
+    }
+
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline ElementData GetNextElementData()
+    {
+        return mpMeshReader->GetNextElementData();
+    }
+    /** 
+     * Method uses the public method of the delegated mesh reader
+     */
+    inline ElementData GetNextFaceData()
+    {
+        return mpMeshReader->GetNextFaceData();
+    }
+
+  
+};
+
+#endif //_GENERICMESHREADER_HPP_
