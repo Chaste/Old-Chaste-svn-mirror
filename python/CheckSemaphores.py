db7a3cb70acbc3614440c1f94a53f88e9920a89e
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2010-05-14 11:15:50 +0000

    Make semaphore test a bit stricter (so it fails before we get segnums)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@8903 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/CheckSemaphores.py
+++ python/CheckSemaphores.py
@@ -41,7 +41,7 @@ from pwd import getpwuid
 max_semaphore_arrays=int(os.popen('ipcs -l | awk \'/max number of arrays/\'').read().split()[-1])
 #On chaste-bob server (and other Ubuntu machines) the value is max_semaphore_arrays==128
 #MPI starts to fail when the number of semaphores reaches 128
-semaphore_limit = 3*max_semaphore_arrays/4
+semaphore_limit = 5*max_semaphore_arrays/8
 
 semaphore_data=os.popen('tail -n +2 /proc/sysvipc/sem  |awk \'{print $5}\'| sort | uniq -c').readlines()
 
