e0092ad596cea50325b6f760610523246eaf2305
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2010-05-25 14:22:51 +0000

    #1354 - more tests
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@8997 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/test/TestProtocol.py
+++ python/test/TestProtocol.py
@@ -64,8 +64,33 @@ class TestProtocol(unittest.TestCase):
         self.assertEqual(V.get_type(), pycml.VarTypes.State)
         self.assertEqual(V._get_dependencies(), [])
         self.assert_(V._get_ode_dependency(time) is dv_dt)
+        
+    def TestChangeMathsErrors(self):
+        """Test that changing mathematics reports errors for wrong inputs.
+        
+        In particular, it should throw if we try to define a Mapped variable
+        using mathematics. 
+        """
+        doc = self.LoadModel('heart/src/odes/cellml/luo_rudy_1991.cellml')
+        p = protocol.Protocol(doc.model, multi_stage=True)
+        # T is a Mapped variable in time_dependent_potassium_current
+        T = doc.model.get_variable_by_name(u'time_dependent_potassium_current', u'T')
+        T_const = pycml.mathml_apply.create_new(T, u'eq', [u'time_dependent_potassium_current,T',
+                                                           (unicode(T.get_value()), T.units)])
+        p.inputs.append(T_const)
+        self.assertRaises(protocol.ProtocolError, p.modify_model)
+        # Same should apply if we replace by an ODE
+        T_ode = pycml.mathml_diff.create_new(T, u'time_dependent_potassium_current,time',
+                                             u'time_dependent_potassium_current,T',
+                                             (u'1', u'dimensionless'))
+        p.inputs = [T_ode]
+        self.assertRaises(protocol.ProtocolError, p.modify_model)
 
-    def TestChangeMaths(self):
+    def TestChangeMathsToComputed(self):
+        """Test changing the mathematical definition of a variable to type Computed.
+        
+        Covers the cases of variables originally of type State, Computed, and Constant.
+        """
         doc = self.LoadModel('heart/src/odes/cellml/luo_rudy_1991.cellml')
         p = protocol.Protocol(doc.model, multi_stage=True)
         # Add maths setting Cai to a constant, thus replacing the ODE (state->computed)
@@ -75,12 +100,21 @@ class TestProtocol(unittest.TestCase):
         Cai_const = pycml.mathml_apply.create_new(ode, u'eq', [u'intracellular_calcium_concentration,Cai',
                                                                (u'0.0002', u'millimolar')])
         p.inputs.append(Cai_const)
+        self.failUnlessEqual(Cai.get_type(), pycml.VarTypes.State)
         # Change computed defintion for g_K (computed->computed)
         g_K = doc.model.get_variable_by_name(u'time_dependent_potassium_current', u'g_K')
         g_K_const = pycml.mathml_apply.create_new(g_K, u'eq', [u'time_dependent_potassium_current,g_K',
                                                                (u'0.282', u'milliS_per_cm2')])
         p.inputs.append(g_K_const)
         old_g_K = g_K._get_dependencies()[0]
+        self.failUnlessEqual(g_K.get_type(), pycml.VarTypes.Computed)
+        # Change PR_NaK from constant->computed (with same value)
+        PR_NaK = doc.model.get_variable_by_name(u'time_dependent_potassium_current', u'PR_NaK')
+        PR_NaK_const = pycml.mathml_apply.create_new(PR_NaK, u'eq',
+                                                     [u'time_dependent_potassium_current,PR_NaK',
+                                                      (PR_NaK.initial_value, PR_NaK.units)])
+        p.inputs.append(PR_NaK_const)
+        self.failUnlessEqual(PR_NaK.get_type(), pycml.VarTypes.Constant)
         # Apply protocol to model
         p.modify_model()
         # Check the changes
@@ -92,4 +126,62 @@ class TestProtocol(unittest.TestCase):
         self.assertEqual(old_g_K.xml_parent, None)
         self.assertEqual(g_K._get_dependencies()[0], g_K_const)
         self.assertEqual(g_K.get_type(), pycml.VarTypes.Computed)
-        # Note: still need to test constant->computed, state->state, computed->state, constant->state, mapped->x (error case), define new var
+        self.assertEqual(PR_NaK, doc.model.get_variable_by_name(u'time_dependent_potassium_current', u'PR_NaK'))
+        self.failIf(hasattr(PR_NaK, u'initial_value'))
+        self.assertEqual(PR_NaK._get_dependencies()[0], PR_NaK_const)
+        self.assertEqual(PR_NaK.get_type(), pycml.VarTypes.Computed)
+
+    def TestChangeMathsToState(self):
+        """Test changing the mathematical definition of a variable to type State.
+        
+        Covers the cases of variables originally of type State, Computed, and Constant.
+        """
+        doc = self.LoadModel('heart/src/odes/cellml/luo_rudy_1991.cellml')
+        p = protocol.Protocol(doc.model, multi_stage=True)
+        # state->state: membrane,V
+        V = doc.model.get_variable_by_name(u'membrane', u'V')
+        self.failUnlessEqual(V.get_type(), pycml.VarTypes.State)
+        time = doc.model.get_variable_by_name(u'membrane', u'time')
+        V_old = V._get_ode_dependency(time)
+        currents = map(lambda i: u'membrane,i_' + i, ['Na', 'si', 'K', 'b'])
+        rhs = pycml.mathml_apply.create_new(V, u'plus', currents)
+        V_new = pycml.mathml_diff.create_new(V, u'membrane,time', u'membrane,V', rhs)
+        V_new_str = str(V_new)
+        p.inputs.append(V_new)
+        # constant->state: membrane,T
+        T = doc.model.get_variable_by_name(u'membrane', u'T')
+        self.assertEqual(T.get_type(), pycml.VarTypes.Constant)
+        self.assertEqual(T._get_dependencies(), [])
+        T_new = pycml.mathml_diff.create_new(T, u'membrane,time', u'membrane,T',
+                                             (u'1', u'dimensionless')) # TODO: proper units
+        p.inputs.append(T_new)
+        # computed->state: membrane,FonRT
+        FonRT = doc.model.get_variable_by_name(u'membrane', u'FonRT')
+        self.assertEqual(FonRT.get_type(), pycml.VarTypes.Computed)
+        FonRT_old = FonRT._get_dependencies()[0]
+        FonRT_new = pycml.mathml_diff.create_new(FonRT, u'membrane,time', u'membrane,FonRT',
+                                                 (u'0', u'per_millivolt_millisecond'))
+        # TODO: FonRT now has no initial condition; this is a validation warning so OK...
+        p.inputs.append(FonRT_new)
+        # Apply protocol to model
+        p.modify_model()
+        # Check the changes
+        self.assertEqual(V, doc.model.get_variable_by_name(u'membrane', u'V'))
+        self.assertEqual(V_old.xml_parent, None)
+        self.assertNotEqual(V_old, V._get_ode_dependency(time))
+        self.assertEqual(V_new, V._get_ode_dependency(time))
+        self.assertEqual(V.get_type(), pycml.VarTypes.State)
+        self.assertNotEqual(str(V_new), V_new_str) # Variable refs get renamed
+        self.assertEqual(str(V_new), 'timeVi_Nai_sii_Ki_b')
+        self.assertEqual(T.get_type(), pycml.VarTypes.State)
+        self.assertEqual(T_new, T._get_ode_dependency(time))
+        self.assertEqual(FonRT.id, u'FonRT')
+        self.assertEqual(FonRT.get_type(), pycml.VarTypes.State)
+        self.assertEqual(FonRT_new, FonRT._get_ode_dependency(time))
+        self.assertEqual(FonRT._get_dependencies(), [])
+        self.assertEqual(FonRT_old.xml_parent, None)
+
+    def TestAddNewVariables(self):
+        """Test we can add new variables along with defining mathematics.
+        """
+        pass
