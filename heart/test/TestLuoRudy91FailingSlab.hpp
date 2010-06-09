1e6cd6dc42f11c69439bd1a31aeac8113b926faf
Chris Arthurs <chris.arthurs@comlab.ox.ac.uk>
2010-06-09 16:55:35 +0000

    Switch from a 3D to a 2D LR91 failing test for speed
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@9180 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestLuoRudy91FailingSlab.hpp
+++ heart/test/TestLuoRudy91FailingSlab.hpp
@@ -11,14 +11,14 @@
 
 
 
-class BenchmarkCellFactory : public AbstractCardiacCellFactory<3>
+class BenchmarkCellFactory : public AbstractCardiacCellFactory<2>
 {
 private:
     boost::shared_ptr<SimpleStimulus> mpStimulus;
 
 public:
     BenchmarkCellFactory()
-        : AbstractCardiacCellFactory<3>(),
+        : AbstractCardiacCellFactory<2>(),
           mpStimulus(new SimpleStimulus(-50000.0, 2))
     {
     }
@@ -27,7 +27,7 @@ public:
     {
         double x = this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[0];
         double y = this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[1];
-        double z = this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[2];
+        double z = 0.0;//this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[2];
 
         if ( (x<0.15+1e-6) && (y<0.15+1e-6) && (z<0.15+1e-6) ) 
         {
@@ -42,14 +42,15 @@ public:
     
 
 
-
-class TestBenchmark : public CxxTest::TestSuite
+// This test fails because the minimum membrane potential in the domain slowly drops below
+// the standard LR91 resting potential, until the m-gate goes out of range.
+class TestLuoRudy91FailingSlab : public CxxTest::TestSuite
 {
 
 public:
     void TestMonodomain( void ) throw(Exception)
     {
-        TetrahedralMesh<3,3> mesh;
+        TetrahedralMesh<2,2> mesh;
         double h=0.01; //cm
         unsigned num_elem_x = (unsigned)(2/h);
         unsigned num_elem_y = (unsigned)(0.7/h);
@@ -57,20 +58,25 @@ public:
         
         std::cout << num_elem_x << " " << num_elem_y << " " << num_elem_z << "\n";  
         
-        mesh.ConstructCuboid(num_elem_x, num_elem_y, num_elem_z);
-        mesh.Scale(h, h, h); 
-        c_vector<double,6> extremes = mesh.GetExtremes();
+        //mesh.ConstructCuboid(num_elem_x, num_elem_y, num_elem_z);
+        mesh.ConstructRectangularMesh(num_elem_x, num_elem_y);
+        mesh.Scale(h); 
+        /*c_vector<double,2> extremes = mesh.GetExtremes();
         std::cout << extremes(0) << " "
                   << extremes(1) << " "
                   << extremes(2) << " "
                   << extremes(3) << " "
                   << extremes(4) << " "
                   << extremes(5) << "\n";
-                  
+          */        
         HeartConfig::Instance()->SetOutputDirectory("TestBenchmark");
         HeartConfig::Instance()->SetOutputFilenamePrefix("results");          
  
         HeartConfig::Instance()->SetSimulationDuration(50); //ms
+	HeartConfig::Instance()->SetUseAbsoluteTolerance(1e-7);
+	std::cout << HeartConfig::Instance()->GetAbsoluteTolerance() << "\n\n";
+	HeartConfig::Instance()->SetUseRelativeTolerance(1e-7);
+        std::cout << HeartConfig::Instance()->GetRelativeTolerance() << "\n\n";
         
         HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.01, 0.01, 0.1);
         HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(1400); // 1400 1/cm
@@ -83,7 +89,7 @@ public:
  
         BenchmarkCellFactory cell_factory;
         
-        MonodomainProblem<3> problem( &cell_factory );
+        MonodomainProblem<2> problem( &cell_factory );
 
         problem.SetMesh(&mesh);
         
