025bff300c9bef7237c67cf9926f696a056b1254
Chris Arthurs <chris.arthurs@comlab.ox.ac.uk>
2010-06-11 14:03:02 +0000

    Benchmark now passes: needed smaller ODE timestep
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@9217 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestLuoRudy91FailingSlab.hpp
+++ heart/test/TestLuoRudy91FailingSlab.hpp
@@ -38,19 +38,18 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include "PetscSetupAndFinalize.hpp"
 #include "LuoRudyIModel1991OdeSystem.hpp"
-#include "BackwardEulerLuoRudyIModel1991.hpp"
 
 
 
-class BenchmarkCellFactory : public AbstractCardiacCellFactory<2>
+class BenchmarkCellFactory : public AbstractCardiacCellFactory<3>
 {
 private:
     boost::shared_ptr<SimpleStimulus> mpStimulus;
 
 public:
     BenchmarkCellFactory()
-        : AbstractCardiacCellFactory<2>(),
-          mpStimulus(new SimpleStimulus(-50000.0, 2))
+        : AbstractCardiacCellFactory<3>(),
+          mpStimulus(new SimpleStimulus(-70000.0, 2))
     {
     }
 
@@ -58,19 +57,15 @@ public:
     {
         double x = this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[0];
         double y = this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[1];
-        double z = 0.0;//this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[2];
+        double z = this->GetMesh()->GetNode(nodeIndex)->rGetLocation()[2];
 
         if ( (x<0.15+1e-6) && (y<0.15+1e-6) && (z<0.15+1e-6) ) 
         {
-            //return new LuoRudyIModel1991OdeSystem(mpSolver, mpStimulus);
-            return new BackwardEulerLuoRudyIModel1991(mpSolver, mpStimulus);
-            
-            
+            return new LuoRudyIModel1991OdeSystem(mpSolver, mpStimulus);
         }
         else
         {
-            //return new LuoRudyIModel1991OdeSystem(mpSolver, mpZeroStimulus);
-            return new BackwardEulerLuoRudyIModel1991(mpSolver, mpZeroStimulus);
+            return new LuoRudyIModel1991OdeSystem(mpSolver, mpZeroStimulus);
         }
     }
 };
@@ -79,13 +74,14 @@ public:
 
 // This test fails because the minimum membrane potential in the domain slowly drops below
 // the standard LR91 resting potential, until the m-gate goes out of range.
+// Update: this test now passes when we run with an ODE timestep of 0.005 instead of 0.01.
 class TestLuoRudy91FailingSlab : public CxxTest::TestSuite
 {
 
 public:
     void TestMonodomain( void ) throw(Exception)
     {
-        TetrahedralMesh<2,2> mesh;
+        TetrahedralMesh<3,3> mesh;
         double h=0.01; //cm
         unsigned num_elem_x = (unsigned)(2/h);
         unsigned num_elem_y = (unsigned)(0.7/h);
@@ -93,9 +89,9 @@ public:
         
         std::cout << num_elem_x << " " << num_elem_y << " " << num_elem_z << "\n";  
         
-        //mesh.ConstructCuboid(num_elem_x, num_elem_y, num_elem_z);
-        mesh.ConstructRectangularMesh(num_elem_x, num_elem_y, false);
-        mesh.Scale(h); 
+        mesh.ConstructCuboid(num_elem_x, num_elem_y, num_elem_z);
+//        mesh.ConstructRectangularMesh(num_elem_x, num_elem_y);
+        mesh.Scale(h,h,h);
         /*c_vector<double,2> extremes = mesh.GetExtremes();
         std::cout << extremes(0) << " "
                   << extremes(1) << " "
@@ -104,17 +100,16 @@ public:
                   << extremes(4) << " "
                   << extremes(5) << "\n";
           */        
-        HeartConfig::Instance()->SetOutputDirectory("TestBenchmark");
+        HeartConfig::Instance()->SetOutputDirectory("TestLuoRudy91FailingSlab");
         HeartConfig::Instance()->SetOutputFilenamePrefix("results");          
-        HeartConfig::Instance()->SetVisualizeWithVtk(true);
-        
+ 
         HeartConfig::Instance()->SetSimulationDuration(50); //ms
-        HeartConfig::Instance()->SetUseAbsoluteTolerance(1e-7);
-        std::cout << HeartConfig::Instance()->GetAbsoluteTolerance() << "\n\n";
-        HeartConfig::Instance()->SetUseRelativeTolerance(1e-7);
-        std::cout << HeartConfig::Instance()->GetRelativeTolerance() << "\n\n";
+//        HeartConfig::Instance()->SetUseAbsoluteTolerance(1e-7);
+//        std::cout << HeartConfig::Instance()->GetAbsoluteTolerance() << "\n\n";
+//        HeartConfig::Instance()->SetUseRelativeTolerance(1e-7);
+//        std::cout << HeartConfig::Instance()->GetRelativeTolerance() << "\n\n";
         
-        HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.01, 0.01, 0.1);
+        HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.005, 0.01, 0.1);
         HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(1400); // 1400 1/cm
         HeartConfig::Instance()->SetCapacitance(1); // 1uF/cm^2
  
@@ -125,7 +120,7 @@ public:
  
         BenchmarkCellFactory cell_factory;
         
-        MonodomainProblem<2> problem( &cell_factory );
+        MonodomainProblem<3> problem( &cell_factory );
 
         problem.SetMesh(&mesh);
         
