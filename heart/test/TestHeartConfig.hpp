89c0af93f5d6ddb4857c448fee09597b027d0a03
Alberto Corrias <alberto@nus.edu.sg>
2010-08-12 05:52:46 +0000

    #1530 Failing test (commented out)
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@9932 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestHeartConfig.hpp
+++ heart/test/TestHeartConfig.hpp
@@ -972,6 +972,9 @@ public:
          //Throws when printing step is not a multiple of pde time step
         TS_ASSERT_THROWS_THIS(HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.1,0.2, 0.3), "Printing time-step should be a multiple of PDE time step");
 
+        //Shouldn't throw because 10 is a multiple of 0.1
+        //TS_ASSERT_THROWS_NOTHING(HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.1,0.1, 10.0));
+
         // Throws because ode time step is bigger than pde time step
         TS_ASSERT_THROWS_THIS(HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.1,0.2, 0.3), "Printing time-step should be a multiple of PDE time step");
 
