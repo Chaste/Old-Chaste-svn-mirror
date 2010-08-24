e1eab597225cfe78933ad4c553bdad878e82d281
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2010-08-24 15:57:46 +0000

    #1545 Mesh a cube
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@10099 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- mesh/test/TestMutableMeshRemesh.hpp
+++ mesh/test/TestMutableMeshRemesh.hpp
@@ -575,164 +575,58 @@ public:
         free(out.edgemarkerlist);
     }
 
-    void todoTestRawTetgenLibraryCall()
+    void TestRawTetgenLibraryCall()
     {
-        ///\todo #1545
-    
         tetgen::tetgenio in, out;
-        tetgen::tetgenio::facet *f;
-        tetgen::tetgenio::polygon *p;
-        int i;
+        //int i;
         
-        // All indices start from 1.
-        in.firstnumber = 1;
         
+        in.numberofpointattributes = 0;
         in.numberofpoints = 8;
-        in.pointlist = new REAL[in.numberofpoints * 3];
-        in.pointlist[0]  = 0;  // node 1.
+        in.pointlist = new REAL[in.numberofpoints * 3];//tetgenio's destructor will automatically free this.
+        in.pointlist[0]  = 0;
         in.pointlist[1]  = 0;
         in.pointlist[2]  = 0;
-        in.pointlist[3]  = 2;  // node 2.
+        
+        in.pointlist[3]  = 1;
         in.pointlist[4]  = 0;
         in.pointlist[5]  = 0;
-        in.pointlist[6]  = 2;  // node 3.
-        in.pointlist[7]  = 2;
+        
+        in.pointlist[6]  = 0;
+        in.pointlist[7]  = 1;
         in.pointlist[8]  = 0;
-        in.pointlist[9]  = 0;  // node 4.
-        in.pointlist[10] = 2;
-        in.pointlist[11] = 0;
-        // Set node 5, 6, 7, 8.
-        for (i = 4; i < 8; i++) {
-          in.pointlist[i * 3]     = in.pointlist[(i - 4) * 3];
-          in.pointlist[i * 3 + 1] = in.pointlist[(i - 4) * 3 + 1];
-          in.pointlist[i * 3 + 2] = 12;
-        }
         
-        in.numberoffacets = 6;
-        in.facetlist = new tetgen::tetgenio::facet[in.numberoffacets];
-        in.facetmarkerlist = new int[in.numberoffacets];
+        in.pointlist[9]  = 0;
+        in.pointlist[10]  = 0;
+        in.pointlist[11]  = 1;
         
-        // Facet 1. The leftmost facet.
-        f = &in.facetlist[0];
-        f->numberofpolygons = 1;
-        f->polygonlist = new tetgen::tetgenio::polygon[f->numberofpolygons];
-        f->numberofholes = 0;
-        f->holelist = NULL;
-        p = &f->polygonlist[0];
-        p->numberofvertices = 4;
-        p->vertexlist = new int[p->numberofvertices];
-        p->vertexlist[0] = 1;
-        p->vertexlist[1] = 2;
-        p->vertexlist[2] = 3;
-        p->vertexlist[3] = 4;
+        in.pointlist[12]  = 1;
+        in.pointlist[13]  = 1;
+        in.pointlist[14]  = 0;
         
-        // Facet 2. The rightmost facet.
-        f = &in.facetlist[1];
-        f->numberofpolygons = 1;
-        f->polygonlist = new tetgen::tetgenio::polygon[f->numberofpolygons];
-        f->numberofholes = 0;
-        f->holelist = NULL;
-        p = &f->polygonlist[0];
-        p->numberofvertices = 4;
-        p->vertexlist = new int[p->numberofvertices];
-        p->vertexlist[0] = 5;
-        p->vertexlist[1] = 6;
-        p->vertexlist[2] = 7;
-        p->vertexlist[3] = 8;
+        in.pointlist[15]  = 1;
+        in.pointlist[16]  = 0;
+        in.pointlist[17]  = 1;
         
-        // Facet 3. The bottom facet.
-        f = &in.facetlist[2];
-        f->numberofpolygons = 1;
-        f->polygonlist = new tetgen::tetgenio::polygon[f->numberofpolygons];
-        f->numberofholes = 0;
-        f->holelist = NULL;
-        p = &f->polygonlist[0];
-        p->numberofvertices = 4;
-        p->vertexlist = new int[p->numberofvertices];
-        p->vertexlist[0] = 1;
-        p->vertexlist[1] = 5;
-        p->vertexlist[2] = 6;
-        p->vertexlist[3] = 2;
+        in.pointlist[18]  = 0;
+        in.pointlist[19]  = 1;
+        in.pointlist[20]  = 1;
         
-        // Facet 4. The back facet.
-        f = &in.facetlist[3];
-        f->numberofpolygons = 1;
-        f->polygonlist = new tetgen::tetgenio::polygon[f->numberofpolygons];
-        f->numberofholes = 0;
-        f->holelist = NULL;
-        p = &f->polygonlist[0];
-        p->numberofvertices = 4;
-        p->vertexlist = new int[p->numberofvertices];
-        p->vertexlist[0] = 2;
-        p->vertexlist[1] = 6;
-        p->vertexlist[2] = 7;
-        p->vertexlist[3] = 3;
+        in.pointlist[21]  = 1;
+        in.pointlist[22]  = 1;
+        in.pointlist[23]  = 1;
+        tetgen::tetrahedralize((char*)"Q", &in, &out);
         
-        // Facet 5. The top facet.
-        f = &in.facetlist[4];
-        f->numberofpolygons = 1;
-        f->polygonlist = new tetgen::tetgenio::polygon[f->numberofpolygons];
-        f->numberofholes = 0;
-        f->holelist = NULL;
-        p = &f->polygonlist[0];
-        p->numberofvertices = 4;
-        p->vertexlist = new int[p->numberofvertices];
-        p->vertexlist[0] = 3;
-        p->vertexlist[1] = 7;
-        p->vertexlist[2] = 8;
-        p->vertexlist[3] = 4;
-        
-        // Facet 6. The front facet.
-        f = &in.facetlist[5];
-        f->numberofpolygons = 1;
-        f->polygonlist = new tetgen::tetgenio::polygon[f->numberofpolygons];
-        f->numberofholes = 0;
-        f->holelist = NULL;
-        p = &f->polygonlist[0];
-        p->numberofvertices = 4;
-        p->vertexlist = new int[p->numberofvertices];
-        p->vertexlist[0] = 4;
-        p->vertexlist[1] = 8;
-        p->vertexlist[2] = 5;
-        p->vertexlist[3] = 1;
-        
-        // Set 'in.facetmarkerlist'
-        
-        in.facetmarkerlist[0] = -1;
-        in.facetmarkerlist[1] = -2;
-        in.facetmarkerlist[2] = 0;
-        in.facetmarkerlist[3] = 0;
-        in.facetmarkerlist[4] = 0;
-        in.facetmarkerlist[5] = 0;
-        
-        
-        // Tetrahedralize the PLC. Switches are chosen to read a PLC (p),
-        //   do quality mesh generation (q) with a specified quality bound
-        //   (1.414), and apply a maximum volume constraint (a0.1).
-        
-        tetgen::tetrahedralize((char*)"pq1.414a0.1", &in, &out);
-    
-
-
-
-        TS_ASSERT_EQUALS(out.numberofpoints,         324);
+        TS_ASSERT_EQUALS(out.numberofpoints,         8); //As in original
         TS_ASSERT_EQUALS(out.numberofpointattributes,    0);
-     // TS_ASSERT_EQUALS(out.numberoftriangles,        4);
-        TS_ASSERT_EQUALS(out.numberofcorners,        4);
-     // TS_ASSERT_EQUALS(out.numberoftriangleattributes, 0);
+        TS_ASSERT_EQUALS(out.numberofcorners,        4); //Vertices per tet
         TS_ASSERT_EQUALS(out.numberofedges,          0);
-
-        // Free all allocated arrays, including those allocated by Tetgen
-//        free(in.pointlist);
-//
-//        free(out.pointlist);
-//        free(out.pointattributelist);
-//        free(out.pointmarkerlist);
-//        //free(out.trianglelist);
-//        //free(out.triangleattributelist);
-//        free(out.edgelist);
-//        free(out.edgemarkerlist);
+        TS_ASSERT_EQUALS(out.numberoftetrahedra,          6); //Elements
+        TS_ASSERT_EQUALS(out.numberofedges,          0);
+        TS_ASSERT_EQUALS(out.numberoftrifaces,          12); //2 triangles on each die face
+        
     }
+
     void TestRemeshWithLibraryMethodSimple() throw (Exception)
     {
         // Same data as previous 2d test
