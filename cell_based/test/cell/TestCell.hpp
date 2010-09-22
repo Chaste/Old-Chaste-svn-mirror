db77c817320fc84647bdc79b8aa513db72648dfc
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2010-09-22 10:52:36 +0000

    fix #1569 (bizarre intel error) by splitting files
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@10433 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cell_based/test/cell/TestCell.hpp
+++ cell_based/test/cell/TestCell.hpp
@@ -37,14 +37,17 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 #include <iostream>
 
 #include "AbstractCellBasedTestSuite.hpp"
-#include "FixedDurationGenerationBasedCellCycleModel.hpp"
-#include "Cell.hpp"
 #include "WildTypeCellMutationState.hpp"
-#include "StochasticDurationGenerationBasedCellCycleModel.hpp"
-#include "StochasticWntCellCycleModel.hpp"
-#include "TysonNovakCellCycleModel.hpp"
-#include "CellLabel.hpp"
+#include "FixedDurationGenerationBasedCellCycleModel.hpp"
 #include "CellPropertyRegistry.hpp"
+#include "CellLabel.hpp"
+#include "ApcTwoHitCellMutationState.hpp"
+#include "ApcOneHitCellMutationState.hpp"
+#include "StochasticWntCellCycleModel.hpp"
+#include "StochasticDurationGenerationBasedCellCycleModel.hpp"
+#include "TysonNovakCellCycleModel.hpp"
+//#include "Cell.hpp"
+
 
 class TestCell: public AbstractCellBasedTestSuite
 {
@@ -1212,95 +1215,6 @@ public:
         TS_ASSERT_EQUALS(dead_cells[num_time_steps-1], 8u);
     }
 
-    void TestArchiveCell() throw(Exception)
-    {
-        OutputFileHandler handler("archive", false);
-        std::string archive_filename = handler.GetOutputDirectoryFullPath() + "cell->arch";
-
-        // Archive a cell
-        {
-            SimulationTime* p_simulation_time = SimulationTime::Instance();
-            p_simulation_time->SetEndTimeAndNumberOfTimeSteps(2.0, 4);
-
-            // Create mutation state
-            boost::shared_ptr<AbstractCellProperty> p_healthy_state(CellPropertyRegistry::Instance()->Get<WildTypeCellMutationState>());
-
-            // Create cell cycle model
-            FixedDurationGenerationBasedCellCycleModel* p_cell_model = new FixedDurationGenerationBasedCellCycleModel();
-            p_cell_model->SetCellProliferativeType(STEM);
-
-            // Create cell property collection
-	        CellPropertyCollection collection;
-	        boost::shared_ptr<AbstractCellProperty> p_label(new CellLabel);
-	        collection.AddProperty(p_label);
-
-            // Create cell
-            CellPtr p_cell(new Cell(p_healthy_state, p_cell_model, false, collection));
-            p_cell->InitialiseCellCycleModel();
-            p_simulation_time->IncrementTimeOneStep();
-
-            TS_ASSERT_EQUALS(p_cell->GetAge(), 0.5);
-
-            // Create an output archive
-            std::ofstream ofs(archive_filename.c_str());
-            boost::archive::text_oarchive output_arch(ofs);
-
-            CellPtr const p_const_cell = p_cell;
-
-            // Write the cell to the archive
-            output_arch << static_cast<const SimulationTime&> (*p_simulation_time);
-            output_arch << p_const_cell;
-
-            // Tidy up
-            SimulationTime::Destroy();
-        }
-
-        // Restore CellPtr
-        {
-            // Need to set up time to initialize a cell
-            SimulationTime* p_simulation_time = SimulationTime::Instance();
-            p_simulation_time->SetStartTime(1.0);
-            p_simulation_time->SetEndTimeAndNumberOfTimeSteps(2.0, 1); // will be restored
-
-            // Initialize a cell
-
-            CellPtr p_cell;
-
-            // Restore the cell
-            std::ifstream ifs(archive_filename.c_str(), std::ios::binary);
-            boost::archive::text_iarchive input_arch(ifs);
-
-            input_arch >> *p_simulation_time;
-            input_arch >> p_cell;
-
-            // Check the simulation time has been restored (through the cell)
-            TS_ASSERT_EQUALS(p_simulation_time->GetTime(), 0.5);
-            TS_ASSERT_EQUALS(p_simulation_time->GetTimeStep(), 0.5);
-
-            TS_ASSERT_EQUALS(p_cell->GetAge(), 0.5);
-            TS_ASSERT_EQUALS(static_cast<FixedDurationGenerationBasedCellCycleModel*>(p_cell->GetCellCycleModel())->GetGeneration(), 0u);
-            TS_ASSERT_EQUALS(p_cell->GetCellCycleModel()->GetCellProliferativeType(), STEM);
-
-            AbstractCellCycleModel* p_model = p_cell->GetCellCycleModel();
-            TS_ASSERT_EQUALS(p_model->GetCell(), p_cell);
-
-            CellPropertyCollection& collection = p_cell->rGetCellPropertyCollection();
-            TS_ASSERT_EQUALS(collection.GetSize(), 2u);
-	        TS_ASSERT_EQUALS(collection.HasProperty<WildTypeCellMutationState>(), true);
-	        TS_ASSERT_EQUALS(collection.HasProperty<ApcOneHitCellMutationState>(), false);
-	        TS_ASSERT_EQUALS(collection.HasProperty<ApcTwoHitCellMutationState>(), false);
-            TS_ASSERT_EQUALS(collection.HasProperty<CellLabel>(), true);
-	        TS_ASSERT_EQUALS(collection.HasPropertyType<AbstractCellProperty>(), true);
-	        TS_ASSERT_EQUALS(collection.HasPropertyType<AbstractCellMutationState>(), true);
-
-	        for (CellPropertyCollection::Iterator it = collection.Begin(); it != collection.End(); ++it)
-	        {
-	            TS_ASSERT_EQUALS(collection.HasProperty(*it), true);
-	            TS_ASSERT((*it)->IsType<WildTypeCellMutationState>() || (*it)->IsType<CellLabel>());
-	        }
-        }
-    }
-
     /*
      * We are checking that the CellPtrs work with the Wnt
      * cell cycle models here. This just tests the set-up and checks that
@@ -1357,7 +1271,7 @@ public:
         TS_ASSERT_EQUALS(p_wnt_cell3->ReadyToDivide(), false);
         TS_ASSERT_EQUALS(p_wnt_cell4->ReadyToDivide(), false);
 
-        // Tidy up
+        //Tidy up
         WntConcentration<2>::Destroy();
     }
 
@@ -1555,7 +1469,6 @@ public:
             TS_ASSERT_EQUALS(cells[i]->GetCellCycleModel()->GetCellProliferativeType(), DIFFERENTIATED);
         }
     }
-
 };
 
 #endif /*TESTCELL_HPP_*/
