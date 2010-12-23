3a68f5a5092ad426f4205e0fe535cd489f2d43e4
Miguel Bernabeu <miguel.bernabeu@ucl.ac.uk>
2010-12-23 14:58:28 +0000

    #1200 - removing #include not used
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@11189 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- mesh/src/common/AbstractTetrahedralMesh.hpp
+++ mesh/src/common/AbstractTetrahedralMesh.hpp
@@ -47,7 +47,6 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 #include "TrianglesMeshReader.hpp"
 #include "TrianglesMeshWriter.hpp"
 #include "ArchiveLocationInfo.hpp"
-//#include "HeartConfig.hpp"
 #include "GenericMeshReader.hpp"
 
 /// Forward declaration which is going to be used for friendship
