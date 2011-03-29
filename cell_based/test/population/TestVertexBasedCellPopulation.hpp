293b73dd10c0390dab8f3582c5125d3b3907fb7d
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2011-03-29 12:13:58 +0000

    #1731 Small oops in r12180
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@12181 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- cell_based/test/population/TestVertexBasedCellPopulation.hpp
+++ cell_based/test/population/TestVertexBasedCellPopulation.hpp
@@ -855,7 +855,7 @@ public:
         ArchiveLocationInfo::SetMeshFilename("vertex_mesh_2d");
 
         // Create mesh
-        VertexMeshReader<2,2> mesh_reader("mesh/test/data/TestVertexMeshReader/vertex_mesh_2d");
+        VertexMeshReader<2,2> mesh_reader("mesh/test/data/TestVertexMeshWriter/vertex_mesh_2d");
         MutableVertexMesh<2,2> mesh;
         mesh.ConstructFromMeshReader(mesh_reader);
 
