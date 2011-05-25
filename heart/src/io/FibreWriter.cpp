98e87397c1a67e25541755a1659d50c9c69d08af
Nejib Zemzemi <nejib.zemzemi@gmail.com>
2011-05-25 15:52:24 +0000

    #1768 - Added initial `FibreWriter` class with method to write ascii .axi files.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@12769 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ heart/src/io/FibreWriter.cpp
@@ -0,0 +1,75 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "FibreWriter.hpp"
+#include "Version.hpp"
+
+template<unsigned DIM>
+FibreWriter<DIM>::FibreWriter(const std::string& rDirectory,
+                              const std::string& rBaseName,
+                              const bool clearOutputDir)
+    : mBaseName(rBaseName)               
+{
+    mpOutputFileHandler= new OutputFileHandler(rDirectory, clearOutputDir); 
+}
+
+template<unsigned DIM>
+FibreWriter<DIM>::~FibreWriter()
+{
+    delete mpOutputFileHandler;
+}
+
+template<unsigned DIM>
+void FibreWriter<DIM>::WriteAllAxi(std::vector< c_vector<double, DIM> >& direction)
+{
+    // Write axi file
+    std::string axi_file_name = this->mBaseName + ".axi";
+    out_stream p_axi_file = this->mpOutputFileHandler->OpenOutputFile(axi_file_name);
+    
+    *p_axi_file << direction.size() << std::endl;
+    for (unsigned i=0; i<direction.size();i++ )
+    {
+        for(unsigned j=0; j<DIM; j++)
+        {
+            *p_axi_file << direction[i](j) << "\t";
+        }
+        *p_axi_file <<"\n";
+    }
+    *p_axi_file << "#\n# " <<  ChasteBuildInfo::GetProvenanceString();
+    p_axi_file->close();
+}
+
+
+template class FibreWriter<1>;
+template class FibreWriter<2>;
+template class FibreWriter<3>;
+
+
+
+
+
---
+++ heart/src/io/FibreWriter.hpp
@@ -0,0 +1,76 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef FIBREWRITER_HPP_
+#define FIBREWRITER_HPP_
+
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "OutputFileHandler.hpp"
+#include "UblasIncludes.hpp"
+
+
+/**
+ * A class for writing .axi files (files which define the fibre direction
+ * for each element) and .ortho files (files which define the fibre, sheet
+ * and normal directions for each element. 
+ */
+template<unsigned DIM>
+class FibreWriter
+{
+private:
+    OutputFileHandler* mpOutputFileHandler; /**< Output file handler */
+
+    std::string mBaseName; /**< Base name for the input files */
+
+public:
+    /**
+     * Create a new FibreWriter.
+     * 
+     * @param rDirectory  the directory in which to write the fibre to file
+     * @param rBaseName  the base name of the files in which to write the fibre data
+     * @param clearOutputDir  whether to clean the directory (defaults to true)
+     */
+    FibreWriter(const std::string& rDirectory,
+                const std::string& rBaseName,
+                const bool clearOutputDir=true);
+    
+    /**
+     *  Destructor closes file.
+     */
+    ~FibreWriter();
+    
+     /**
+      * Writes all axisymmetric vectors to the file.
+      */
+     void WriteAllAxi(std::vector< c_vector<double, DIM> >& direction);
+};
+
+#endif /*FIBREWRITER_HPP_*/
