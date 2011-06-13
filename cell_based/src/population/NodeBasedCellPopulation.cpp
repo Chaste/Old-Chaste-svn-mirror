Chaste/Old-Chaste-svn-mirror

commit 7c6f039aad08c319cc65a74aa20c8ac05e124475

git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@12854 b5f69653-2bf5-0310-8201-b0dafb8e1405

cell_based/src/population/NodeBasedCellPopulation.cpp
@@ -336,6 +336,56 @@ double NodeBasedCellPopulation<DIM>::GetWidth(const unsigned& rDimension)
 }
 
 template<unsigned DIM>
+void NodeBasedCellPopulation<DIM>::WriteCellVolumeResultsToFile()
+{
+    assert(DIM==2 || DIM==3);
+
+    // Write time to file
+    *(this->mpCellVolumesFile) << SimulationTime::Instance()->GetTime() << " ";
+
+    // Loop over cells
+    for (typename AbstractCellPopulation<DIM>::Iterator cell_iter=this->Begin(); 
+         cell_iter!=this->End(); ++cell_iter)
+    {
+        // Get the index of the corresponding node in mrMesh
+        unsigned node_index = this->GetLocationIndexUsingCell(*cell_iter);
+
+        // Get cell radius
+        double cell_radius = mrMesh.GetCellRadius(node_index);
+
+        // Get cell volume from radius
+        double cell_volume;
+        
+        if (DIM == 2)
+        {
+            cell_volume = M_PI*cell_radius*cell_radius;
+        }
+        else if (DIM == 3)
+        {
+            cell_volume = (4.0/3.0)*M_PI*cell_radius*cell_radius*cell_radius;
+        }
+           
+        // Write node index to file
+        *(this->mpCellVolumesFile) << node_index << " ";
+
+        // Write cell ID to file
+        unsigned cell_index = cell_iter->GetCellId();
+        *(this->mpCellVolumesFile) << cell_index << " ";
+
+        // Write node location to file
+        c_vector<double, DIM> node_location = this->GetNode(node_index)->rGetLocation();
+        for (unsigned i=0; i<DIM; i++)
+        {
+            *(this->mpCellVolumesFile) << node_location[i] << " ";
+        }
+
+        // Write cell volume (in 3D) or area (in 2D) to file
+        *(this->mpCellVolumesFile) << cell_volume << " ";
+    }
+    *(this->mpCellVolumesFile) << "\n";
+}
+
+template<unsigned DIM>
 void NodeBasedCellPopulation<DIM>::WriteVtkResultsToFile()
 {
 #ifdef CHASTE_VTK
@@ -349,8 +399,9 @@ void NodeBasedCellPopulation<DIM>::WriteVtkResultsToFile()
     std::vector<double> cell_mutation_states(num_nodes);
     std::vector<double> cell_ages(num_nodes);
     std::vector<double> cell_cycle_phases(num_nodes);
+    std::vector<double> cell_radii(num_nodes);
     std::vector<std::vector<double> > cellwise_data;
-
+    
     if (CellwiseData<DIM>::Instance()->IsSetUp())
     {
         CellwiseData<DIM>* p_data = CellwiseData<DIM>::Instance();
@@ -394,7 +445,12 @@ void NodeBasedCellPopulation<DIM>::WriteVtkResultsToFile()
         {
             double cycle_phase = cell_iter->GetCellCycleModel()->GetCurrentCellCyclePhase();
             cell_cycle_phases[node_index] = cycle_phase;
-        }
+        }        
+        if (this->mOutputCellVolumes)
+        {
+            double cell_radius = mrMesh.GetCellRadius(node_index);    
+            cell_radii[node_index] = cell_radius;
+        }         
         if (CellwiseData<DIM>::Instance()->IsSetUp())
         {
             CellwiseData<DIM>* p_data = CellwiseData<DIM>::Instance();
@@ -426,6 +482,10 @@ void NodeBasedCellPopulation<DIM>::WriteVtkResultsToFile()
     {
         mesh_writer.AddPointData("Cycle phases", cell_cycle_phases);
     }
+    if (this->mOutputCellVolumes)
+    {
+        mesh_writer.AddPointData("Cell radii", cell_radii);
+    }
     if (CellwiseData<DIM>::Instance()->IsSetUp())
     {
         for (unsigned var=0; var<cellwise_data.size(); var++)
@@ -445,15 +505,6 @@ void NodeBasedCellPopulation<DIM>::WriteVtkResultsToFile()
     *(this->mpVtkMetaFile) << SimulationTime::Instance()->GetTimeStepsElapsed();
     *(this->mpVtkMetaFile) << ".vtu\"/>\n";
 #endif //CHASTE_VTK
-}
-
-template<unsigned DIM>
-void NodeBasedCellPopulation<DIM>::SetOutputCellVolumes(bool outputCellVolumes)
-{
-    if (outputCellVolumes)
-    {
-        EXCEPTION("This method currently not implemented for a NodeBasedCellPopulation");
-    }
 }
 
 /////////////////////////////////////////////////////////////////////////////

cell_based/src/population/AbstractCellPopulation.cpp
@@ -303,6 +303,10 @@ void AbstractCellPopulation<DIM>::CreateOutputFiles(const std::string& rDirector
     {
         mpCellIdFile = output_file_handler.OpenOutputFile("loggedcell.dat");
     }
+    if (this->mOutputCellVolumes)
+    {
+        mpCellVolumesFile = output_file_handler.OpenOutputFile("cellareas.dat");
+    }
 
     mDirPath = rDirectory;
 #ifdef CHASTE_VTK
@@ -347,6 +351,10 @@ void AbstractCellPopulation<DIM>::CloseOutputFiles()
     if (mOutputCellIdData)
     {
         mpCellIdFile->close();
+    }
+    if (this->mOutputCellVolumes)
+    {
+        mpCellVolumesFile->close();
     }
 #ifdef CHASTE_VTK
     *mpVtkMetaFile << "    </Collection>\n";
@@ -601,6 +609,10 @@ void AbstractCellPopulation<DIM>::WriteResultsToFiles()
     if (mOutputCellAges)
     {
         *mpCellAgesFile << time << "\t";
+    }
+    if (this->mOutputCellVolumes)
+    {
+        WriteCellVolumeResultsToFile();
     }
 
     GenerateCellResultsAndWriteToFiles();

cell_based/src/population/AbstractCellPopulation.hpp
@@ -138,6 +138,9 @@ protected:
 
     /** Results file for logged cell data. */
     out_stream mpCellIdFile;
+        
+    /** Results file for cell volume (in 3D) or area (in 2D) data. */
+    out_stream mpCellVolumesFile;
 
     /** Results file for boundary nodes. */
     out_stream mpVizBoundaryNodesFile;
@@ -514,6 +517,13 @@ public:
                              std::vector<unsigned>& rCellCyclePhaseCounter);
 
     /**
+     * Write the current volume of each cell to file.      
+     * As this method is pure virtual, it must be overridden
+     * in subclasses.
+     */
+    virtual void WriteCellVolumeResultsToFile()=0;
+
+    /**
      * Write the current state of each cell to output files.
 
      * @param rCellProliferativeTypeCounter cell type counter
@@ -661,7 +671,7 @@ public:
      *
      * @param outputCellVolumes the new value of mOutputCellVolumes
      */
-    virtual void SetOutputCellVolumes(bool outputCellVolumes);
+    void SetOutputCellVolumes(bool outputCellVolumes);
 
     /**
      * Iterator class allows one to iterate over cells in the cell population.

cell_based/src/population/MeshBasedCellPopulation.cpp
@@ -387,10 +387,6 @@ void MeshBasedCellPopulation<DIM>::CreateOutputFiles(const std::string& rDirecto
     {
         mpCellPopulationVolumesFile = output_file_handler.OpenOutputFile("cellpopulationareas.dat");
     }
-    if (this->mOutputCellVolumes)
-    {
-        mpCellVolumesFile = output_file_handler.OpenOutputFile("cellareas.dat");
-    }
 }
 
 template<unsigned DIM>
@@ -407,10 +403,6 @@ void MeshBasedCellPopulation<DIM>::CloseOutputFiles()
     if (mOutputCellPopulationVolumes)
     {
         mpCellPopulationVolumesFile->close();
-    }
-    if (this->mOutputCellVolumes)
-    {
-        mpCellVolumesFile->close();
     }
 }
 
@@ -466,10 +458,6 @@ void MeshBasedCellPopulation<DIM>::WriteResultsToFiles()
         if (mOutputCellPopulationVolumes)
         {
             WriteCellPopulationVolumeResultsToFile();
-        }
-        if (this->mOutputCellVolumes)
-        {
-            WriteCellVolumeResultsToFile();
         }
     }
 }
@@ -785,48 +773,51 @@ void MeshBasedCellPopulation<DIM>::WriteCellPopulationVolumeResultsToFile()
 template<unsigned DIM>
 void MeshBasedCellPopulation<DIM>::WriteCellVolumeResultsToFile()
 {
-    assert(DIM==2 || DIM==3);
-
-     // Write time to file
-    *mpCellVolumesFile << SimulationTime::Instance()->GetTime() << " ";
-
-    // Loop over elements of mpVoronoiTessellation
-    for (typename VertexMesh<DIM,DIM>::VertexElementIterator elem_iter = mpVoronoiTessellation->GetElementIteratorBegin();
-         elem_iter != mpVoronoiTessellation->GetElementIteratorEnd();
-         ++elem_iter)
-    {
-        // Get index of this element in mpVoronoiTessellation
-        unsigned elem_index = elem_iter->GetIndex();
-
-        // Get the index of the corresponding node in mrMesh
-        unsigned node_index = mpVoronoiTessellation->GetDelaunayNodeIndexCorrespondingToVoronoiElementIndex(elem_index);
-
-        // Discount ghost nodes
-        if (!this->IsGhostNode(node_index))
+    if (mpVoronoiTessellation != NULL)
+    {    
+        assert(DIM==2 || DIM==3);
+    
+        // Write time to file
+        *(this->mpCellVolumesFile) << SimulationTime::Instance()->GetTime() << " ";
+    
+        // Loop over elements of mpVoronoiTessellation
+        for (typename VertexMesh<DIM,DIM>::VertexElementIterator elem_iter = mpVoronoiTessellation->GetElementIteratorBegin();
+             elem_iter != mpVoronoiTessellation->GetElementIteratorEnd();
+             ++elem_iter)
         {
-            // Write node index to file
-            *mpCellVolumesFile << node_index << " ";
-
-            // Get the cell corresponding to this node
-            CellPtr p_cell =  this->mLocationCellMap[node_index];
-
-            // Write cell ID to file
-            unsigned cell_index = p_cell->GetCellId();
-            *mpCellVolumesFile << cell_index << " ";
-
-            // Write node location to file
-            c_vector<double, DIM> node_location = this->GetNode(node_index)->rGetLocation();
-            for (unsigned i=0; i<DIM; i++)
+            // Get index of this element in mpVoronoiTessellation
+            unsigned elem_index = elem_iter->GetIndex();
+    
+            // Get the index of the corresponding node in mrMesh
+            unsigned node_index = mpVoronoiTessellation->GetDelaunayNodeIndexCorrespondingToVoronoiElementIndex(elem_index);
+    
+            // Discount ghost nodes
+            if (!this->IsGhostNode(node_index))
             {
-                *mpCellVolumesFile << node_location[i] << " ";
+                // Write node index to file
+                *(this->mpCellVolumesFile) << node_index << " ";
+    
+                // Get the cell corresponding to this node
+                CellPtr p_cell =  this->mLocationCellMap[node_index];
+    
+                // Write cell ID to file
+                unsigned cell_index = p_cell->GetCellId();
+                *(this->mpCellVolumesFile) << cell_index << " ";
+    
+                // Write node location to file
+                c_vector<double, DIM> node_location = this->GetNode(node_index)->rGetLocation();
+                for (unsigned i=0; i<DIM; i++)
+                {
+                    *(this->mpCellVolumesFile) << node_location[i] << " ";
+                }
+    
+                // Write cell volume (in 3D) or area (in 2D) to file
+                double cell_volume = mpVoronoiTessellation->GetVolumeOfElement(elem_index);
+                *(this->mpCellVolumesFile) << cell_volume << " ";
             }
-
-            // Write cell volume (in 3D) or area (in 2D) to file
-            double cell_volume = mpVoronoiTessellation->GetVolumeOfElement(elem_index);
-            *mpCellVolumesFile << cell_volume << " ";
         }
+        *(this->mpCellVolumesFile) << "\n";
     }
-    *mpCellVolumesFile << "\n";
 }
 
 template<unsigned DIM>

cell_based/src/population/MeshBasedCellPopulation.hpp
@@ -133,9 +133,6 @@ protected:
     /** Results file for cell population volume (in 3D) or area (in 2D) data. */
     out_stream mpCellPopulationVolumesFile;
 
-    /** Results file for cell volume (in 3D) or area (in 2D) data. */
-    out_stream mpCellVolumesFile;
-
     /** Whether to use a viscosity that is linear in the cell area, rather than constant. */
     bool mUseAreaBasedDampingConstant;
 
@@ -343,12 +340,10 @@ public:
      */
     void WriteCellPopulationVolumeResultsToFile();
 
-    /**
-     * Write the current index and location of each non-ghost node in mrMesh, as well as the ID and
-     * (using mpVoronoiTessellation) the area (in 2D) or volume (in 3D) of its corresponding
-     * cell, to mpCellVolumesFile.
+    /** 
+     * Overridden WriteCellVolumeResultsToFile() method.
      */
-    void WriteCellVolumeResultsToFile();
+    void WriteCellVolumeResultsToFile();    
 
     /**
      * Create a Voronoi tessellation of the mesh.

cell_based/src/population/NodeBasedCellPopulation.hpp
@@ -65,7 +65,7 @@ private:
     /** Vector of maximal spatial positions in each dimension. */
     c_vector<double, DIM> mMaxSpatialPositions;
 
-    /** Node pairs for force calculations */
+    /** Node pairs for force calculations. */
     std::set< std::pair<Node<DIM>*, Node<DIM>* > > mNodePairs;
 
     /** Whether to delete the nodes-only mesh (taken in one of the constructors, defaults to false). */
@@ -132,6 +132,11 @@ private:
      * Loops over nodes and sets mMinSpatialPositions and mMaxSpatialPositions
      */
     void FindMaxAndMin();
+
+    /** 
+     * Overridden WriteCellVolumeResultsToFile() method.
+     */
+    void WriteCellVolumeResultsToFile();    
 
     /**
      * Overridden WriteVtkResultsToFile() method.
@@ -265,17 +270,6 @@ public:
      * @return The maximum distance between any nodes in this dimension.
      */
     double GetWidth(const unsigned& rDimension);
-
-    /**
-     * Overridden SetOutputCellVolumes() method.
-     *
-     * Currently there is no facility for computing the volume associated
-     * with each cell in a NodeBasedCellPopulation, so if this method is
-     * called with outputCellVolumes = true, an exception is thrown.
-     *
-     * @param outputCellVolumes the new value of mOutputCellVolumes
-     */
-    void SetOutputCellVolumes(bool outputCellVolumes);
 };
 
 #include "SerializationExportWrapper.hpp"

cell_based/src/population/VertexBasedCellPopulation.cpp
@@ -450,7 +450,7 @@ void VertexBasedCellPopulation<DIM>::WriteCellVolumeResultsToFile()
     assert(DIM==2);
 
      // Write time to file
-    *mpCellVolumesFile << SimulationTime::Instance()->GetTime() << " ";
+    *(this->mpCellVolumesFile) << SimulationTime::Instance()->GetTime() << " ";
 
     // Loop over cells and find associated elements so in the same order as the cells in output files
     for (std::list<CellPtr>::iterator cell_iter = this->mCells.begin();
@@ -471,25 +471,25 @@ void VertexBasedCellPopulation<DIM>::WriteCellVolumeResultsToFile()
         if ( !(GetElement(elem_index)->IsDeleted()) && !elem_corresponds_to_dead_cell)
         {
             // Write element index to file
-            *mpCellVolumesFile << elem_index << " ";
+            *(this->mpCellVolumesFile) << elem_index << " ";
 
             // Write cell ID to file
             unsigned cell_index = (*cell_iter)->GetCellId();
-            *mpCellVolumesFile << cell_index << " ";
+            *(this->mpCellVolumesFile) << cell_index << " ";
 
             // Write location of element centroid to file
             c_vector<double, DIM> centre_location = GetLocationOfCellCentre(*cell_iter);
             for (unsigned i=0; i<DIM; i++)
             {
-                *mpCellVolumesFile << centre_location[i] << " ";
+                *(this->mpCellVolumesFile) << centre_location[i] << " ";
             }
 
             // Write cell volume (in 3D) or area (in 2D) to file
             double cell_volume = mrMesh.GetVolumeOfElement(elem_index);
-            *mpCellVolumesFile << cell_volume << " ";
+            *(this->mpCellVolumesFile) << cell_volume << " ";
         }
     }
-    *mpCellVolumesFile << "\n";
+    *(this->mpCellVolumesFile) << "\n";
 }
 
 
@@ -631,11 +631,6 @@ void VertexBasedCellPopulation<DIM>::CreateOutputFiles(const std::string& rDirec
     mpVizElementsFile = output_file_handler.OpenOutputFile("results.vizelements");
     mpT1SwapLocationsFile = output_file_handler.OpenOutputFile("T1SwapLocations.dat");
     mpT3SwapLocationsFile = output_file_handler.OpenOutputFile("T3SwapLocations.dat");
-
-    if (this->mOutputCellVolumes)
-    {
-        mpCellVolumesFile = output_file_handler.OpenOutputFile("cellareas.dat");
-    }
 }
 
 template<unsigned DIM>
@@ -645,13 +640,7 @@ void VertexBasedCellPopulation<DIM>::CloseOutputFiles()
     mpVizElementsFile->close();
     mpT1SwapLocationsFile->close();
     mpT3SwapLocationsFile->close();
-
-    if (this->mOutputCellVolumes)
-    {
-        mpCellVolumesFile->close();
-    }
 }
-
 
 template<unsigned DIM>
 void VertexBasedCellPopulation<DIM>::GenerateCellResultsAndWriteToFiles()

cell_based/src/population/VertexBasedCellPopulation.hpp
@@ -71,9 +71,6 @@ private:
     /** Results file for locations of T3Swaps. */
     out_stream mpT3SwapLocationsFile;
 
-    /** Results file for cell volume (in 3D) or area (in 2D) data. */
-    out_stream mpCellVolumesFile;
-
     /**
      * Overridden WriteVtkResultsToFile() method.
      */

commit 08e23094c1958582f62a7badfa5bc96ea37b2e06

Fix profile build broken in r12854/#1762
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@12924 b5f69653-2bf5-0310-8201-b0dafb8e1405

cell_based/src/population/NodeBasedCellPopulation.cpp
@@ -351,12 +351,25 @@ void NodeBasedCellPopulation<DIM>::WriteCellVolumeResultsToFile()
     {
         // Get the index of the corresponding node in mrMesh
         unsigned node_index = this->GetLocationIndexUsingCell(*cell_iter);
+           
+        // Write node index to file
+        *(this->mpCellVolumesFile) << node_index << " ";
+
+        // Write cell ID to file
+        *(this->mpCellVolumesFile) << cell_iter->GetCellId() << " ";
+
+        // Write node location to file
+        c_vector<double, DIM> node_location = this->GetNode(node_index)->rGetLocation();
+        for (unsigned i=0; i<DIM; i++)
+        {
+            *(this->mpCellVolumesFile) << node_location[i] << " ";
+        }
 
         // Get cell radius
         double cell_radius = mrMesh.GetCellRadius(node_index);
 
         // Get cell volume from radius
-        double cell_volume;
+        double cell_volume = 0.0;
         
         if (DIM == 2)
         {
@@ -366,21 +379,6 @@ void NodeBasedCellPopulation<DIM>::WriteCellVolumeResultsToFile()
         {
             cell_volume = (4.0/3.0)*M_PI*cell_radius*cell_radius*cell_radius;
         }
-           
-        // Write node index to file
-        *(this->mpCellVolumesFile) << node_index << " ";
-
-        // Write cell ID to file
-        unsigned cell_index = cell_iter->GetCellId();
-        *(this->mpCellVolumesFile) << cell_index << " ";
-
-        // Write node location to file
-        c_vector<double, DIM> node_location = this->GetNode(node_index)->rGetLocation();
-        for (unsigned i=0; i<DIM; i++)
-        {
-            *(this->mpCellVolumesFile) << node_location[i] << " ";
-        }
-
         // Write cell volume (in 3D) or area (in 2D) to file
         *(this->mpCellVolumesFile) << cell_volume << " ";
     }