cc5f157763017c2d053cac8ea50497907ce72731
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2011-06-27 13:22:15 +0000

    #1755 reduce fibre file sizes using #1768
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@12997 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- heart/test/TestFibreWriter.hpp
+++ heart/test/TestFibreWriter.hpp
@@ -108,6 +108,110 @@ public:
         std::string results_dir = OutputFileHandler::GetChasteTestOutputDirectory() + "TestFibreWriter/";
         TS_ASSERT_EQUALS(system(("diff -a -I \"Created by Chaste\" " + results_dir + "/Orthotropic3DBin.ortho heart/test/data/fibre_tests/Orthotropic3DBin.ortho").c_str()), 0);      
     }
+
+
+    //Following are convenience methods
+private:
+    void ConvertToBinaryOrtho(std::string fullPath, std::string baseName, bool output) throw (Exception)
+    {
+        FileFinder file_finder(fullPath+"/"+baseName+".ortho", RelativeTo::ChasteSourceRoot);
+        FibreReader<3> fibre_reader(file_finder, ORTHO);
+        std::vector< c_vector<double, 3> > fibres;
+        std::vector< c_vector<double, 3> > second;
+        std::vector< c_vector<double, 3> > third;
+        fibre_reader.GetAllOrtho(fibres, second, third);
+        TS_ASSERT_EQUALS(fibres.size(), second.size());
+        TS_ASSERT_EQUALS(fibres.size(), third.size());
+        std::string dir =   "FibreConverter";
+        //Write binary file
+        FibreWriter<3> fibre_writer(dir, baseName+"_bin", false);
+        fibre_writer.SetWriteFileAsBinary();
+        fibre_writer.WriteAllOrtho(fibres, second, third);
+        
+        //Read it back
+        FileFinder file_finder_bin(dir+"/"+baseName+"_bin.ortho", RelativeTo::ChasteTestOutput);
+        FibreReader<3> fibre_reader_bin(file_finder_bin, ORTHO);
+        std::vector< c_vector<double, 3> > fibres_bin;
+        std::vector< c_vector<double, 3> > second_bin;
+        std::vector< c_vector<double, 3> > third_bin;
+        fibre_reader_bin.GetAllOrtho(fibres_bin, second_bin, third_bin);
+        
+        TS_ASSERT_EQUALS(fibres.size(), fibres_bin.size());
+        TS_ASSERT_EQUALS(second.size(), second_bin.size());
+        TS_ASSERT_EQUALS(third.size(), third_bin.size());
+        
+        for (unsigned i = 0; i< fibres.size(); i++)
+        {
+            for (unsigned j = 0; j< 3u ; j++)
+            {
+                TS_ASSERT_DELTA(fibres[i][j], fibres_bin[i][j], 1e-16);
+                TS_ASSERT_DELTA(second[i][j], second_bin[i][j], 1e-16);
+                TS_ASSERT_DELTA(third[i][j], third_bin[i][j], 1e-16);
+            }
+        }
+        if (output)
+        {
+            std::cout<<"cp "<<
+                OutputFileHandler::GetChasteTestOutputDirectory() 
+                << dir <<"/"<<baseName<<"_bin.ortho " <<
+                fullPath << "/" << baseName << ".ortho\n";
+            system(("ls -lh " + OutputFileHandler::GetChasteTestOutputDirectory() 
+                + dir + "/" + baseName + "_bin.ortho " +
+                fullPath + "/" + baseName + ".ortho\n").c_str());
+            }
+        }
+    void ConvertToBinaryAxi(std::string fullPath, std::string baseName, bool output) throw (Exception)
+    {
+        FileFinder file_finder(fullPath+"/"+baseName+".axi", RelativeTo::ChasteSourceRoot);
+        FibreReader<3> fibre_reader(file_finder, AXISYM);
+        std::vector< c_vector<double, 3> > fibres;
+        fibre_reader.GetAllAxi(fibres);
+        std::string dir =   "FibreConverter";
+        //Write binary file
+        FibreWriter<3> fibre_writer(dir, baseName+"_bin", false);
+        fibre_writer.SetWriteFileAsBinary();
+        fibre_writer.WriteAllAxi(fibres);
+        
+        //Read it back
+        FileFinder file_finder_bin(dir+"/"+baseName+"_bin.axi", RelativeTo::ChasteTestOutput);
+        FibreReader<3> fibre_reader_bin(file_finder_bin, AXISYM);
+        std::vector< c_vector<double, 3> > fibres_bin;
+        fibre_reader_bin.GetAllAxi(fibres_bin);
+        
+        TS_ASSERT_EQUALS(fibres.size(), fibres_bin.size());
+        
+        for (unsigned i = 0; i< fibres.size(); i++)
+        {
+            for (unsigned j = 0; j< 3u ; j++)
+            {
+                TS_ASSERT_DELTA(fibres[i][j], fibres_bin[i][j], 1e-16);
+            }
+        }
+        if (output)
+        {
+            std::cout<<"cp "<<
+                OutputFileHandler::GetChasteTestOutputDirectory() 
+                << dir <<"/"<<baseName<<"_bin.axi " <<
+                fullPath << "/" << baseName << ".axi\n";
+            system(("ls -lh " + OutputFileHandler::GetChasteTestOutputDirectory() 
+                + dir + "/" + baseName + "_bin.axi " +
+                fullPath + "/" + baseName + ".axi\n").c_str());
+        }
+    }
+public:
+    void TestConvertFiles() throw (Exception)
+    {
+          ConvertToBinaryOrtho("heart/test/data/box_shaped_heart/", "box_heart", false);
+          ConvertToBinaryAxi("heart/test/data/box_shaped_heart/", "box_heart", false);
+    }
+    void doNotTestReallyConvertFiles() throw (Exception)
+    {
+          ConvertToBinaryOrtho("heart/test/data/fibre_tests/", "downsampled", true);
+          ConvertToBinaryOrtho("heart/test/data/point50_heart_mesh/", "point50", true);
+          ConvertToBinaryAxi("apps/texttest/weekly/Propagation3d/", "heart_chaste2_renum_i_triangles", true);
+          //This one is dodgy... ConvertToBinaryAxi("notforrelease/test/data/simplified_very_low_res/", "heart_chaste2_renum_e_triangles", true);
+    }
+
 };
 
 
