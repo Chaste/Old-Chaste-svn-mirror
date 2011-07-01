a46a557265888ca197046db2a89e0ba248067f36
Pras Pathmanathan <pras@cs.ox.ac.uk>
2011-07-01 12:04:58 +0000

    #1818 3d test
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@13052 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- pde/test/TestIncompressibleNonlinearElasticitySolverLong.hpp
+++ pde/test/TestIncompressibleNonlinearElasticitySolverLong.hpp
@@ -120,29 +120,29 @@ double ThreeDimensionalModelProblem::a = 0.1;
 double ThreeDimensionalModelProblem::b = 0.1;
 double ThreeDimensionalModelProblem::c1 = 0.1;
 
-/**
- * Solve 3D nonlinear elasticity problem with an exact solution.
- *
- * For full details see Pathmanathan, Gavaghan, Whiteley "A comparison of numerical
- * methods used in finite element modelling of soft tissue deformation", J. Strain
- * Analysis, to appear.
- *
- * We solve a 3d problem on a cube with a Neo-Hookean material, assuming the solution
- * will be
- *   x = X+aX^2/2
- *   y = Y+bY^2/2
- *   z = Z/(1+aX)(1+bY)
- * with p=2c (c the material parameter),
- * which, note, has been constructed to be an incompressible. We assume displacement
- * boundary conditions on X=0 and traction boundary conditions on the remaining 5 surfaces.
- * It is then possible to determine the body force and surface tractions required for
- * this deformation, and they are defined in the above class.
- */
+
 class TestIncompressibleNonlinearElasticitySolverLong : public CxxTest::TestSuite
 {
 public:
-
-    void TestSolve3d() throw(Exception)
+    /**
+     * Solve 3D nonlinear elasticity problem with an exact solution.
+     *
+     * For full details see Pathmanathan, Gavaghan, Whiteley "A comparison of numerical
+     * methods used in finite element modelling of soft tissue deformation", J. Strain
+     * Analysis, to appear.
+     *
+     * We solve a 3d problem on a cube with a Neo-Hookean material, assuming the solution
+     * will be
+     *   x = X+aX^2/2
+     *   y = Y+bY^2/2
+     *   z = Z/(1+aX)(1+bY)
+     * with p=2c (c the material parameter),
+     * which, note, has been constructed to be an incompressible. We assume displacement
+     * boundary conditions on X=0 and traction boundary conditions on the remaining 5 surfaces.
+     * It is then possible to determine the body force and surface tractions required for
+     * this deformation, and they are defined in the above class.
+     */
+    void xTestSolve3d() throw(Exception)
     {
         unsigned num_elem_each_dir = 5;
         QuadraticMesh<3> mesh(1.0/num_elem_each_dir, 1.0, 1.0, 1.0);
@@ -222,6 +222,140 @@ public:
             TS_ASSERT_DELTA( solver.rGetPressures()[i]/(2*ThreeDimensionalModelProblem::c1), 1.0, 2e-1);
         }
     }
+
+
+    /**
+     *  Solve a problem in which the traction boundary condition is a normal pressure
+     *  applied to a surface of the deformed body.
+     *
+     *  The initial body is the unit cube. The deformation is given by x=Rx', where
+     *  R is a rotation matrix and x'=[X/(lambda^2)  lambda*Y lambda*Z], ie simple stretching
+     *  followed by a rotation.
+     *
+     *  Ignoring R for the time being, the deformation gradient would be
+     *  F=diag(1/lambda^2, lambda, lambda).
+     *  Assuming a Neo-Hookean material law, the first Piola-Kirchoff tensor is
+     *
+     *   S = 2c F^T - p F^{-1}
+     *     = diag ( 2c lam^{-2} - p lam^2,  2c lam - p lam^{-1},  2c lam - p lam^{-1}
+     *
+     *  We choose the internal pressure (p) to be 2clam^2, so that
+     *
+     *  S = diag( 2c (lam^{-2} - lam^4), 0, 0)
+     *
+     *  To obtain this deformation as the solution, we can specify fixed location boundary conditions
+     *  on X=0 and specify a traction of t=[2c(lam^{-2} - lam^4) 0 0] on the X=1 surface.
+     *
+     *  Now, including the rotation matrix, we can specify appropriate fixed location boundary conditions
+     *  on the X=0 surface, and specify that a normal pressure should act on the *deformed* X=1 surface,
+     *  with value P = 2c(lam^{-2} - lam^4)/(lambda^2) [divided by lambda^2 as the deformed surface
+     *  has a smaller area than the undeformed surface.
+     *
+     */
+    void TestWithPressureOnDeformedSurfaceBoundaryCondition3d() throw (Exception)
+    {
+        double lambda = 0.85;
+
+        unsigned num_elem_each_dir = 5;
+        QuadraticMesh<3> mesh(1.0/num_elem_each_dir, 1.0, 1.0, 1.0);
+
+        // Neo-Hookean material law
+        double c1 = 1.0;
+        MooneyRivlinMaterialLaw<3> law(c1, 0.0);
+
+        // anything will do here
+        c_matrix<double,3,3> rotation_matrix = identity_matrix<double>(3);
+        rotation_matrix(0,0)=1.0/sqrt(2);
+        rotation_matrix(0,1)=-1.0/sqrt(2);
+        rotation_matrix(1,0)=1.0/sqrt(2);
+        rotation_matrix(1,1)=1.0/sqrt(2);
+
+
+        // Define displacement boundary conditions
+        std::vector<unsigned> fixed_nodes;
+        std::vector<c_vector<double,3> > locations;
+
+        for (unsigned i=0; i<mesh.GetNumNodes(); i++)
+        {
+            double X = mesh.GetNode(i)->rGetLocation()[0];
+            double Y = mesh.GetNode(i)->rGetLocation()[1];
+            double Z = mesh.GetNode(i)->rGetLocation()[2];
+
+            // if X=0
+            if ( fabs(X)<1e-6)
+            {
+                fixed_nodes.push_back(i);
+                c_vector<double,3> new_position_before_rotation;
+                new_position_before_rotation(0) = 0.0;
+                new_position_before_rotation(1) = lambda*Y;
+                new_position_before_rotation(2) = lambda*Z;
+                locations.push_back(prod(rotation_matrix, new_position_before_rotation));
+            }
+        }
+        assert(fixed_nodes.size()==(2*num_elem_each_dir+1)*(2*num_elem_each_dir+1));
+
+        // Define pressure boundary conditions X=1 surface
+        std::vector<BoundaryElement<2,3>*> boundary_elems;
+
+//        std::vector<c_vector<double,3> > tractions;
+//        c_vector<double,3> traction = zero_vector<double>(3);
+//        traction(0)=2*c1*(pow(lambda,-2)-pow(lambda,4));
+
+        double pressure_bc = 2*c1*((pow(lambda,-2)-pow(lambda,4)))/(lambda*lambda);
+
+        for (TetrahedralMesh<3,3>::BoundaryElementIterator iter
+              = mesh.GetBoundaryElementIteratorBegin();
+            iter != mesh.GetBoundaryElementIteratorEnd();
+            ++iter)
+        {
+            if (fabs((*iter)->CalculateCentroid()[0]-1)<1e-6)
+            {
+                BoundaryElement<2,3>* p_element = *iter;
+                boundary_elems.push_back(p_element);
+//                tractions.push_back(traction);
+            }
+        }
+        assert(boundary_elems.size()==2*num_elem_each_dir*num_elem_each_dir);
+
+        SolidMechanicsProblemDefinition<3> problem_defn(mesh);
+        problem_defn.SetFixedNodes(fixed_nodes, locations);
+        problem_defn.SetApplyNormalPressureOnDeformedSurface(boundary_elems, pressure_bc);
+//        problem_defn.SetTractionBoundaryConditions(boundary_elems, tractions);
+
+
+        IncompressibleNonlinearElasticitySolver<3> solver(mesh,
+                                                          problem_defn,
+                                                          &law,
+                                                          "nonlin_elas_3d_pressure_on_deformed");
+
+        solver.Solve();
+
+        // Compare
+        std::vector<c_vector<double,3> >& r_solution = solver.rGetDeformedPosition();
+        for (unsigned i=0; i<mesh.GetNumNodes(); i++)
+        {
+            double X = mesh.GetNode(i)->rGetLocation()[0];
+            double Y = mesh.GetNode(i)->rGetLocation()[1];
+            double Z = mesh.GetNode(i)->rGetLocation()[2];
+
+            c_vector<double,3> new_position_before_rotation;
+            new_position_before_rotation(0) = X/(lambda*lambda);
+            new_position_before_rotation(1) = lambda*Y;
+            new_position_before_rotation(2) = lambda*Z;
+
+            c_vector<double,3> new_position = prod(rotation_matrix, new_position_before_rotation);
+
+            TS_ASSERT_DELTA(r_solution[i](0), new_position(0), 1e-2);
+            TS_ASSERT_DELTA(r_solution[i](1), new_position(1), 1e-2);
+            TS_ASSERT_DELTA(r_solution[i](2), new_position(2), 1e-2);
+        }
+
+        for (unsigned i=0; i<mesh.GetNumVertices(); i++)
+        {
+            TS_ASSERT_DELTA( solver.rGetPressures()[i], 2*c1*lambda*lambda, 0.05);
+        }
+    }
 };
+
 
 #endif /*TESTINCOMPRESSIBLENONLINEARELASTICITYSOLVERLONG_HPP_*/
