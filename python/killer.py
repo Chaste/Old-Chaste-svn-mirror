f6873f856324a40138d00814da08ed66f31f07e9
Jon Cooper <jonathan.cooper@cs.ox.ac.uk>
2011-10-27 13:10:36 +0000

    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@14115 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- python/killer.py
+++ python/killer.py
@@ -36,28 +36,28 @@ import sys
 
 sim = '-s' in sys.argv
 if '-d' in sys.argv:
-  i = sys.argv.index('-d')
-  kill_dir = os.path.realpath(sys.argv[i+1])
+    i = sys.argv.index('-d')
+    kill_dir = os.path.realpath(sys.argv[i+1])
 else:
-  kill_dir = os.path.realpath(os.getcwd())
+    kill_dir = os.path.realpath(os.getcwd())
 
-print "Killing processes owned by", os.getpid(), "in", kill_dir
+print "Killing processes owned by", os.getuid(), "in", kill_dir
 
 for pid in os.listdir('/proc/'):
-  if pid[0] in "0123456789":
-    try:
-      s = os.stat('/proc/' + pid)
-      if s.st_uid == os.getuid():
-        cwd = os.path.realpath('/proc/' + pid + '/cwd')
-        if cwd == kill_dir and int(pid) != os.getpid():
-          print pid, "is running from our dir as",
-          f = open('/proc/' + pid + '/cmdline')
-          cmdline = f.read().split('\x00')[:-1]
-          f.close()
-          print cmdline[0:3]
-          if not sim:
-            os.kill(int(pid), signal.SIGTERM)
-            print " ** SENT SIGTERM  mwa ha ha"
-    except OSError:
-      # We can't read all our processes; that's ok
-      pass
+    if pid[0] in "0123456789":
+        try:
+            s = os.stat('/proc/' + pid)
+            if s.st_uid == os.getuid():
+                cwd = os.path.realpath('/proc/' + pid + '/cwd')
+                if cwd == kill_dir and int(pid) != os.getpid():
+                    print pid, "is running from our dir as",
+                    f = open('/proc/' + pid + '/cmdline')
+                    cmdline = f.read().split('\x00')[:-1]
+                    f.close()
+                    print cmdline[0:3]
+                    if not sim:
+                        os.kill(int(pid), signal.SIGTERM)
+                        print " ** SENT SIGTERM  mwa ha ha"
+        except OSError:
+            # We can't read all our processes; that's ok
+            pass
