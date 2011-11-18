eabed69bd8fa6db5df1dfa28240b0046d4f9e16d
Joe Pitt-Francis <jmpf@comlab.ox.ac.uk>
2011-11-18 16:05:13 +0000

    #1885 Fix build
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@14220 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- global/src/TimeStepper.hpp
+++ global/src/TimeStepper.hpp
@@ -42,6 +42,7 @@ along with Chaste. If not, see <http://www.gnu.org/licenses/>.
 class TimeStepper
 {
     friend class TestTimeStepper;
+    friend class SimulationTime; ///\todo #1885 remove this when SimulationTime uses archived TimeStepper
 private:
     /*
      * Private default constructor for archiving
