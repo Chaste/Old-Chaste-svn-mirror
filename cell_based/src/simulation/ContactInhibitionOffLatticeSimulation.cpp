bf46403888392b369dc0a52478cca76249b6fba8
Yohan Davit <davit@maths.ox.ac.uk>
2011-12-14 12:13:54 +0000

    [yohan] #1727 Added simulations for contact inhibition
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@14422 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ cell_based/src/simulation/ContactInhibitionOffLatticeSimulation.cpp
@@ -0,0 +1,85 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "ContactInhibitionOffLatticeSimulation.hpp"
+#include "MeshBasedCellPopulation.hpp"
+#include "CellwiseData.hpp"
+#include "ContactInhibitionCellCycleModel.hpp"
+
+template<unsigned DIM>
+ContactInhibitionOffLatticeSimulation<DIM>::ContactInhibitionOffLatticeSimulation(AbstractCellPopulation<DIM>& rCellPopulation,
+                                                                  bool deleteCellPopulationInDestructor,
+                                                                  bool initialiseCells)
+    : OffLatticeSimulation<DIM>(rCellPopulation, deleteCellPopulationInDestructor, initialiseCells)
+{
+}
+
+template<unsigned DIM>
+ContactInhibitionOffLatticeSimulation<DIM>::~ContactInhibitionOffLatticeSimulation()
+{
+}
+
+template<unsigned DIM>
+void ContactInhibitionOffLatticeSimulation<DIM>::PostSolve()
+{
+	assert(dynamic_cast<MeshBasedCellPopulation<DIM>*>(&(this->mrCellPopulation)));
+	MeshBasedCellPopulation<DIM>* p_static_cast_cell_population = static_cast<MeshBasedCellPopulation<DIM>*>(&(this->mrCellPopulation));
+    this->mrCellPopulation.Update();
+    CellwiseData<DIM>::Instance()->ReallocateMemory();
+
+	// Create and initialize a vector of cell volumes
+	unsigned num_cells = this->mrCellPopulation.GetNumRealCells();
+	std::vector<double> volume(num_cells);
+	for (unsigned i=0; i<num_cells; i++)
+	{
+		volume[i] = 0.0;
+	}
+
+	// Loop over cells and set volume value in CellWiseData
+	p_static_cast_cell_population->CreateVoronoiTessellation();
+	for (typename AbstractCellPopulation<DIM>::Iterator cell_iter = this->mrCellPopulation.Begin();
+		 cell_iter != this->mrCellPopulation.End();
+		 ++cell_iter)
+	{
+		unsigned node_index = this->mrCellPopulation.GetLocationIndexUsingCell(*cell_iter);
+		volume[node_index] = p_static_cast_cell_population->GetVolumeOfVoronoiElement(node_index);
+		CellwiseData<DIM>::Instance()->SetValue(volume[node_index], node_index, 0);
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Explicit instantiation
+/////////////////////////////////////////////////////////////////////////////
+
+template class ContactInhibitionOffLatticeSimulation<1>;
+template class ContactInhibitionOffLatticeSimulation<2>;
+template class ContactInhibitionOffLatticeSimulation<3>;
+
+// Serialization for Boost >= 1.36
+#include "SerializationExportWrapperForCpp.hpp"
+EXPORT_TEMPLATE_CLASS_SAME_DIMS(ContactInhibitionOffLatticeSimulation)
---
+++ cell_based/test/simulation/TestOffLatticeSimulationWithContactInhibition.hpp
@@ -0,0 +1,162 @@
+/*
+
+Copyright (C) University of Oxford, 2005-2011
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef TESTOFFLATTICESIMULATIONWITHCONTACTINHIBITION_HPP_
+#define TESTOFFLATTICESIMULATIONWITHCONTACTINHIBITION_HPP_
+
+#include <cxxtest/TestSuite.h>
+
+// Must be included before other cell_based headers
+#include "CellBasedSimulationArchiver.hpp"
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <fstream>
+
+#include "OffLatticeSimulation.hpp"
+#include "MeshBasedCellPopulation.hpp"
+#include "ContactInhibitionCellCycleModel.hpp"
+#include "ContactInhibitionOffLatticeSimulation.hpp"
+#include "WildTypeCellMutationState.hpp"
+#include "HoneycombMeshGenerator.hpp"
+#include "SmartPointers.hpp"
+#include "OutputFileHandler.hpp"
+#include "GeneralisedLinearSpringForce.hpp"
+#include "SimulationTime.hpp"
+#include "CellLabel.hpp"
+#include "OutputFileHandler.hpp"
+#include "MutableMesh.hpp"
+#include "PlaneBoundaryCondition.hpp"
+
+class TestContactInhibitionOffLatticeSimulation : public CxxTest::TestSuite
+{ 
+public:
+
+    void TestBoxContactInhibitionCellCycleModel()
+      {
+          // Set up SimulationTime
+          SimulationTime* p_simulation_time = SimulationTime::Instance();
+          p_simulation_time->SetStartTime(0.0);
+
+          // Create a simple mesh
+          HoneycombMeshGenerator generator(2, 2, 1);
+          MutableMesh<2,2>* p_mesh = generator.GetMesh();
+
+          // Create cell state
+      	  MAKE_PTR(WildTypeCellMutationState, p_state);
+          std::vector<CellPtr> cells;
+
+          for (unsigned i=0; i<p_mesh->GetNumNodes(); i++)
+          {
+          	ContactInhibitionCellCycleModel* p_cycle_model = new ContactInhibitionCellCycleModel();
+            p_cycle_model->SetCellProliferativeType(STEM);
+            p_cycle_model->SetDimension(2);
+            p_cycle_model->SetBirthTime(0.0);
+            p_cycle_model->SetQuiescentVolumeFraction(0.5);
+            p_cycle_model->SetEquilibriumVolume(1.0);
+            p_cycle_model->SetStemCellG1Duration(0.5);
+            p_cycle_model->SetTransitCellG1Duration(0.5);
+
+            CellPtr p_cell(new Cell(p_state, p_cycle_model));
+            p_cell->InitialiseCellCycleModel();
+            cells.push_back(p_cell);
+          }
+
+          // Create a cell population
+          MeshBasedCellPopulation<2> cell_population(*p_mesh, cells);
+
+          // Create a force law
+          MAKE_PTR(GeneralisedLinearSpringForce<2>, p_force);
+          p_force->SetCutOffLength(1.5);
+
+          // Create a singleton class to store the volume of the cells and initialize it
+          CellwiseData<2>* p_data = CellwiseData<2>::Instance();
+          p_data->SetNumCellsAndVars(cell_population.GetNumRealCells(), 1);
+          p_data->SetCellPopulation(&cell_population);
+
+          for (unsigned i=0; i<p_mesh->GetNumNodes(); i++)
+          {
+          	p_data->SetValue(1.0, p_mesh->GetNode(i)->GetIndex());
+          }
+
+          // Create a contact inhibition simulator
+          ContactInhibitionOffLatticeSimulation<2> simulator(cell_population);
+          simulator.SetOutputDirectory("TestOffLatticeBoxWithContactInhibition");
+          simulator.SetEndTime(100.0);
+          simulator.AddForce(p_force);
+
+          // Create some boundary conditions and pass them to the simulation
+          c_vector<double,2> point = zero_vector<double>(2);
+          c_vector<double,2> normal = zero_vector<double>(2);
+          point(0) = -3.0;
+          point(1) = 0.0;
+          normal(0) = -1.0;
+          normal(1) = 0.0;
+          MAKE_PTR_ARGS(PlaneBoundaryCondition<2>, p_bc1, (&cell_population, point, normal)); // y>0
+          simulator.AddCellPopulationBoundaryCondition(p_bc1);
+          point(0) = 3.0;
+          point(1) = 0.0;
+          normal(0) = 1.0;
+          normal(1) = 0.0;
+          MAKE_PTR_ARGS(PlaneBoundaryCondition<2>, p_bc2, (&cell_population, point, normal)); // y<2
+          simulator.AddCellPopulationBoundaryCondition(p_bc2);
+          point(0) = 0.0;
+          point(1) = -3.0;
+          normal(0) = 0.0;
+          normal(1) = -1.0;
+          MAKE_PTR_ARGS(PlaneBoundaryCondition<2>, p_bc3, (&cell_population, point, normal)); // y<2
+          simulator.AddCellPopulationBoundaryCondition(p_bc3);
+          point(0) = 0.0;
+          point(1) = 3.0;
+          normal(0) = 0.0;
+          normal(1) = 1.0;
+          MAKE_PTR_ARGS(PlaneBoundaryCondition<2>, p_bc4, (&cell_population, point, normal)); // y<2
+          simulator.AddCellPopulationBoundaryCondition(p_bc4);
+
+          // Run simulation
+          simulator.Solve();
+
+          // Test that the volumes of the cells are correct
+          cell_population.CreateVoronoiTessellation();
+          for (MeshBasedCellPopulation<2>::Iterator cell_iter = cell_population.Begin();
+          		 cell_iter != cell_population.End();
+          		 ++cell_iter)
+			{
+				unsigned node_index = cell_population.GetLocationIndexUsingCell(*cell_iter);
+				TS_ASSERT_LESS_THAN(cell_population.GetVolumeOfVoronoiElement(node_index), 0.51);
+			}
+
+          // Tidy up
+          SimulationTime::Destroy();
+          RandomNumberGenerator::Destroy();
+          CellwiseData<2>::Destroy();
+      }
+};
+
+#endif /*TESTOFFLATTICESIMULATIONWITHCONTACTINHIBITION_HPP_*/
