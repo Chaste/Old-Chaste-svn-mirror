Chaste/Old-Chaste-svn-mirror

commit c48d2b5b7abc015586a1d47e15f59dd4377983d7

#2300 Scale flux so that the solutions to flux/pressure in the coupled system are comparable
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21208 b5f69653-2bf5-0310-8201-b0dafb8e1405

continuum_mechanics/src/ventilation/VentilationProblem.cpp
@@ -59,7 +59,8 @@ VentilationProblem::VentilationProblem(const std::string& rMeshDirFilePath, unsi
     // Note pipe flow equation has 3 variables and flux balance has 3 variables (at a bifurcation)
     // preallocating 5 non-zeros allows for 4-way branching
     mpLinearSystem = new LinearSystem(mMesh.GetNumNodes()+mMesh.GetNumElements(), 5u);
-    // Possibly more robust... mpLinearSystem->SetKspType("gmres");
+    mpLinearSystem->SetAbsoluteTolerance(1e-10);
+    mpLinearSystem->SetKspType("gmres");
 
     /*
      * Set up the Acinar units at the terminals
@@ -148,7 +149,7 @@ void VentilationProblem::SetConstantInflowFluxes(double flux)
      {
          if ((*iter)->GetIndex() != mOutletNodeIndex)
          {
-             SetFluxAtBoundaryNode(*(*iter), flux);
+             SetFluxAtBoundaryNode(*(*iter), flux*mFluxScaling);
          }
      }
 }
@@ -249,7 +250,7 @@ void VentilationProblem::Assemble(bool dynamicReassemble)
                  * The upshot of this calculation is that the resistance is scaled with sqrt(Q)
                  */
                 // Note that we can only do this if mSolution is valid AND we own the local part
-                double flux = PetscVecTools::GetElement(mSolution, element_index);
+                double flux = PetscVecTools::GetElement(mSolution, element_index)/mFluxScaling;
                 double reynolds_number = fabs( 2.0 * mDensity * flux / (mViscosity * M_PI * radius) );
                 double c = 1.85;
                 double z = (c/4.0) * sqrt(reynolds_number * radius / length);
@@ -263,7 +264,7 @@ void VentilationProblem::Assemble(bool dynamicReassemble)
             unsigned pressure_index_0 =  mMesh.GetNumElements() +  iter->GetNodeGlobalIndex(0);
             unsigned pressure_index_1 =  mMesh.GetNumElements() +  iter->GetNodeGlobalIndex(1);
 
-            mpLinearSystem->SetMatrixElement(element_index, element_index, -resistance);
+            mpLinearSystem->SetMatrixElement(element_index, element_index, -resistance/mFluxScaling);
             mpLinearSystem->SetMatrixElement(element_index, pressure_index_0,  1.0);
             mpLinearSystem->SetMatrixElement(element_index, pressure_index_1, -1.0);
         }
@@ -309,8 +310,8 @@ void VentilationProblem::Solve()
 {
     Assemble();
     mpLinearSystem->AssembleFinalLinearSystem();
-    // mpLinearSystem->DisplayMatrix();
-    // mpLinearSystem->DisplayRhs();
+//     mpLinearSystem->DisplayMatrix();
+//     mpLinearSystem->DisplayRhs();
 
     if (mSolution==NULL)
     {
@@ -363,7 +364,7 @@ void VentilationProblem::GetSolutionAsFluxesAndPressures(std::vector<double>& rF
     rFluxesOnEdges.resize(num_elem);
     for (unsigned i=0; i<num_elem; i++)
     {
-        rFluxesOnEdges[i] = solution_vector_repl[i];
+        rFluxesOnEdges[i] = solution_vector_repl[i]/mFluxScaling;
     }
 
     rPressuresOnNodes.resize(mMesh.GetNumNodes());

continuum_mechanics/src/ventilation/VentilationProblem.hpp
@@ -81,7 +81,7 @@ private:
      *  This is used in the dynamic (Pedley) resistance calculation
      */
     double mDensity;
-
+    static const double mFluxScaling = 2e-5; /**< In order to keep the pressure and flux solution at a comparable magnitude, so solve for mFluxScaling * flux */
     Vec mSolution; /**< Allow access to the solution of the linear system and use as a guess later */
 
     std::vector<Swan2012AcinarUnit*> mAcinarUnits; /**< One acinar unit for each terminal node. \todo These will be abstract*/
@@ -184,7 +184,6 @@ public:
      * processors.  This method replicates the solution across all processors and then splits it into its flux
      * and pressure components.  Because of the replication it makes sense to get both solutions in a single call.
      *
-     * \todo Make a scaling of fluxes and reverse the scale here.
      * @param rFluxesOnEdges The component of the mSolution Vec which represents fluxes (this vector is resized)
      * @param rPressuresOnNodes The component of the mSolution Vec which represents pressures (this vector is resized)
      */

continuum_mechanics/test/ventilation/TestVentilationProblem.hpp
@@ -248,7 +248,6 @@ public:
         TS_ASSERT_DELTA(flux[4],  -0.0710, 1e-4); // (Inflow flux)
         TS_ASSERT_DELTA(flux[5],  -0.0710, 1e-4); // (Inflow flux)
         TS_ASSERT_DELTA(flux[6],  -0.0710, 1e-4); // (Inflow flux)
-        std::cout<<flux[6];
         //This is the extra node at the Trachea
         TS_ASSERT_DELTA(pressure[8], 2.2222, 1e-4); //Between root and first bifurcation
 
@@ -353,7 +352,7 @@ public:
         VentilationProblem problem("continuum_mechanics/test/data/three_bifurcations");
         problem.SolveProblemFromFile("continuum_mechanics/test/data/ChasteVentilationInput.txt", "VentilationOutput", "3_bifurcations");
     }
-    void joeTestReadFile()  throw (Exception)
+    void TestReadFile()  throw (Exception)
     {
         ReadDataFromFile("continuum_mechanics/test/data/Pdata-Normal.txt");
         TS_ASSERT_EQUALS(pressureAt1.size(), 2501u);
@@ -409,7 +408,7 @@ public:
         VentilationProblem problem("continuum_mechanics/test/data/top_of_tree", 0u);
         PetscOptionsSetValue("-ksp_monitor", "");
 
-        problem.SetViscosity(3e-2); //Even six orders of magnitude don't fix stability issues
+        //problem.SetViscosity(3e-2); //Even six orders of magnitude don't fix stability issues
         problem.SetOutflowPressure(0.0);
         problem.SetConstantInflowPressures(50.0);
 //        problem.SetConstantInflowFluxes(100.0);

commit 3ba343b1a0e8c8ed0a70f868cf78a07a475b1e64

#2300 Fix Windows build broken by r21208
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@21219 b5f69653-2bf5-0310-8201-b0dafb8e1405

continuum_mechanics/src/ventilation/VentilationProblem.cpp
@@ -45,6 +45,7 @@ VentilationProblem::VentilationProblem(const std::string& rMeshDirFilePath, unsi
       mRadiusOnEdge(false),
       mViscosity(1.92e-5),
       mDensity(1.51e-6),
+      mFluxScaling(mViscosity),
       mSolution(NULL)
 {
     TrianglesMeshReader<1,3> mesh_reader(rMeshDirFilePath);

continuum_mechanics/src/ventilation/VentilationProblem.hpp
@@ -81,7 +81,7 @@ private:
      *  This is used in the dynamic (Pedley) resistance calculation
      */
     double mDensity;
-    static const double mFluxScaling = 2e-5; /**< In order to keep the pressure and flux solution at a comparable magnitude, so solve for mFluxScaling * flux */
+    double mFluxScaling;  /**< In order to keep the pressure and flux solution at a comparable magnitude, so solve for mFluxScaling * flux.  This should be the same scale as Poiseuille resistance (comparable to viscosity).*/
     Vec mSolution; /**< Allow access to the solution of the linear system and use as a guess later */
 
     std::vector<Swan2012AcinarUnit*> mAcinarUnits; /**< One acinar unit for each terminal node. \todo These will be abstract*/