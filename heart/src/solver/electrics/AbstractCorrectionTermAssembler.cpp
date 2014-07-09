Chaste/Old-Chaste-svn-mirror

commit 3158925d4c7479cd4726b61fa4e2edb3265882cf

#2560 Should probably check against all bath IDs.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@22123 b5f69653-2bf5-0310-8201-b0dafb8e1405

heart/src/solver/electrics/AbstractCorrectionTermAssembler.cpp
@@ -53,7 +53,7 @@ AbstractCorrectionTermAssembler<ELEMENT_DIM,SPACE_DIM,PROBLEM_DIM>::AbstractCorr
         {
             unsigned element_index = r_element.GetIndex();
             // If bath element, don't try to use SVI
-            if ( r_element.GetAttribute()==HeartRegionCode::GetValidBathId() )
+            if ( HeartRegionCode::IsRegionBath(r_element.GetAttribute()) )
             {
                 mElementsCanDoSvi[element_index] = false;
                 continue;

commit 40cde2bfbcafbfadb70fd927b13029bb8924397d

Fix Intel build broken by r22123/#2560
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@22131 b5f69653-2bf5-0310-8201-b0dafb8e1405

heart/src/solver/electrics/AbstractCorrectionTermAssembler.cpp
@@ -53,7 +53,7 @@ AbstractCorrectionTermAssembler<ELEMENT_DIM,SPACE_DIM,PROBLEM_DIM>::AbstractCorr
         {
             unsigned element_index = r_element.GetIndex();
             // If bath element, don't try to use SVI
-            if ( HeartRegionCode::IsRegionBath(r_element.GetAttribute()) )
+            if ( HeartRegionCode::IsRegionBath(r_element.GetUnsignedAttribute()) )
             {
                 mElementsCanDoSvi[element_index] = false;
                 continue;