Chaste/Old-Chaste-svn-mirror

commit 103dad7b852a02db59dc223e135bd7904eeff5ae

#2788 moving Tyson Novak to non phase based and fixing comments on CreateCCM methods.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@26392 b5f69653-2bf5-0310-8201-b0dafb8e1405

cell_based/src/cell/cycle/AbstractOdeBasedCellCycleModel.cpp
@@ -1,0 +1,157 @@
+/*
+
+Copyright (c) 2005-2016, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "AbstractOdeBasedCellCycleModel.hpp"
+#include <iostream>
+#include <cassert>
+#include "Exception.hpp"
+
+AbstractOdeBasedCellCycleModel::AbstractOdeBasedCellCycleModel(double lastTime,
+                                                               boost::shared_ptr<AbstractCellCycleModelOdeSolver> pOdeSolver)
+    : CellCycleModelOdeHandler(lastTime, pOdeSolver),
+      mDivideTime(lastTime)
+{
+    AbstractCellCycleModel::SetBirthTime(lastTime);
+}
+
+AbstractOdeBasedCellCycleModel::~AbstractOdeBasedCellCycleModel()
+{
+}
+
+AbstractOdeBasedCellCycleModel::AbstractOdeBasedCellCycleModel(const AbstractOdeBasedCellCycleModel& rModel)
+    : AbstractCellCycleModel(rModel),
+      CellCycleModelOdeHandler(rModel),
+      mDivideTime(rModel.mDivideTime)
+{
+    /*
+     * Set each member variable of the new cell-cycle model that inherits
+     * its value from the parent.
+     *
+     * Note 1: some of the new cell-cycle model's member variables will already
+     * have been correctly initialized in its constructor or parent classes.
+     *
+     * Note 2: one or more of the new cell-cycle model's member variables
+     * may be set/overwritten as soon as InitialiseDaughterCell() is called on
+     * the new cell-cycle model.
+     *
+     * Note 3: Only set the variables defined in this class. Variables defined
+     * in parent classes will be defined there.
+     *
+     */
+}
+
+void AbstractOdeBasedCellCycleModel::SetBirthTime(double birthTime)
+{
+    AbstractCellCycleModel::SetBirthTime(birthTime);
+    mLastTime = birthTime;
+    mDivideTime = birthTime;
+}
+
+std::vector<double> AbstractOdeBasedCellCycleModel::GetProteinConcentrations() const
+{
+    assert(mpOdeSystem != NULL);
+    return mpOdeSystem->rGetStateVariables();
+}
+
+void AbstractOdeBasedCellCycleModel::SetProteinConcentrationsForTestsOnly(double lastTime, std::vector<double> proteinConcentrations)
+{
+    assert(mpOdeSystem != NULL);
+    assert(proteinConcentrations.size()==mpOdeSystem->rGetStateVariables().size());
+    mLastTime = lastTime;
+    mpOdeSystem->SetStateVariables(proteinConcentrations);
+}
+
+bool AbstractOdeBasedCellCycleModel::ReadyToDivide()
+{
+    assert(mpCell != NULL);
+
+    if (!mReadyToDivide)
+    {
+        assert(mpOdeSystem != NULL);
+
+        double current_time = SimulationTime::Instance()->GetTime();
+
+        if (current_time > mLastTime)
+        {
+            // Update whether a stopping event has occurred
+            mReadyToDivide = SolveOdeToTime(current_time);
+
+            // Check no concentrations have gone negative
+            for (unsigned i=0; i<mpOdeSystem->GetNumberOfStateVariables(); i++)
+            {
+                if (mpOdeSystem->rGetStateVariables()[i] < -DBL_EPSILON)
+                {
+                    #define COVERAGE_IGNORE
+                    EXCEPTION("A protein concentration " << i << " has gone negative (" <<
+                              mpOdeSystem->rGetStateVariables()[i] << ")\n"
+                              << "Chaste predicts that the CellCycleModel numerical method is probably unstable.");
+                    #undef COVERAGE_IGNORE
+                }
+            }
+            if (mReadyToDivide)
+            {
+                mDivideTime = GetOdeStopTime();
+            }
+        }
+    }
+    return mReadyToDivide;
+}
+
+void AbstractOdeBasedCellCycleModel::ResetForDivision()
+{
+    assert(mReadyToDivide);
+    AbstractCellCycleModel::ResetForDivision();
+    mBirthTime = mDivideTime;
+    mLastTime = mDivideTime;
+    mDivideTime = DBL_MAX;
+}
+
+double AbstractOdeBasedCellCycleModel::GetOdeStopTime()
+{
+    double stop_time = DOUBLE_UNSET;
+    if (mpOdeSolver->StoppingEventOccurred())
+    {
+        stop_time = mpOdeSolver->GetStoppingTime();
+    }
+    return stop_time;
+}
+
+void AbstractOdeBasedCellCycleModel::OutputCellCycleModelParameters(out_stream& rParamsFile)
+{
+    // No new parameters to output
+
+    // Call method on direct parent class
+    AbstractCellCycleModel::OutputCellCycleModelParameters(rParamsFile);
+}

cell_based/src/cell/Cell.cpp
@@ -466,7 +466,7 @@ bool Cell::ReadyToDivide()
 
     // NOTE - we run the SRN model here first before the CCM
     mpSrnModel->SimulateToCurrentTime();
-    // This in turn runs any simulations within the CCM thru UpdateCellCyclePhases()
+    // This in turn runs any simulations within the CCM through ReadyToDivide();
     mCanDivide = mpCellCycleModel->ReadyToDivide();
 
     return mCanDivide;

cell_based/src/cell/cycle/AbstractCellCycleModel.hpp
@@ -121,7 +121,7 @@ protected:
      * This copy-constructor helps subclasses to ensure that all member variables are correctly copied when this happens.
      *
      * This method is called by child classes to set member variables for a daughter cell upon cell division.
-     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateSrnModel() is called,
+     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateCellCycleModel() is called,
      * so performing an exact copy of the parent is suitable behaviour. Any daughter-cell-specific initialisation
      * can be done in InitialiseDaughterCell().
      *
@@ -263,6 +263,18 @@ public:
      *  @return whether a cell with this cell-cycle model is able to fully (terminally) differentiate.
      */
     virtual bool CanCellTerminallyDifferentiate();
+
+    /**
+     * @return the typical cell cycle duration for a transit cell, in hours.
+     * This method must be declared in subclasses.
+     */
+    virtual double GetAverageTransitCellCycleTime()=0;
+
+    /**
+     * @return the typical cell cycle duration for a stem cell, in hours.
+     * This method must be declared in subclasses.
+     */
+    virtual double GetAverageStemCellCycleTime()=0;
 
     /**
      * Outputs cell-cycle model used in the simulation to file and then calls

cell_based/src/cell/cycle/AbstractOdeBasedCellCycleModel.hpp
@@ -33,8 +33,8 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#ifndef ABSTRACTODEBASEDPHASEBASEDCELLCYCLEMODEL_HPP_
-#define ABSTRACTODEBASEDPHASEBASEDCELLCYCLEMODEL_HPP_
+#ifndef ABSTRACTODEBASEDCELLCYCLEMODEL_HPP_
+#define ABSTRACTODEBASEDCELLCYCLEMODEL_HPP_
 
 #include <vector>
 
@@ -42,23 +42,23 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "ClassIsAbstract.hpp"
 #include <boost/serialization/base_object.hpp>
 
-#include "AbstractPhaseBasedCellCycleModel.hpp"
+#include "AbstractCellCycleModel.hpp"
 #include "CellCycleModelOdeHandler.hpp"
 #include "SimulationTime.hpp"
 
 /**
  * This class contains all the functionality shared by 'ODE-based' cell-cycle models,
- * where the duration of one or more cell cycle phases are evaluated 'on the fly'
+ * where the duration of the cell cycle is evaluated 'on the fly'
  * as the cell ages, according to a system of ordinary differential equations (ODEs)
  * governing (for example) the concentrations of key intracellular proteins. To
  * determine when cell division should occur, one or more stopping conditions for
  * this ODE system may be specified.
  *
  * This class of cell-cycle models is distinct from 'simple' cell-cycle models, where
- * the duration of each cell cycle phase is determined when the cell-cycle model is
+ * the duration of the cell cycle is determined when the cell-cycle model is
  * created.
  */
-class AbstractOdeBasedPhaseBasedCellCycleModel : public AbstractPhaseBasedCellCycleModel, public CellCycleModelOdeHandler
+class AbstractOdeBasedCellCycleModel : public AbstractCellCycleModel, public CellCycleModelOdeHandler
 {
 private:
 
@@ -73,11 +73,10 @@ private:
     template<class Archive>
     void serialize(Archive & archive, const unsigned int version)
     {
-        archive & boost::serialization::base_object<AbstractPhaseBasedCellCycleModel>(*this);
+        archive & boost::serialization::base_object<AbstractCellCycleModel>(*this);
         archive & boost::serialization::base_object<CellCycleModelOdeHandler>(*this);
         archive & mDivideTime;
         archive & mFinishedRunningOdes;
-        archive & mG2PhaseStartTime;
     }
 
 protected:
@@ -87,9 +86,6 @@ protected:
 
     /** Whether the cell-cycle model is currently in a delay (not solving ODEs). */
     bool mFinishedRunningOdes;
-
-    /** The start time for the G2 phase. */
-    double mG2PhaseStartTime;
 
     /**
      * Protected copy-constructor for use by CreateCellCycleModel.
@@ -104,33 +100,25 @@ protected:
      *
      * @param rModel the cell cycle model to copy.
      */
-    AbstractOdeBasedPhaseBasedCellCycleModel(const AbstractOdeBasedPhaseBasedCellCycleModel& rModel);
+    AbstractOdeBasedCellCycleModel(const AbstractOdeBasedCellCycleModel& rModel);
 
 public:
 
     /**
-     * Creates an AbstractOdeBasedPhaseBasedCellCycleModel, calls SetBirthTime on the
+     * Creates an AbstractOdeBasedCellCycleModel, calls SetBirthTime on the
      * AbstractPhaseBasedCellCycleModel to make sure that can be set 'back in time' for
      * cells which did not divide at the current time.
      *
      * @param lastTime  The birth time of the cell / last time model was evaluated (defaults to the current SimulationTime)
      * @param pOdeSolver An optional pointer to a cell-cycle model ODE solver object (allows the use of different ODE solvers)
      */
-    AbstractOdeBasedPhaseBasedCellCycleModel(double lastTime = SimulationTime::Instance()->GetTime(),
+    AbstractOdeBasedCellCycleModel(double lastTime = SimulationTime::Instance()->GetTime(),
                                    boost::shared_ptr<AbstractCellCycleModelOdeSolver> pOdeSolver = boost::shared_ptr<AbstractCellCycleModelOdeSolver>());
 
     /**
      * Destructor.
      */
-    virtual ~AbstractOdeBasedPhaseBasedCellCycleModel();
-
-    /**
-     * Default UpdateCellCyclePhase() method for an ODE-based cell-cycle model.
-     * This method calls SolveOdeToTime() for G1 phase and adds time for the other phases.
-     *
-     * Can be overridden if they should do something more subtle.
-     */
-    virtual void UpdateCellCyclePhase();
+    virtual ~AbstractOdeBasedCellCycleModel();
 
     /**
      * Get the time at which the ODE stopping event occurred.
@@ -142,7 +130,7 @@ public:
     double GetOdeStopTime();
 
     /**
-     * This overrides the AbstractPhaseBasedCellCycleModel::SetBirthTime(double birthTime)
+     * This overrides the AbstractCellCycleModel::SetBirthTime(double birthTime)
      * because an ODE based cell-cycle model has more to reset...
      *
      * @param birthTime the simulation time when the cell was born
@@ -166,9 +154,16 @@ public:
     void SetProteinConcentrationsForTestsOnly(double lastTime, std::vector<double> proteinConcentrations);
 
     /**
+     * See AbstractCellCycleModel::ResetForDivision()
+     *
+     * @return whether the cell is ready to divide (enter M phase).
+     */
+    virtual bool ReadyToDivide();
+
+    /**
      * For a naturally cycling model this does not need to be overridden in the
      * subclasses. But most models should override this function and then
-     * call AbstractOdeBasedPhaseBasedCellCycleModel::ResetForDivision() from inside their version.
+     * call AbstractOdeBasedCellCycleModel::ResetForDivision() from inside their version.
      */
     virtual void ResetForDivision();
 
@@ -180,6 +175,6 @@ public:
     virtual void OutputCellCycleModelParameters(out_stream& rParamsFile);
 };
 
-CLASS_IS_ABSTRACT(AbstractOdeBasedPhaseBasedCellCycleModel)
+CLASS_IS_ABSTRACT(AbstractOdeBasedCellCycleModel)
 
-#endif /*ABSTRACTODEBASEDPHASEBASEDCELLCYCLEMODEL_HPP_*/
+#endif /*ABSTRACTODEBASDCELLCYCLEMODEL_HPP_*/

cell_based/src/cell/cycle/AbstractPhaseBasedCellCycleModel.hpp
@@ -134,7 +134,7 @@ protected:
      * This copy-constructor helps subclasses to ensure that all member variables are correctly copied when this happens.
      *
      * This method is called by child classes to set member variables for a daughter cell upon cell division.
-     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateSrnModel() is called,
+     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateCellCycleModel() is called,
      * so performing an exact copy of the parent is suitable behaviour. Any daughter-cell-specific initialisation
      * can be done in InitialiseDaughterCell().
      *

cell_based/src/cell/cycle/AbstractSimpleGenerationBasedCellCycleModel.hpp
@@ -87,7 +87,7 @@ protected:
      * This copy-constructor helps subclasses to ensure that all member variables are correctly copied when this happens.
      *
      * This method is called by child classes to set member variables for a daughter cell upon cell division.
-     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateSrnModel() is called,
+     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateCellCycleModel() is called,
      * so performing an exact copy of the parent is suitable behaviour. Any daughter-cell-specific initialisation
      * can be done in InitialiseDaughterCell().
      *

cell_based/src/cell/cycle/AbstractSimplePhaseBasedCellCycleModel.hpp
@@ -88,7 +88,7 @@ protected:
      * This copy-constructor helps subclasses to ensure that all member variables are correctly copied when this happens.
      *
      * This method is called by child classes to set member variables for a daughter cell upon cell division.
-     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateSrnModel() is called,
+     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateCellCycleModel() is called,
      * so performing an exact copy of the parent is suitable behaviour. Any daughter-cell-specific initialisation
      * can be done in InitialiseDaughterCell().
      *

cell_based/src/cell/cycle/Alarcon2004OxygenBasedCellCycleModel.hpp
@@ -85,7 +85,7 @@ protected:
      * This copy-constructor helps subclasses to ensure that all member variables are correctly copied when this happens.
      *
      * This method is called by child classes to set member variables for a daughter cell upon cell division.
-     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateSrnModel() is called,
+     * Note that the parent cell cycle model will have had ResetForDivision() called just before CreateCellCycleModel() is called,
      * so performing an exact copy of the parent is suitable behaviour. Any daughter-cell-specific initialisation
      * can be done in InitialiseDaughterCell().
      *

commit 150db50b2c94cce087fbc7361b7a4430324dad60

#2788 - fix Intel build broken by r26392 by initialising all member variables.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/trunk@26409 b5f69653-2bf5-0310-8201-b0dafb8e1405

cell_based/src/cell/cycle/AbstractOdeBasedCellCycleModel.cpp
@@ -41,7 +41,8 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 AbstractOdeBasedCellCycleModel::AbstractOdeBasedCellCycleModel(double lastTime,
                                                                boost::shared_ptr<AbstractCellCycleModelOdeSolver> pOdeSolver)
     : CellCycleModelOdeHandler(lastTime, pOdeSolver),
-      mDivideTime(lastTime)
+      mDivideTime(lastTime),
+      mFinishedRunningOdes(false)
 {
     AbstractCellCycleModel::SetBirthTime(lastTime);
 }
@@ -53,7 +54,8 @@ AbstractOdeBasedCellCycleModel::~AbstractOdeBasedCellCycleModel()
 AbstractOdeBasedCellCycleModel::AbstractOdeBasedCellCycleModel(const AbstractOdeBasedCellCycleModel& rModel)
     : AbstractCellCycleModel(rModel),
       CellCycleModelOdeHandler(rModel),
-      mDivideTime(rModel.mDivideTime)
+      mDivideTime(rModel.mDivideTime),
+      mFinishedRunningOdes(false)
 {
     /*
      * Set each member variable of the new cell-cycle model that inherits